c4baa02d44c47158c0fbfab829143c6c18f44ca4
Neural Network Research Group <nn@cs.utexas.edu>
2004-02-22 01:09:07 +0000

    More generic version of gaussiansheet.py. However it only supports gaussian kernels.

---
+++ topo/inputsheet.py
@@ -0,0 +1,85 @@
+"""
+sheet for randomly generating inputs
+
+$Id$
+"""
+
+import debug
+from kernelfactory import *
+import random
+from simulator import EventProcessor
+from sheet import Sheet 
+from utils import NxN
+
+from Numeric import *
+from pprint import pprint,pformat
+from params import setup_params
+
+class GaussianSheet(Sheet):
+
+    period = 1
+    phase = 0
+
+    x = (random.uniform,-1,1)
+
+    def __init__(self,**config):
+        Sheet.__init__(self,**config)
+        setup_params(self,GaussianSheet,**config)
+
+        self.kernel =  KernelFactory(bounds=self.bounds,
+                                     density=self.density,
+                                     x=self.x,
+                                     y=self.y,
+                                     theta=self.theta,
+                                     width=self.width,
+                                     height=self.height)
+
+    def start(self):
+        assert self.simulator
+
+        # connect self<->self (for repeating)
+        self.simulator.connect(src=self,dest=self,delay=self.period)
+
+        # first event is special
+        self.simulator.enqueue_event_rel(self.phase,self,self,data=self.activation)
+
+    def input_event(self,src,src_port,dest_port,data):
+        self.db_print("Received %s input from %s." % (NxN(data.shape),src), debug.VERBOSE)
+
+        self.db_print("Generating a new kernel...",debug.VERBOSE)
+
+        self.activation = self.kernel.create( )
+  
+        self.send_output(data=self.activation)
+        self.db_print("Sending %s output." % NxN(self.activation.shape))
+
+if __name__ == '__main__':
+    from simulator import Simulator
+    from image import ImageSaver
+
+    def random_gaussian(mean, std):
+        while( 1 ):
+            yield random.gauss(mean, std)
+
+    def random_uniform(lower, upper):
+        while( 1 ):        
+            yield random.uniform(lower, upper)
+
+    #GaussianSheet.density = 81 
+    GaussianSheet.density = 10000
+    GaussianSheet.period = 10.0
+    GaussianSheet.phase = 3.0
+
+    GaussianSheet.x = random_gaussian(0,0.5)
+    GaussianSheet.y = random_uniform(-0.5,0.5)
+    GaussianSheet.theta = random_uniform(-3.1415926,3.1415926)
+    GaussianSheet.width = random_uniform(0,1)
+    GaussianSheet.height = random_uniform(0,1)
+
+
+    s = Simulator()
+    g = GaussianSheet()
+    save = ImageSaver()
+
+    s.connect(src=g,dest=save)
+    s.run()
