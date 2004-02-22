a634a74c105551cbd72bc5760e69795a90441b46
Neural Network Research Group <nn@cs.utexas.edu>
2004-02-22 01:09:35 +0000

    Change params back to the original.

--- topo/params.py
+++ topo/params.py
@@ -6,22 +6,6 @@ This module is a placeholder for our params/defaults database system
 $Id$
 """
 
-
-"""
-applytuple -- takes in an argument, if its a tuple, it treats it 
-                as a function and applies it over the arguments
-                otherwise it treats it as a literal
-"""
-
-def applytuple(func):
-    # print "Called apply tuple on: ", func
-    if(type(func) != tuple): 
-        return func
-    else:
-        return (apply(func[0],func[1:]))
-
-
-
 def get_param(param_name,dict,default=None):
   """
   Get the named parameter from a dictionary, else return the default.
@@ -40,7 +24,7 @@ def setup_params(obj,cls,**params):
     if var[:2] != '__':
       try:
         #print "Trying to set ", var
-        obj.__dict__[var] = applytuple(params[var])
+        obj.__dict__[var] = params[var]
         #print "Set ", var, " to ", obj.__dict__[var]
       except KeyError:
         pass
