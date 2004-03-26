aa25e0f816c93d4ce7503740a090a4e87a2d7021
Joseph Reisinger <joeraii@cs.utexas.edu>
2004-03-26 08:14:49 +0000

    Merged two different versions. Better testing functions.

--- topo/inputsheet.py
+++ topo/inputsheet.py
@@ -59,19 +59,34 @@ if __name__ == '__main__':
     from image import ImageSaver
 
     # BUG?: these give errors now:
-    GaussianSheet.density = 10000
-    GaussianSheet.period = 1.0
+    #GaussianSheet.density = 10000
+    #GaussianSheet.period = 1.0
 
-    GaussianSheet.x = lambda:random.uniform(-0.5,0.5)
-    GaussianSheet.y = lambda:random.uniform(-0.5,0.5)
-    GaussianSheet.theta = lambda:random.uniform(-3.1415926,3.1415926)
-    GaussianSheet.width = 0.05
-    GaussianSheet.height = 0.2
+    #GaussianSheet.x = lambda:random.uniform(-0.5,0.5)
+    #GaussianSheet.y = lambda:random.uniform(-0.5,0.5)
+    #GaussianSheet.theta = lambda:random.uniform(-3.1415926,3.1415926)
+    #GaussianSheet.width = 0.05
+    #GaussianSheet.height = 0.2
 
     s = Simulator()
-    g = GaussianSheet()
-
+    g = GaussianSheet(density=10000, 
+                      period=10.0, 
+                      phase=3.0,
+                      x = lambda:random.gauss(0,0.5),
+                      y = lambda:random.gauss(0,0.5),
+                      theta  = lambda:random.uniform(-3.1415926, 3.1415926),
+                      width  = lambda:random.uniform(0,1),
+                      height = lambda:random.uniform(0,1))
+    sg = SineGratingSheet(density=10000, 
+                      period=10.0, 
+                      phase=3.0,
+                      x = lambda:random.gauss(0,0.5),
+                      y = lambda:random.gauss(0,0.5),
+                      theta  = lambda:random.uniform(-3.1415926, 3.1415926),
+                      frequency  = lambda:random.uniform(10,20),
+                      amplitude = lambda:random.uniform(5,10))
     save = ImageSaver()
 
+    s.connect(src=sg,dest=save,dest_port='sine_grating')
     s.connect(src=g,dest=save,dest_port='gassian')
     s.run(duration=100)
