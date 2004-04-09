25072eb2f701c93ff5a241cd08eb3d70ecd58bae
Joseph Reisinger <joeraii@cs.utexas.edu>
2004-04-09 07:01:30 +0000

    New kernel factory without classes, and input sheet to reflect these changes. Some problems: Gabor probably doesn't work correctly, also there may be an issue with how parameters are being passed.

--- topo/inputsheet.py
+++ topo/inputsheet.py
@@ -19,8 +19,9 @@ class InputSheet(Sheet):
     period = Parameter(default=1)
     phase  = Parameter(default=0)
 
-    x = Parameter(default=0)
-
+    bounds  = Parameter(default=BoundingBox(points=((-0.5,-0.5), (0.5,0.5))))
+    density = Parameter(default=10000)
+    
     def start(self):
         assert self.simulator
 
@@ -32,64 +33,123 @@ class InputSheet(Sheet):
 
     def input_event(self,src,src_port,dest_port,data):
         self.verbose("Received %s input from %s." % (NxN(data.shape),src))
-
         self.verbose("Generating a new kernel...")
 
-        self.activation = self.create( )
-  
+        self.activation = produce_value(self.function)
+        
         self.send_output(data=self.activation)
         self.message("Sending %s output." % NxN(self.activation.shape))
 
-class GaussianSheet(InputSheet, GaussianKernelFactory):
-    pass
-
-class SineGratingSheet(InputSheet, SineGratingKernelFactory):
-    pass
-
-class GaborSheet(InputSheet, GaborKernelFactory):
-    pass
-
-class UniformRandomSheet(InputSheet, UniformRandomKernelFactory):
-    pass
+"""
+Gassian Kernel Generating Sheet
+"""
 
 
+class GaussianSheet(InputSheet):
+
+    x       = Parameter(default=0)
+    y       = Parameter(default=0)
+    theta   = Parameter(default=0)
+    width   = Parameter(default=1)
+    height  = Parameter(default=1)
+
+    # TODO: Why does this have to be a parameter?
+
+    function = Parameter(default=lambda:gaussian( GaussianSheet.bounds,
+                                                  GaussianSheet.density,
+                                                  GaussianSheet.x, 
+                                                  GaussianSheet.y, 
+                                                  GaussianSheet.theta, 
+                                                  GaussianSheet.width, 
+                                                  GaussianSheet.height ))
+
+"""
+Sine Grating Kernel Generating Sheet
+"""
+
+class SineGratingSheet(InputSheet):
+
+    x         = Parameter(default=0)
+    y         = Parameter(default=0)
+    theta     = Parameter(default=0)
+    amplitude = Parameter(default=1)
+    frequency = Parameter(default=1)
+
+    function  = Parameter(default=lambda:sine_grating( SineGratingSheet.bounds,
+                                                       SineGratingSheet.density,
+                                                       SineGratingSheet.x,
+                                                       SineGratingSheet.y,
+                                                       SineGratingSheet.theta,
+                                                       SineGratingSheet.amplitude,
+                                                       SineGratingSheet.frequency  ) )
+
+
+"""
+Gabor Kernel Generating Sheet
+"""
+
+class GaborSheet(InputSheet):
+
+    x        = Parameter(default=0)
+    y        = Parameter(default=0)
+    theta    = Parameter(default=0)
+    width    = Parameter(default=2)
+    height   = Parameter(default=1)
+
+    function  = Parameter(default=lambda:sine_grating( GaborSheet.bounds,
+                                                       GaborSheet.density,
+                                                       GaborSheet.x,
+                                                       GaborSheet.y,
+                                                       GaborSheet.theta,
+                                                       GaborSheet.width,
+                                                       GaborSheet.height  ) )
+
+
+  
+class UniformRandomSheet(InputSheet):
+
+    # TODO: Does UniformRandomSheet.x refer to this specific instance's x, or
+    # the class's x
+
+    function = Parameter(default=lambda:uniform_random( UniformRandomSheet.bounds,
+                                                        UniformRandomSheet.density ) )
 
 if __name__ == '__main__':
     from simulator import Simulator
     from image import ImageSaver
 
-    GaussianSheet.density = 10000
     GaussianSheet.period = 1.0
-
-    def translation( begin, step, end ):
-        x = begin
-        while(x < end):
-            x += step
-            yield x
-        while(1):
-            yield x    
-
-    #GaussianSheet.x = lambda:random.uniform(-0.5,0.5)
-    GaussianSheet.x = translation(-0.5, 0.05, 0.5)
-    #GaussianSheet.y = lambda:random.uniform(-0.5,0.5)
-    GaussianSheet.y = translation(-0.5, 0.05, 0.5) 
-    #GaussianSheet.theta = lambda:random.uniform(-3.1415926,3.1415926)
+    GaussianSheet.x = lambda:random.uniform(-0.5,0.5)
+    GaussianSheet.y = lambda:random.uniform(-0.5,0.5)
     GaussianSheet.theta = 3.1415926/4
     GaussianSheet.width = 20
     GaussianSheet.height = 0.2 
 
-    s = Simulator()
-    g = GaussianSheet() 
-    sg = SineGratingSheet(density=10000, 
-                      period=10.0, 
-                      phase=3.0,
-                      x = lambda:random.gauss(0,0.5),
-                      y = lambda:random.gauss(0,0.5),
-                      theta  = lambda:random.uniform(-3.1415926, 3.1415926),
-                      frequency  = lambda:random.uniform(10,20),
-                      amplitude = lambda:random.uniform(5,10))
+    SineGratingSheet.period = 1.0
+    SineGratingSheet.x = lambda:random.uniform(-0.5,0.5)
+    SineGratingSheet.y = lambda:random.uniform(-0.5,0.5)
+    SineGratingSheet.theta = 3.1415926/4
+    SineGratingSheet.amplitude = 20
+    SineGratingSheet.freguency = 2 
+
+    GaborSheet.period = 1.0
+    GaborSheet.x = lambda:random.uniform(-0.5,0.5)
+    GaborSheet.y = lambda:random.uniform(-0.5,0.5)
+    GaborSheet.theta = 0
+    GaborSheet.width =  0.5 
+    GaborSheet.height = 0.5 
+    
+    s  = Simulator()
+
+    g  = GaussianSheet() 
+    sg = SineGratingSheet()
+    ga = GaborSheet() 
+    ur = UniformRandomSheet()
+
     save = ImageSaver()
 
+    s.connect(src=ur,dest=save,dest_port='random')
+    s.connect(src=ga,dest=save,dest_port='gabor')
     s.connect(src=sg,dest=save,dest_port='sine_grating')
     s.connect(src=g,dest=save,dest_port='gassian')
     s.run(duration=100)
--- topo/kernelfactory.py
+++ topo/kernelfactory.py
@@ -14,49 +14,26 @@ from boundingregion import BoundingBox
 from sheet import sheet2matrix, matrix2sheet, bounds2shape
 
 from Numeric import *
+from RandomArray import *
 from params import * 
 from pprint import pprint,pformat
 
 from math import pi
 
-
-def rectangle(kernel_x, kernel_y, width, height, theta):
-    # need to specify the bounds somehow 
-    return kernel_x
-
-def fuzzy_line(kernel_x, kernel_y, width, height, theta):
-    return kernel_x
-
-def fuzzy_disc(kernel_x, kernel_y, width, height, theta):
-    return kernel_x
-
 """
-Abstract base class for the different kinds of kernels
-"""
+Gaussian Kernel Factory
+"""      
 
-class KernelFactory(base.TopoObject):
+def gaussian(bounds, density, x, y, theta, width, height):
+        left,bottom,right,top = bounds.aarect().lbrt()
+        bound_width  = right-left
+        bound_height = top-bottom
+        linear_density = sqrt(density)
 
-    #kernel = array([[1.0]])
-
-    bounds   = Parameter(default=BoundingBox( points=((0,0), (1,1)) ))
-    density  = Parameter(default=1.0)
-
-    # setup some default parameters. keep in mind that these are effectively
-    # meaningless
-    x = Parameter(default=0.5) 
-    y = Parameter(default=0.5)
-   
-
-    def create(self):
-        left,bottom,right,top = self.bounds.aarect().lbrt()
-        self.bound_width  = right-left
-        self.bound_height = top-bottom
-        self.linear_density = sqrt(self.density)
-
-        rows,cols = bounds2shape(self.bounds,self.density)
+        rows,cols = bounds2shape(bounds,density)
         
-        x = produce_value(self.x)
-        y = produce_value(self.y) 
+        x = produce_value(x)
+        y = produce_value(y) 
 
         # TODO: this can generate ouput that may be off by one in terms of size,
         # for example most times this generates a 100x100 image, but sometimes
@@ -64,27 +41,12 @@ class KernelFactory(base.TopoObject):
 
         # TODO: Use sheet operations defined in sheet.py
 
-        # TODO: fix the 90 degree rotation problem
-
-        self.kernel_y = arange(left-x,right-x, self.bound_width/cols)
-        self.kernel_x = arange(bottom-y,top-y, self.bound_height/rows)
-  
-        return self.function(self.kernel_x, self.kernel_y)
-
-"""
-Gaussian Kernel Factory
-"""      
-
-class GaussianKernelFactory(KernelFactory):
-    width = Parameter(default=0.5) 
-    height = Parameter(default=0.5)
-    theta = Parameter(default=0.0)
+        kernel_y = arange(left-x,right-x, bound_width/cols)
+        kernel_x = arange(bottom-y,top-y, bound_height/rows)
  
-    def function(self, kernel_x, kernel_y):
-
-        theta  = produce_value(self.theta)
-        width  = produce_value(self.width)
-        height = produce_value(self.height) 
+        theta  = produce_value(theta)
+        width  = produce_value(width)
+        height = produce_value(height) 
 
         new_kernel_x = subtract.outer(cos(theta)*kernel_x, sin(theta)*kernel_y)
         new_kernel_y = add.outer(sin(theta)*kernel_x, cos(theta)*kernel_y)
@@ -98,21 +60,28 @@ Sine Grating Kernel Factory
 """
 
 
-class SineGratingKernelFactory(KernelFactory):
-    amplitude = Parameter(default=0.5) 
-    frequency = Parameter(default=0.5)
-    theta     = Parameter(default=0.0)
- 
-    def function(self, kernel_x, kernel_y):
+def sine_grating(bounds, density, x, y, theta, amplitude, frequency):
+        left,bottom,right,top = bounds.aarect().lbrt()
+        bound_width  = right-left
+        bound_height = top-bottom
+        linear_density = sqrt(density)
 
-        amplitude = produce_value(self.amplitude)
-        frequency = produce_value(self.frequency)
-        theta     = produce_value(self.theta) 
+        rows,cols = bounds2shape(bounds,density)
+        
+        x = produce_value(x)
+        y = produce_value(y) 
+
+        kernel_y = arange(left-x,right-x, bound_width/cols)
+        kernel_x = arange(bottom-y,top-y, bound_height/rows)
+ 
+        amplitude = produce_value(amplitude)
+        frequency = produce_value(frequency)
+        theta     = produce_value(theta) 
 
         new_kernel_x = subtract.outer(cos(theta)*kernel_x, sin(theta)*kernel_y)
         new_kernel_y = add.outer(sin(theta)*kernel_x, cos(theta)*kernel_y)
 
-        new_kernel = amplitude*sin(frequency*new_kernel_x)
+        new_kernel = amplitude*sin(frequency*2*pi*new_kernel_x)
 
         return new_kernel
 
@@ -120,31 +89,50 @@ class SineGratingKernelFactory(KernelFactory):
 Gabor Kernel Factory
 """
 
-class GaborKernelFactory(KernelFactory):
-    width  = Parameter(default=0.5) 
-    height = Parameter(default=0.5)
-    theta  = Parameter(default=0.0)
- 
-    def function(self, kernel_x, kernel_y):
+def gabor(bounds, density, x, y, theta, width, height):
+        left,bottom,right,top = bounds.aarect().lbrt()
+        bound_width  = right-left
+        bound_height = top-bottom
+        linear_density = sqrt(density)
+
+        rows,cols = bounds2shape(bounds,density)
         
-        theta  = produce_value(self.theta)
-        width  = produce_value(self.width)
-        height = produce_value(self.height) 
+        x = produce_value(x)
+        y = produce_value(y) 
+
+        kernel_y = arange(left-x,right-x, bound_width/cols)
+        kernel_x = arange(bottom-y,top-y, bound_height/rows)
+ 
+        theta  = produce_value(theta)
+        width  = produce_value(width)
+        height = produce_value(height) 
 
         new_kernel_x = subtract.outer(cos(theta)*kernel_x, sin(theta)*kernel_y)
         new_kernel_y = add.outer(sin(theta)*kernel_x, cos(theta)*kernel_y)
 
-        return exp( -(new_kernel_x/width)**2 - (new_kernel_y/height)**2 ) * cos( 2*pi*new_kernel_x )
+        # TODO: this doesn't seem to be working correctly.
+
+        return exp( maximum(-100, -(new_kernel_x/width)**2 - (new_kernel_y/height)**2 )) * cos( 2*pi*new_kernel_x )
 
 """
 Uniform Random Kernel Factory
 """
 
-class UniformRandomKernelFactory(KernelFactory):
- 
-    def function(self, kernel_x, kernel_y):
-        
-        return 
+def uniform_random(bounds, density):
+        rows,cols = bounds2shape(bounds,density)
+                                                                                                                   
+        return random((rows,cols)) 
+
+
+def rectangle(kernel_x, kernel_y, width, height, theta):
+    # need to specify the bounds somehow 
+    return kernel_x
+
+def fuzzy_line(kernel_x, kernel_y, width, height, theta):
+    return kernel_x
+
+def fuzzy_disc(kernel_x, kernel_y, width, height, theta):
+    return kernel_x
 
 
 
