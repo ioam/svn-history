ac3771195899de3c7b6f1fb0835aa70ce4606eb7
Judah De Paula <judah@cs.utexas.edu>
2004-06-26 01:14:01 +0000

    Windows Install front end

---
+++ setup.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+ECHO This will install Topographica and its package dependencies.
+
+REM Developer's Note: Install Python then call the setup.py script
+REM inside the external/win32 directory.
+REM $Id$
+
+external\win32\Y_OR_N /Q /A "Do you wish to continue? [Y/n]"
+
+IF ERRORLEVEL 3 GOTO :INSTALL
+IF ERRORLEVEL 2 GOTO :EXIT
+IF ERRORLEVEL 1 GOTO :INSTALL
+
+
+:INSTALL
+ftype Python.File | findstr "Python.File="
+IF ERRORLEVEL 1 GOTO :INSTALL_PYTHON
+IF ERRORLEVEL 0 GOTO :RUN_SETUP
+
+
+:INSTALL_PYTHON
+ECHO Python file associations not detected: Installing Python 2.3
+start /wait external/win32/Python-2.3.4.exe
+
+
+:RUN_SETUP
+cd external/win32
+start /wait setup.py
+cd ../..
+
+
+:EXIT
+ECHO Exiting
