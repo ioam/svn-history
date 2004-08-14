6b55747d51fb42c0bfcf2c4eb97e451c68608b04
Judah De Paula <judah@cs.utexas.edu>
2004-08-14 11:17:09 +0000

    Starting version

---
+++ tests/testhistogram.py
@@ -0,0 +1,14 @@
+import unittest
+
+import Numeric
+from topo.histogram import *
+
+class TestHistogram(unittest.TestCase):
+    def test_calculate(self):
+        h1 = Histogram(Numeric.ones((20,20)))
+        h1.histogram()
+
+
+
+suite = unittest.TestSuite()
+suite.addTest(unittest.makeSuite(TestHistogram))
