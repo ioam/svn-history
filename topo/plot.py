e43da9ff434b9bc159d2312a645f5d5b763b6c1c
Judah De Paula <judah@cs.utexas.edu>
2004-08-14 12:32:23 +0000

    Completed.  No testing yet.

--- topo/plot.py
+++ topo/plot.py
@@ -12,20 +12,25 @@ which arranges multiple plots into a single figure on the screen.
 $Id$
 """
 import sys
+import types
 
 from base import TopoObject
 from sheetview import *
+from bitmap import matrix_rgb_to_hsv
 
 # Types of plots that Plot knows how to piece together from the input
 # matrices.
 RGB = 'RGB'
 HSV = 'HSV'
-CMAP = 'COLORMAP'
+COLORMAP = 'COLORMAP'
 
 # Shape of the plotting display used by PlotGroup.  NOTE: INCOMPLETE,
 # THERE SHOULD BE MORE TYPES OF SHAPES SUCH AS SPECIFYING X ROWS, OR Y
 # COLUMNS, OR GIVING A LIST OF INTEGERS REPRESENTING NUMBER OF PLOTS
-# FOR EACH ROW.
+# FOR EACH ROW.  Proposed format:  If three columns are desired with the
+# plots laid out from left to right, then use (None, 3).  If three rows
+# are desired then (3, None).  Or more interesting, [3,2,4] would have
+# the first row with 3, the second row with 2, the third row with 4, etc.
 FLAT = 'FLAT'
 
 class Plot(TopoObject):
@@ -36,58 +41,91 @@ class Plot(TopoObject):
     plots within a single bitmap on the screen.
     """
 
-    def __init__(self, sheet_tuple_list=None, **params):
+    def __init__(self, (channel_1, channel_2, channel_3), plot_type, sheet=None, **params):
         """
-        sheet_tuple_list has tuples of the sheet and the name of the
-        sheet to be retrieved.  Ex: [(Eye0,Activity),(Eye1,Activity)]
-        The default is None, which means all existing sheets in the known
-        universe that has a View filed under the key entered with
-        generate_plot(key_name).
+        sheet gives the object that the three channels will come from
+        in generation of the bitmap.
+
+        channel_1, channel_2, and channel_3 all have one of two types:
+        It is either a string that points to a stored SheetView on the
+        variable sheet, or
+        It is a SheetView object that has already been constructed.
+
+        If each of the channels are preconstructed SheetViews, there
+        is no reason to pass in the sheet, and therefore it can be
+        left blank.
 
         Named parameter name is being inherited from TopoObject
 
-        __init__ does not generate the plot instead you must also call
-        generate_plot.  This is done in case the measurement causes a
-        change in the Sheet providing the View.
-
-        What about BoundingRegions?
+        WHAT ABOUT BOUNDINGREGIONS?
         """
         super(Plot,self).__init__(**params)
-        self.sheet_tuples = sheet_tuple_list
-        self.op = operation
+        self.source = sheet
+        self.channels = (channel_1, channel_2, channel_3)
+        self.plot_type = plot_type
         self.histograms = []
         self.plot_pending = False
-        self.matrix = None
+        self.channel_views = []
+        self.matrices = None         # Will hold 3 2D matrices.
 
 
     def plot(self):
         """
-        Uses the cached plot, or regenerate if needed.
-        Return: 2-Tuple, (Calculated Matrix, List of Histograms)
+        Get the Views requested from each Channel, Generates
+        Histograms
+        Returns: 2-Tuple, (tri-tuple of 2D Matrix mapping to
+        R/G/B channels, List of Histograms)
         """
-        if not self.plot_pending:
-            generate_plot()
-        return (self.matrix, self.histograms)
 
+        #  Convert what is in the channels into SheetViews if not already
+        for each in self.channels:
+            if isinstance(each,str):
+                sv = self.source.sheet_view(each)
+                if sv:
+                    self.channel_views.append(sv)
+            elif isinstance(each,SheetView):
+                self.channel_views.append(each)
+            elif each == None:
+                self.channel_views.append(each)
+            else:
+                self.warning(each, 'not a valid string, SheetView, or None')
 
-    def generate(self,key_name='Activity'):
-        """
-        Poll each Sheet in the tuple list and get the Views requested,
-        then combine as specified by self.op.  Generates Histograms
-        and caches the data for the next plot() request.
+        # NOTE:  THIS NEEDS TO BE CHANGED WHEN BOUNDINGBOXES ARE
+        # IMPLEMENTED TO TRIM DOWN THE SIZES OF THE MATRICES!
+        shape = [each.shape for each in self.channel_views if not None]
+        for each in self.channel_views:
+            if each == None:
+                self.matrices.append(Numeric.array(shape[0]))
+            else:
+                self.matrices.append(each[0])
 
-        If sheet_tuple_list is empty at object creation, all Sheets
-        are checked for the View filed under key_name (default is
-        Activity).
-        """
-        if self.sheet_tuples:
-            for (sheet, view_name) in self.sheet_tuples:
-                sheet_view = None
-                #NYI
+        # By this point, self.matrices should have a triple of 2D
+        # matrices trimmed and ready to go to the caller of this
+        # function ... after a possible conversion to a different
+        # palette form.
+
+        if self.plot_type == HSV:
+            # Do the RGB-->HSV conversion, assume the caller will be
+            # displaying the plot as an RGB.
+            self.matrices = bitmap.matrix_rgb_to_hsv(self.matrices)
+        elif self.plot_type == COLORMAP:
+            # Don't delete anything, maybe they want position #3, but
+            # do warn them if they have more than one channel requested.
+            single_map = [each for each in self.channels if each]
+            if len(single_map) > 1:
+                self.warning('More than one channel requested for single-channel colormap')
+        elif self.plot_type == RGB:
+            #  Do nothing, we're already in the RGB form.
+            #  Possibly change the order to make it match the Lissom order.
+            None
         else:
-            #  Do the global name check against the key_name
-            sheet_view = None
-            #NYI
+            self.warning('Unrecognized plot type')
+
+        # Construct a list of Histogram objects from the matrices that have
+        # been created and are waiting to go.
+        self.histograms = [Hisogram(each) for each in self.matrices if each]
+        
+        return (self.matrices, self.histograms)
 
 
 
@@ -95,16 +133,13 @@ class PlotGroup(TopoObject):
     """
     Container that has one or more Plots and also knows how to arrange
     the plots and other special parameters.  Works with the bitmap
-    display mechanisms.
+    display mechanisms to create a matrix that the caller can dump to
+    a topo.Bitmap object 
     """
 
-    def __init__(self, sheet_tuple_list=None, shape=FLAT, **params):
+    def __init__(self,plot_list,shape=FLAT,**params):
         """
-        sheet_tuple_list has tuples of the sheet and the name of the
-        sheet to be retrieved.  Ex: [(Eye0,Activity),(Eye1,Activity)]
-        The default is None, which means all existing sheets in the
-        known universe that have a View filed under the key entered
-        with generate_plot(key_name).
+        plot_list has the plots to add to this group. 
 
         Shape gives the visual arrangement of the plots within the group.
         The default is FLAT (None) with each subsequent plot to the right
@@ -113,45 +148,35 @@ class PlotGroup(TopoObject):
         (5, None) will have 5 rows of of plots.  An X-tuple will have
         each subsequent row the length of the corresponding value in
         the tuple.
-
-        __init__ does not generate the plot instead you must also call
-        generate_plot.  This is done in case the measurement causes a
-        change in the Sheet providing the View.
-
-        What about BoundingRegions?
         """
         super(PlotGroup,self).__init__(**params)
-        self.sheet_tuples = sheet_tuple_list
+        self.plot_list = plot_list
         self.shape = shape
-        self.plot_pending = False
         self.plot_list = []
+
+
+    def add(self,new_plot):
+        """
+        new_plot can be a single Plot, or it can be a list of plots.
+        Either way, it will be properly appended to the end of self's
+        plot_list.
+        """
+        if isinstance(new_plot,types.ListType):
+            self.plot_list.extend(new_plot)
+        else:
+            new_plot = self.plot_list.append(new_plot)
 
 
     def plot(self):
         """
-        Uses the cached plot, or regenerate if needed.
-        Return: 2-Tuple, (Calculated Matrix, List of Histograms)
+        Get the matrix data from each of the Plot objects and paste
+        the data into a larger matrix.
         """
-        if not self.plot_pending:
-            generate_plot()
-        return (self.matrix, self.histograms)
+        big_matrix = None
+        # TBI
+        # TBI
+        return big_matrix
 
-
-    def generate_plot(self,key_name='Activity'):
-        """
-        If sheet_tuple_list is empty at object creation, all Sheets
-        are checked for the View filed under key_name (default is
-        Activity).
-        """
-        if self.sheet_tuples:
-            for (sheet, view_name) in self.sheet_tuples:
-                # Do something with the Plot objects.
-                x = 1
-                #NYI
-        else:
-            x = 1
-            #  Do the global name check against the key_name
-            #NYI
 
 
 
