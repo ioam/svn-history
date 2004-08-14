46cd0f0217bbaa9d7e8addfb146ad9ed423f705c
Judah De Paula <judah@cs.utexas.edu>
2004-08-14 12:30:39 +0000

    Changed constructor parameters to compile

--- tests/testplot.py
+++ tests/testplot.py
@@ -1,11 +1,17 @@
 import unittest
+from pprint import pprint
+from topo import plot
+from topo.sheet import Sheet
 
 class TestPlot(unittest.TestCase):
-    def SetUp(self):
-        self.assertEqual(1+1,2)
 
-    def test_add(self):
-        self.assertEqual(1+1,2)
+    def test_plot(self):
+        self.s2 = Sheet()
+        x = plot.Plot(('Activity',None,None),plot.COLORMAP,self.s2)
+        for o in dir():
+            # pprint(o)
+            if isinstance(o,plot.Plot):
+                o.warning('Found ', o.name)
 
 
 
