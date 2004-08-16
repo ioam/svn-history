82dfeccd6b6b3affa82c2a27c314a460177eb228
Judah De Paula <judah@cs.utexas.edu>
2004-08-16 17:09:21 +0000

    End of work day

--- topo/plotengine.py
+++ topo/plotengine.py
@@ -6,17 +6,17 @@ This class is the connector to Simulation
 
 $Id$
 """
+from base import TopoObject
 from plot import Plot
 from plot import PlotGroup
-from topo import base
 
-class PlotEngine(base.TopoObject):
+class PlotEngine(TopoObject):
     """
     Stores the main list of plots available to the simulation.  There are
     some default plot types such as Activity.
     """
 
-    def __init__(self, simulation):
+    def __init__(self, simulation, **params):
         """
         Create a new plot engine that is linked to a particular
         simulation.  The link is necessary since the PlotEngine will
@@ -29,8 +29,46 @@ class PlotEngine(base.TopoObject):
             new_plot_engine = PlotEngine(s)
         QUESTION: PUT A REFERENCE TO PLOT ENGINE WITHIN SIMULATION?
         """
+        super(PlotEngine,self).__init__(**params)
         self.simulation = simulation_name
-        self.figures = []
+        self.plot_group_dict = {}
 
-    
+
+    def _sheets(self):
+        """
+        Get the list of sheets from the event processor list on the
+        local Simulation.
+        """
+        return self.simulation.get_event_processors()
+
+
+    def add_plot_group(self, name, group):
+        """
+        Add a constructed PlotGroup to the local dictionary for later
+        reuse.
+        """
+        self.plot_group_dict[name] = group
+
+
+    def get_plot_group(self, name):
+        """
+        Return the PlotGroup registered in self.plot_group_dict with
+        the provided 'name'.  If the name does not exist, then generate
+        a PlotGroup using the generic dynamic group creator.
+
+        GENERATED PLOTGROUPS WILL POLL ALL SHEETS; NO PARAMETER BASED
+        DEFAULT SHEET GROUPS ARE CURRENTLY IMPLEMENTED BUT IT WOULD BE THE
+        BEST SOLUTION.
+        """
+        self.plot_group_dict.setdefault(name,self._get_sheet_view_plot_group(name,self._sheets()))
         
+
+    def _get_sheet_view_plot_group(self, name, filter_lam):
+        """
+        Default method of creating a plot such as Activity.  Creates
+        a new PlotGroup with a lambda function to poll the latest
+        and greatest SheetView with 'name' from each sheet in 'sheets',
+        using the filter_lambda to choose on inclusion of the sheet.
+        """
+        None
+
