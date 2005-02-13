793f22b4e6f1e4688dacadebcab223cf96350b9c
Judah De Paula <judah@cs.utexas.edu>
2005-02-13 22:36:14 +0000

    Added suite.requires_display example

--- tests/testdummy.py
+++ tests/testdummy.py
@@ -7,4 +7,7 @@ class TestDummy(unittest.TestCase):
 
 
 suite = unittest.TestSuite()
+#  Uncomment the following line of code, to not run the test if
+#  $DISPLAY is undefined.  Used mainly for GUI testing.
+# suite.requires_display = True
 suite.addTest(unittest.makeSuite(TestDummy))
