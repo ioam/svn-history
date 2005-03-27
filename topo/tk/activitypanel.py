d9210980e00fe811018472095cdb80315c08e3e9
Judah De Paula <judah@cs.utexas.edu>
2005-03-27 13:02:31 +0000

    Pulled from plotpanel.py

---
+++ topo/tk/activitypanel.py
@@ -0,0 +1,27 @@
+"""
+ActivityPanel bitmap plot.  Inherits and extends PlotPanel.
+
+$Id$
+"""
+from topo.tk.plotpanel import *
+
+class ActivityPanel(PlotPanel):
+    def __init__(self,parent,pengine=None,console=None,**config):
+        PlotPanel.__init__(self,parent,pengine,console=console,**config)
+        self.plot_key = 'Activation'
+        self.auto_refresh_checkbutton.invoke()
+        self.INITIAL_PLOT_WIDTH = None
+        self.refresh()
+
+
+    def do_plot_cmd(self):
+        """
+        This instantiation polls the PlotEngine for the plotgroup
+        linked to self.plot_key.  Since get_plot_group() is being
+        called, it may update the plots, or it may use a cached
+        version, depending on the type of PlotGroup being requested.
+        """ 
+        self.pe_group = self.pe.get_plot_group(self.plot_key)
+        self.plot_tuples = self.pe_group.plots()
+
+
