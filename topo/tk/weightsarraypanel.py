1d047f6314e5342350c209ee26bf22d8aaa7a63d
Judah De Paula <judah@cs.utexas.edu>
2005-03-28 17:34:16 +0000

    Mid-development cycle: Checked in so current CVS version will build.  TopoConsole now requires a refresh_title() function, that is in this file.  This class has been cleaned up further, and now just needs the 2D plots put it the grid

--- topo/tk/weightsarraypanel.py
+++ topo/tk/weightsarraypanel.py
@@ -3,66 +3,58 @@ WeightsArray Panel for TK GUI visualization.
 
 $Id$
 """
+import __main__
+from topo.tk.regionplotpanel import *
 from topo.tk.plotpanel import *
 
-class WeightsArrayPanel(PlotPanel):
+class WeightsArrayPanel(RegionPlotPanel):
     def __init__(self,parent,pengine,console=None,**config):
+        super(WeightsArrayPanel,self).__init__(parent,pengine,console,**config)
 
-        self.skip_str = StringVar()
-        self.skip_str.set('N/8')
+        self.MIN_PLOT_WIDTH = 20
+        self.INITIAL_PLOT_WIDTH = None
+        self.panel_num = self.console.num_weights_array_windows
 
-        self.situate = StringVar()
-        self.situate.set(0)
-
-        self.region = StringVar()
-        self.region.set('None')
+        self.density_str = StringVar()
+        self.density_str.set('100.0/10.0')
+        self.density = float(eval(self.density_str.get(),__main__.__dict__))
 
         self.weight_name = StringVar()
         self.weight_name.set('None')
 
-        # Used by update_projection_options()
-        self._sim_eps = []
-
-        # Generate the initial plot_key
-        self.generate_plot_key()
-        self.debug('plot_key = ' + str(self.plot_key))
-
-        PlotPanel.__init__(self,parent,pengine,console,**config)
-
         self.params_frame1 = Frame(master=self)
-        self.params_frame1.pack(side=BOTTOM,expand=YES,fill=X)
+        self.params_frame1.pack(side=RIGHT,expand=YES,fill=X)
+        Message(self.params_frame1,text="Units per 1.0:",aspect=1000).pack(side=LEFT)
+        Entry(self.params_frame1,
+              textvariable=self.density_str).pack(side=LEFT,expand=YES,fill=X)
 
-        Message(self.params_frame1,text="Skip:",aspect=1000).pack(side=LEFT)
-        Entry(self.params_frame1,textvariable=self.skip_str).pack(side=LEFT,expand=YES,fill=X)
-        Checkbutton(self.params_frame1,text="Situate",variable=self.situate,
-                    command=self.refresh).pack(side=LEFT)
+        self._add_projection_menu()
 
+        # Situate currently not implemented!  When implemented, should
+        # have each unit plotted within a frame that shows the
+        # sensitivity region on the source Sheet.
+        #
+        # self.situate = StringVar()
+        # self.situate.set(0)
+        # Checkbutton(self.params_frame1,text="Situate",variable=self.situate,
+        #             command=self.refresh).pack(side=LEFT)
+
+
+    def _add_projection_menu(self):
+        """
+        
+        Adds a Projection Widget to th existing frame, and creates the
+        item list for Unit Projection names.  This needs to be
+        changing based on which Region is selected.  See
+        self.region_refresh()
+
+        """
         self.params_frame2 = Frame(master=self)
-        self.params_frame2.pack(side=BOTTOM,expand=YES,fill=X)
+        self.params_frame2.pack(side=LEFT,expand=YES,fill=X)
 
-        # Create the item list for RFSheet 'Region'  This will not change
-        # since this window will only examine one Simulator.
-        sim = console.active_simulator()
-        self._sim_eps = [ep for ep in sim.get_event_processors()
-                  if isinstance(ep,topo.rfsheet.RFSheet)]
-        sim_ep_names = [ep.name for ep in self._sim_eps]
-        if len(sim_ep_names) > 0:
-            self.region.set(sim_ep_names[0])
-
-        Pmw.OptionMenu(self.params_frame2,
-                       command = self._update_projection_options,
-                       labelpos = 'w',
-                       label_text = 'Region:',
-                       menubutton_textvariable = self.region,
-                       items = sim_ep_names
-                       ).pack(side=LEFT)
-
-        # Create the item list for Unit Projection names.  This will
-        # be changing based on which Region is selected.  See
-        # self.update_projection_options()
-        sim_ep = [ep for ep in self._sim_eps
+        self._sim_ep = [ep for ep in self._sim_eps
                   if ep.name == self.region.get()][0]
-        projection_dict = sim_ep.projections
+        projection_dict = self._sim_ep.projections
         projection_list_names = projection_dict.keys()
         if len(projection_list_names) > 0:
             self.weight_name.set(projection_list_names[0])
@@ -73,13 +65,13 @@ class WeightsArrayPanel(PlotPanel):
                        menubutton_textvariable = self.weight_name,
                        items = projection_list_names)
         self.projection_menu.pack(side=LEFT)
+        
 
-
-    def _update_projection_options(self,sheet_name):
+    def region_refresh(self,sheet_name):
         """
-        Create the Projection menu.  This function is only called by the
-        PMW.OptionMenu inside self.params_frame when the Region has been
-        changed.
+        Update the Projection menu.  This overwrites the parent class
+        function RegionPlotPanel.region_refresh() which is called when
+        the Region menu is changed.
         """
         sim_ep = [ep for ep in self._sim_eps if ep.name == sheet_name][0]
         projection_dict = sim_ep.projections
@@ -92,112 +84,81 @@ class WeightsArrayPanel(PlotPanel):
         self.projection_menu.setitems(projection_list_names)
 
 
+    def refresh_title(self):
+        self.parent.title("Weights Array %d. (Region %s, Projection %s, Density %0.2f)" %
+                          (self.panel_num,self.region.get(),
+                           self.weight_name.get(), self.density))
+        
+
     def generate_plot_key(self):
         """
         The plot_key for the WeightsArrayPanel will change depending
         on the input within the window widgets.  This means that the
         key needs to be regenerated at the appropriate times.
 
-        Key Format:  Tuple: ('WeightsArray' ... unknown)
+        Key Format:  Tuple: ('WeightsArray', self.weight_name, self.density)
         """
-        # From WeightsPanel code
-        # g = __main__.__dict__
-        # self.r = eval(self.row_str.get(),g)
-        # self.c = eval(self.col_str.get(),g)
-        # if isinstance(self.r,int): self.r = float(self.r)
-        # if isinstance(self.c,int): self.c = float(self.c)
-        self.plot_key = 'Activation'
-        # self.plot_key = ('WeightsArray',3,3)
+        self.density = float(eval(self.density_str.get(),__main__.__dict__))
+        self.plot_key = ('WeightsArray',self.weight_name.get(),self.density)
 
 
-    def do_plot_cmd(self):
-        Pmw.showbusycursor()
+#     def generate_coords(self, num_points, bbox):
+#         """
+#         Evenly space out the units within the sheet bounding box, so
+#         that it doesn't matter which corner the measurements start
+#         from.  A 4 unit grid needs 5 segments.
+#         """
+#         aarect = bbox.aarect()
+#         (l,b,r,t) = aarect.lbrt()
+#         x = float(r - l)
+#         y = float(b - t)
+#         x_step = x / (num_points + 1)
+#         y_step = y / (num_points + 1)
+#         l = l + x_step
+#         t = t + y_step
+#         coords = []
+#         for j in range(num_points):
+#             y_list = []
+#             for i in range(num_points):
+#                 y_list.append((x_step*j + l, y_step*i + t))
+#             coords.append(y_list)
+#         return coords
+#             
 
-        self.generate_plot_key()
-        self.pe_group = self.pe.get_plot_group(self.plot_key)
-        self.plot_tuples = self.pe_group.plots()
-        self.pe.debug('Type of plot_group', type(self.pe_group))
 
-        self.plotlist = []
-        self.plotlabels = []
-
-        self.pe_group = self.pe.get_plot_group(self.plot_key)
-        self.plot_tuples = self.pe_group.plots()
-        self.pe.debug('Type of plot_group', type(self.pe_group))
-
-#        self.parent.title("Weights Array %d. (r=%0.4f, c=%0.4f)" %
-#                          (self.console.num_weights_windows,self.r, self.c))
-        Pmw.hidebusycursor()
-
-    def load_images(self):
-        # need to calculate the old min width, so we know if we need to reset
-        # the zoom factors
-        if self.images:
-            old_min_width = reduce(min,[im.width() for im in self.images])
-        else:
-            old_min_width = -1
-
-        self.images = []
-        for (figure_tuple, hist_tuple) in self.plot_tuples:
-            (r,g,b) = figure_tuple
-            if r.shape != (0,0) and g.shape != (0,0) and b.shape != (0,0):
-                # Normalize activation to a maximum of 1.  Will scale brighter
-                # or darker, depending.
-                if max(r.flat) > 0: r = Numeric.divide(r,max(r.flat))
-                if max(g.flat) > 0: g = Numeric.divide(g,max(g.flat)) 
-                if max(b.flat) > 0: b = Numeric.divide(b,max(b.flat))
-                win = topo.bitmap.RGBMap(r,g,b)
-                self.images.append(win)
-                self.plotlist.append(win)
-                self.plotlabels.append(self.pe_group.name + ' ' + str(len(self.plotlist)))
-
-        
-        if self.images:
-            min_width = reduce(min,[im.width() for im in self.images])
-        else:
-            min_width = 0
-        
-        if old_min_width != min_width:
-            # if the min width changed, then recalculate the zoom factor
-            # If no plots, then no window.
-            if MIN_PLOT_WIDTH > min_width and old_min_width != -1:
-                self.min_zoom_factor = MIN_PLOT_WIDTH/min_width + 1
-            else:
-                self.min_zoom_factor = 1
-
-            print 'pre zoom_factor', self.zoom_factor
-        
-
-            self.zoom_factor = self.min_zoom_factor
-            print 'post zoom_factor', self.zoom_factor
-            self.reduce_button.config(state=DISABLED)
-
-        
     def display_plots(self):
-        self.zoomed_images = [ImageTk.PhotoImage(im.zoom(self.zoom_factor)) for im in self.images]
+        """
+        PlotPanel will display plots in a single row.  WeightsArrayPanel requires
+        a 2D grid of plots.
+        """
+        # Generate the zoomed images.
+        self.zoomed_images = [ImageTk.PhotoImage(im.zoom(self.zoom_factor))
+                              for im in self.images]
         old_canvases = self.canvases
         self.canvases = [Canvas(self.plot_frame,
                                 width=image.width(),
                                 height=image.height(),
                                 bd=2)
                          for image in self.zoomed_images]
+
+        # Lay out images
         for i,image,canvas in zip(range(len(self.zoomed_images)),
                                   self.zoomed_images,self.canvases):
             canvas.grid(row=0,column=i,padx=5)
             canvas.create_image(image.width()/2,image.height()/2,image=image)
 
+        # Delete old ones.  This may resize the grid.
         for c in old_canvases:
             c.grid_forget()
 
+
     def display_labels(self):
+        """
+        It's not a good idea to show the name of every plot, but it is reasonable
+        to put information on the X and Y axes.
+        """
         for i,name in enum(self.plotlabels):
             self.debug(i, " ", name)
             Label(self.plot_frame,text=name).grid(row=1,column=i,sticky=NSEW)
 
 
-
-
-    def plotname_to_filename(self,plotname):
-        """CURRENTLY NOT USED 3/2005"""
-        return 'gui.'+self.plotname_components(plotname)[0]+'_Weights.ppm'
-####################
