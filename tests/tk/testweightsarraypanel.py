b30f4816c13ce25617f4d49adcc6f6da046e144b
Judah De Paula <judah@cs.utexas.edu>
2005-03-28 17:22:25 +0000

    Pulled from testplotpanel.py, since the classes were pulled from plotpanel.py recently.

---
+++ tests/tk/testweightsarraypanel.py
@@ -0,0 +1,23 @@
+import unittest
+import topo
+import Numeric
+import Tkinter
+from PIL import Image
+from topo.inputsheet import *
+from topo.kernelfactory import *
+from topo.simulator import *
+from topo.sheetview import *
+from topo.plotengine import *
+from topo.tk.activitypanel import *
+
+class TestWeightsArrayPanel(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+suite = unittest.TestSuite()
+#  Uncomment the following line of code, to not run the test if
+#  $DISPLAY is undefined.  Used mainly for GUI testing.
+suite.requires_display = True
+suite.addTest(unittest.makeSuite(TestWeightsArrayPanel))
