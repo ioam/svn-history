4813555ad2af3ca699be64aec57b6d4a0a9d7c5e
Judah De Paula <judah@cs.utexas.edu>
2005-03-28 17:27:42 +0000

    Added refresh_title() and generate_plot_key()

--- topo/tk/activitypanel.py
+++ topo/tk/activitypanel.py
@@ -8,11 +8,20 @@ from topo.tk.plotpanel import *
 class ActivityPanel(PlotPanel):
     def __init__(self,parent,pengine=None,console=None,**config):
         PlotPanel.__init__(self,parent,pengine,console=console,**config)
-        self.plot_key = 'Activation'
         self.auto_refresh_checkbutton.invoke()
         self.INITIAL_PLOT_WIDTH = None
+        self.panel_num = self.console.num_activity_windows
+        self.generate_plot_key()
         self.refresh()
 
+
+    def refresh_title(self):
+        self.parent.title("Activity %d" % self.panel_num)
+
+
+    def generate_plot_key(self):
+        self.plot_key = 'Activation'
+    
 
     def do_plot_cmd(self):
         """
