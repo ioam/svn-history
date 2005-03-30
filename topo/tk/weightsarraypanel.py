2d3b30672853d64c0468ce909b234608fbe896ba
Judah De Paula <judah@cs.utexas.edu>
2005-03-30 09:28:43 +0000

    The UnitViews are now shown in a 2D grid, and the source projection sheet is now named

--- topo/tk/weightsarraypanel.py
+++ topo/tk/weightsarraypanel.py
@@ -6,27 +6,31 @@ $Id$
 import __main__
 from topo.tk.regionplotpanel import *
 from topo.tk.plotpanel import *
+from itertools import chain
 
 class WeightsArrayPanel(RegionPlotPanel):
     def __init__(self,parent,pengine,console=None,**config):
         super(WeightsArrayPanel,self).__init__(parent,pengine,console,**config)
 
-        self.MIN_PLOT_WIDTH = 20
-        self.INITIAL_PLOT_WIDTH = None
+        self.MIN_PLOT_WIDTH = 5
+        self.INITIAL_PLOT_WIDTH = 20
         self.panel_num = self.console.num_weights_array_windows
 
         self.density_str = StringVar()
         self.density_str.set('100.0/10.0')
         self.density = float(eval(self.density_str.get(),__main__.__dict__))
+        self.shape = (0,0)
 
         self.weight_name = StringVar()
         self.weight_name.set('None')
+        self.projections = {}
+
 
         self.params_frame1 = Frame(master=self)
         self.params_frame1.pack(side=RIGHT,expand=YES,fill=X)
         Message(self.params_frame1,text="Units per 1.0:",aspect=1000).pack(side=LEFT)
         Entry(self.params_frame1,
-              textvariable=self.density_str).pack(side=LEFT,expand=YES,fill=X)
+              textvariable=self.density_str).pack(side=LEFT,expand=YES,fill=X,padx=5)
 
         self._add_projection_menu()
 
@@ -39,55 +43,83 @@ class WeightsArrayPanel(RegionPlotPanel):
         # Checkbutton(self.params_frame1,text="Situate",variable=self.situate,
         #             command=self.refresh).pack(side=LEFT)
 
+        self.refresh()
+
+
+    def _create_projection_dict(self,sheet_name):
+        """
+        PRE: Each Projection in the RFSheet should have a unique name.
+        If there are two Projections with the same name, then even
+        though the list will show two entries with the same name, only
+        one Projection object will be accessible.
+
+        POST: self.projections dictionary has been populated with the
+        list of projections for the active Region name in self.region.
+        
+        Both _add_projection_menu(), and refresh_region() need to
+        create a dictionary with the Projection Name as key, and the
+        Projection object as value, so this is its own function since
+        it needs to be done the same way in both places.
+        """
+        self._sim_ep = [ep for ep in self._sim_eps
+                  if ep.name == sheet_name][0]
+        self.projections = dict([(i.name, i) for i in
+                                 chain(*self._sim_ep.projections.values())])
+
+        old_projection_name = self.weight_name.get()
+        if len(self.projections.keys()) == 0:
+            self.weight_name.set('None')
+        elif old_projection_name not in self.projections.keys():
+            self.weight_name.set(self.projections.keys()[0])
+
 
     def _add_projection_menu(self):
         """
-        
         Adds a Projection Widget to th existing frame, and creates the
         item list for Unit Projection names.  This needs to be
         changing based on which Region is selected.  See
-        self.region_refresh()
-
+        self.region_refresh() 
         """
         self.params_frame2 = Frame(master=self)
         self.params_frame2.pack(side=LEFT,expand=YES,fill=X)
 
-        self._sim_ep = [ep for ep in self._sim_eps
-                  if ep.name == self.region.get()][0]
-        projection_dict = self._sim_ep.projections
-        projection_list_names = projection_dict.keys()
-        if len(projection_list_names) > 0:
-            self.weight_name.set(projection_list_names[0])
+        self._create_projection_dict(self.region.get())
 
         self.projection_menu = Pmw.OptionMenu(self.params_frame2,
+                       command = self.projection_refresh,
                        labelpos = 'w',
                        label_text = 'Projection:',
                        menubutton_textvariable = self.weight_name,
-                       items = projection_list_names)
+                       items = self.projections.keys())
         self.projection_menu.pack(side=LEFT)
-        
 
+
+    def projection_refresh(self,projection_name):
+        """
+        Called by the Pmw.OptionMenu when the menubutton_textvariable
+        is updated.  The do_plot_cmd() already polls the correct
+        Projection but this will force a refresh() if auto_refresh if
+        it is enabled.
+        """
+        if self.auto_refresh:
+            self.refresh()
+        
+        
     def region_refresh(self,sheet_name):
         """
         Update the Projection menu.  This overwrites the parent class
         function RegionPlotPanel.region_refresh() which is called when
-        the Region menu is changed.
+        the Region Widget menu is changed.
         """
-        sim_ep = [ep for ep in self._sim_eps if ep.name == sheet_name][0]
-        projection_dict = sim_ep.projections
-        projection_list_names = projection_dict.keys()
-        old_projection_name = self.weight_name.get()
-        if len(projection_list_names) == 0:
-            self.weight_name.set('None')
-        elif old_projection_name not in projection_list_names:
-            self.weight_name.set(projection_list_names[0])
-        self.projection_menu.setitems(projection_list_names)
+        self._create_projection_dict(sheet_name)
+        self.projection_menu.setitems(self.projections.keys())
+        if self.auto_refresh:
+            self.refresh()
 
 
     def refresh_title(self):
-        self.parent.title("Weights Array %d. (Region %s, Projection %s, Density %0.2f)" %
-                          (self.panel_num,self.region.get(),
-                           self.weight_name.get(), self.density))
+        self.parent.title("Weights Array %d. (Region %s, Projection %s, Density %0.2f)" % (self.panel_num,self.region.get(),
+            self.weight_name.get(), self.density))
         
 
     def generate_plot_key(self):
@@ -102,28 +134,49 @@ class WeightsArrayPanel(RegionPlotPanel):
         self.plot_key = ('WeightsArray',self.weight_name.get(),self.density)
 
 
-#     def generate_coords(self, num_points, bbox):
-#         """
-#         Evenly space out the units within the sheet bounding box, so
-#         that it doesn't matter which corner the measurements start
-#         from.  A 4 unit grid needs 5 segments.
-#         """
-#         aarect = bbox.aarect()
-#         (l,b,r,t) = aarect.lbrt()
-#         x = float(r - l)
-#         y = float(b - t)
-#         x_step = x / (num_points + 1)
-#         y_step = y / (num_points + 1)
-#         l = l + x_step
-#         t = t + y_step
-#         coords = []
-#         for j in range(num_points):
-#             y_list = []
-#             for i in range(num_points):
-#                 y_list.append((x_step*j + l, y_step*i + t))
-#             coords.append(y_list)
-#         return coords
-#             
+    def do_plot_cmd(self):
+        """
+        The WeightsArrayPanel will calculate the number of plots to
+        show for the active Sheet and Projection.  It will generate
+        multiple unit_view() requests; keep the Projections desired,
+        and then store the composite list back into the Sheet under a
+        key that other functions in this class will use to display.
+        """
+        self.generate_plot_key()
+        coords = self.generate_coords()
+        
+        full_unitview_list = [self._sim_ep.unit_view(x,y)
+                         for (x,y) in coords]
+        filtered_list = [view for view in chain(*full_unitview_list)
+                         if view.projection == self.projections[self.weight_name.get()]]
+        self._sim_ep.add_sheet_view(self.plot_key,filtered_list)
+
+        super(WeightsArrayPanel,self).do_plot_cmd()
+
+        for (x,y) in coords: self._sim_ep.release_unit_view(x,y)
+        
+
+    def generate_coords(self):
+        """
+        Evenly space out the units within the sheet bounding box, so
+        that it doesn't matter which corner the measurements start
+        from.  A 4 unit grid needs 5 segments.
+        """
+        aarect = self._sim_ep.bounds.aarect()
+        (l,b,r,t) = aarect.lbrt()
+        x = float(r - l) 
+        y = float(t - b)
+        x_step = x / (int(x * self.density) + 1)
+        y_step = y / (int(y * self.density) + 1)
+        l = l + x_step
+        b = b + y_step
+        coords = []
+        self.shape = (int(x * self.density), int(y * self.density))
+        for j in range(self.shape[1]):
+            for i in range(self.shape[0]):
+                coords.append((x_step*j + l, y_step*i + b))
+        return coords
+            
 
 
     def display_plots(self):
@@ -144,7 +197,8 @@ class WeightsArrayPanel(RegionPlotPanel):
         # Lay out images
         for i,image,canvas in zip(range(len(self.zoomed_images)),
                                   self.zoomed_images,self.canvases):
-            canvas.grid(row=0,column=i,padx=5)
+            canvas.grid(row=i%self.shape[0],column=i//self.shape[1],
+                        padx=1,pady=1)
             canvas.create_image(image.width()/2,image.height()/2,image=image)
 
         # Delete old ones.  This may resize the grid.
@@ -154,11 +208,16 @@ class WeightsArrayPanel(RegionPlotPanel):
 
     def display_labels(self):
         """
-        It's not a good idea to show the name of every plot, but it is reasonable
-        to put information on the X and Y axes.
+        It's not a good idea to show the name of every plot, but it is
+        reasonable to put information on the X and Y axes.
         """
-        for i,name in enum(self.plotlabels):
-            self.debug(i, " ", name)
-            Label(self.plot_frame,text=name).grid(row=1,column=i,sticky=NSEW)
+        if len(self.projections) > 0:
+            src_name = self.projections[self.weight_name.get()].src.name
+            new_title = 'Projections from ' + src_name
+            self.plot_group.configure(tag_text = new_title)
+        else:
+            self.plot_group.configure(tag_text = 'No Projections')
+        
+
 
 
