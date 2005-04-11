5b4f0d5531f7f8d4890cc94339762c20c523a9d8
Judah De Paula <judah@cs.utexas.edu>
2005-04-11 05:18:11 +0000

    Added accessor and mutator for input_generator

--- topo/inputsheet.py
+++ topo/inputsheet.py
@@ -23,8 +23,29 @@ class InputSheet(Sheet):
     
     def __init__(self,**params):
         super(InputSheet,self).__init__(**params)
+        self.init_input_generator(self.input_generator)
+
+
+    def init_input_generator(self,new_ig):
+        """
+        Allow assignment of a new input generator to this InputSheet.
+        Only one generator is linked at a time, so if more than one is
+        going to be used, the code driving the sheet needs to keep
+        tabs on which one is being used.
+        """
+        self.input_generator = new_ig
         self.input_generator.bounds = self.bounds
         self.input_generator.density = self.density
+        
+
+    def get_input_generator(self):
+        """
+        Return the existing input_generator Parameter.  If a temporary
+        input generator is going to be used, then this function will
+        be needed so that the old generator can be replaced when done.
+        """
+        return input_generator
+    
         
     def start(self):
         assert self.simulator
