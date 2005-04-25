69f260d2acbe72f90f785db79e4d23138c6cd854
Judah De Paula <judah@cs.utexas.edu>
2005-04-25 01:36:05 +0000

    Tweaked default values of Sliders

--- topo/tk/inputparamspanel.py
+++ topo/tk/inputparamspanel.py
@@ -138,23 +138,23 @@ class InputParamsPanel(PlotPanel):
         self.tparams['x'] = \
           self.add_slider( 'x'   ,        "-1"     ,   "1"      ,  "0"      )
         self.tparams['y'] = \
-          self.add_slider( 'y'   ,        "0"      ,  "1"       , "0"       )
+          self.add_slider( 'y'   ,        "-1"      ,  "1"       , "0"       )
         self.tparams['min'] = \
           self.add_slider( 'min' ,        "0"      ,  "1"       , "0"       )
         self.tparams['max'] = \
           self.add_slider( 'max'  ,       "0"      ,  "1"       , "1"       )
         self.tparams['width'] = \
-          self.add_slider( 'width',       "0.001"  ,  "1"       , "0.5"     )
+          self.add_slider( 'width',       "0.000001", "1"       , "0.2"     )
         self.tparams['height'] = \
-          self.add_slider( 'height',      "0.001"  ,  "1"       , "0.5"     )
+          self.add_slider( 'height',      "0.000001", "1"       , "0.2"     )
         self.tparams['frequency'] = \
-          self.add_slider( 'frequency',   "0.01"  ,   "1.25"   ,  "0.5"     )
+          self.add_slider( 'frequency',   "0.01"  ,   "1.25"   ,  "1.5"     )
         self.tparams['phase'] = \
           self.add_slider( 'phase',        "0"     ,   "PI*2"   ,  "PI/2"   )
         self.tparams['disk_radius'] = \
-          self.add_slider( 'disk_radius',  "0"     ,   "1"      ,  "0.8"    )
+          self.add_slider( 'disk_radius',  "0"     ,   "1"      ,  "0.2"    )
         self.tparams['gaussian_width'] = \
-          self.add_slider( 'gaussian_width',"0.001",   "1"      ,  "1"      )
+          self.add_slider( 'gaussian_width',"0.000001","1"      ,  "0.15"      )
 
 # NOT IN EXISTING KERNELFACTORIES 4/2005
 #        self.tparams['xsigma'] = \
@@ -252,7 +252,7 @@ class InputParamsPanel(PlotPanel):
 
     def add_slider(self,name,min,max,init):
         return self.prop_frame.add_tagged_slider_property(name,init,
-                 min_value=min,max_value=max,width=30,string_format='%.3f',
+                 min_value=min,max_value=max,width=30,string_format='%.6f',
                  string_translator=eval_atof)
 
 
