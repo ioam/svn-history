6b42c01134929e96c2411ed105dbaad3642ac85d
Judah De Paula <judah@cs.utexas.edu>
2005-05-01 03:05:44 +0000

    Added additional tests for state_push and state_pop

--- tests/testsimulator.py
+++ tests/testsimulator.py
@@ -1,10 +1,40 @@
 import unittest
+import copy
 
+from Numeric import array
 from topo.simulator import *
 from topo.scalarep import *
 
 class TestSimulator(unittest.TestCase):
-    def test_get_event_processors(self,):
+    def test_event_copy(self):
+        """
+        Test to make sure a copy of an Event works, which is pointers to
+        all the sheets, but a deepcopy of the data.  The data is probably
+        an array, but it's not required.
+        """
+        s = Simulator(step_mode = 1)
+        data = array([4,3])
+        se = Simulator.Event(1,2,3,4,5,data)
+        se2 = copy(se)
+        assert se != se2, 'Objects are the same'
+        se2.data[0] = 5
+        assert se.data[0] != se2.data[0], 'Matrices should be different'
+
+    def test_state_stack(self):
+        s = Simulator(step_mode = 1)
+        pulse1 = PulseGenerator(period = 1)
+        pulse2 = PulseGenerator(period = 3)
+        sum = SumUnit()
+        s.connect(pulse1,sum,delay=1)
+        s.connect(pulse2,sum,delay=1)
+        s.run(1.0)
+        s.state_push()
+        self.assertEqual(s.state_len(),1)
+        s.state_pop()
+        self.assertEqual(s.state_len(),0)
+        
+
+    def test_get_event_processors(self):
         s = Simulator(step_mode = 1)
 
         pulse1 = PulseGenerator(period = 1)
