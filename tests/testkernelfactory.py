3d2a84b2752ad618e791b2502538c0a8562f4919
Judah De Paula <judah@cs.utexas.edu>
2005-05-13 07:43:08 +0000

    Added orientation test

--- tests/testkernelfactory.py
+++ tests/testkernelfactory.py
@@ -4,8 +4,9 @@ Created to test the kernel input orientations.
 $Id$
 """
 import unittest
-import topo.kernelfactory
+from topo.kernelfactory import *
 from MLab import flipud, rot90
+from Numeric import *
 
 class TestKernelFactory(unittest.TestCase):
 
@@ -13,12 +14,20 @@ class TestKernelFactory(unittest.TestCase):
         """
         Create a kernel, and test the orientation.
         """
-        rect = topo.kernelfactory.RectangleFactory(x=-0.1,y=0.1,width=0.25,height=1.0,density=100)
-        # print rect()
-        # print 'rotated'
-        # print flipud(rot90(rect()))
-        # rb = rect.bounds.aarect()
-        # print rb.lbrt()
+        target = array([[0, 0, 0, 0, 0, 0],
+                        [0, 0, 1, 1, 0, 0],
+                        [0, 0, 1, 1, 0, 0],
+                        [0, 0, 1, 1, 0, 0],
+                        [0, 0, 1, 1, 0, 0],
+                        [0, 0, 0, 0, 0, 0]])
+        bounds = BoundingBox(points=((-0.8,-0.8),(0.8,0.8)))
+        density = 16**2
+        rect = RectangleFactory(width=0.3,height=0.9,
+                                density=4**2,bounds=bounds)
+        self.assertEqual(rect(),target)
+#        print rect()
+        
+
 
 suite = unittest.TestSuite()
 #  Uncomment the following line of code, to disable the test if
