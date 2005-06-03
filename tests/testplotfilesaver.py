d6c96eb70a0620244a5b9f474edba31b060338c2
Judah De Paula <judah@cs.utexas.edu>
2005-06-03 22:34:13 +0000

    Added skeleton functionality for plot file saving.

--- tests/testplotfilesaver.py
+++ tests/testplotfilesaver.py
@@ -5,15 +5,18 @@ from topo.sheet import *
 from topo.bitmap import RGBMap
 from topo.image import ImageGenerator
 from topo.inputsheet import *
-import Numeric
-import topo.kernelfactory
+import Numeric, random, os
+from math import pi
+from topo.kernelfactory import FuzzyLineFactory
 import topo.base
+import topo.cfsom
+import topo.cfsheet
 from topo.plotfilesaver import *
 from PIL import *
 
 class TestPlotFileSaver(unittest.TestCase):
 
-    def setup(self):
+    def test_file_saving(self):
         InputSheet.period = 1.0
         InputSheet.density = 400
         FuzzyLineFactory.x = Dynamic(lambda : random.uniform(-0.5,0.5))
@@ -21,33 +24,35 @@ class TestPlotFileSaver(unittest.TestCase):
         FuzzyLineFactory.theta = Dynamic(lambda :random.uniform(-pi,pi))
         FuzzyLineFactory.width = 0.02
         FuzzyLineFactory.bounds = BoundingBox(points=((-0.8,-0.8),(0.8,0.8)))
-        CFSOM.density = 100
-        CFSOM.learning_length = 10000
-        CFSOM.radius_0 = 0.1
-        KernelProjection.weights_factory = UniformRandomFactory(bounds=BoundingBox(points=((-0.1,-0.1),(0.1,0.1))))
+        topo.cfsom.CFSOM.density = 100
+        topo.cfsom.CFSOM.learning_length = 10000
+        topo.cfsom.CFSOM.radius_0 = 0.1
+        topo.cfsheet.KernelProjection.weights_factory = UniformRandomFactory(bounds=BoundingBox(points=((-0.1,-0.1),(0.1,0.1))))
         
-        base.min_print_level = base.MESSAGE
-        s = topo.simulator.Simulator()
+        topo.base.min_print_level = topo.base.WARNING
+        self.s = topo.simulator.Simulator()
         
         retina = InputSheet(input_generator=FuzzyLineFactory(),name='Retina')
         retina2 = InputSheet(input_generator=FuzzyLineFactory(),name='Retina2')
-        V1 = CFSOM(name='V1')
-        V2 = CFSOM(name='V2')
-        save  = ImageSaver(name='CFSOM')
+        V1 = topo.cfsom.CFSOM(name='V1')
+        V2 = topo.cfsom.CFSOM(name='V2')
+        retina.print_level = topo.base.WARNING
+        retina2.print_level = topo.base.WARNING
+        V1.print_level = topo.base.WARNING
+        V2.print_level = topo.base.WARNING
         
-        s.connect(retina,V1,delay=0.5,projection_params={'name':'R1toV1'})
-        s.connect(retina,V2,delay=0.5,projection_params={'name':'R1toV2'})
-        s.connect(retina2,V2,delay=0.5,projection_params={'name':'R2toV2'})
+        self.s.connect(retina,V1,delay=0.5,projection_params={'name':'R1toV1'})
+        self.s.connect(retina,V2,delay=0.5,projection_params={'name':'R1toV2'})
+        self.s.connect(retina2,V2,delay=0.5,projection_params={'name':'R2toV2'})
         self.pe = topo.plotengine.PlotEngine(self.s)
-        s.run(2)
-        
+        self.s.run(2)
 
-    def test_activity_file(self):
-        af = PlotFileSaver('Activation')
-
-
-#    def test_weights_file(self):
-#        wf = PlotFileSaver('Weights',x=0.0,y=0.0,
+        af = ActivationFile('Retina')
+        wf = WeightsFile('V1',0.0,0.0)
+        waf = WeightsArrayFile('V1','R1toV1',10)
+#        for each in af.files + wf.files + waf.files:
+#            os.remove(each)
+#
 
 
 suite = unittest.TestSuite()
--- topo/plotfilesaver.py
+++ topo/plotfilesaver.py
@@ -14,41 +14,83 @@ import topo.plotengine
 from topo.utils import *
 
 class PlotFileSaver(topo.base.TopoObject):
-    def __init__(self,plot_type='Activation',**config):
+    def __init__(self,**config):
         super(PlotFileSaver,self).__init__(**config)
-        self.pe = topo.plotengine.PlotEngine(topo.simulator.active_sim())
-        self.name_prefix = ''
+        self.sim = topo.simulator.active_sim()
+        self.pe = topo.plotengine.PlotEngine(self.sim)
         self.bitmaps = []
+        self.files = []
+        self.name = {'base':self.sim.name, 'iteration':self.sim.time(), \
+                    'presentation':'0', 'region':'', 'type':''}
 
-        if plot_type == 'Activation':
-            self.activation_save()
-        elif plot_type == 'Weights':
-            self.weights_save(config['x'],config['y'],sheetname)
-        elif plot_type == 'WeightsArray':
-            self.weightsarray_save()
-        else:
-            self.warning('Unknown plot type to save.')
-        self.save_to_disk()
-
+    def create_bitmaps(self):
+        pass
 
     def save_to_disk(self):
         if self.bitmaps:
             for i in range(len(self.bitmaps)):
-                self.bitmaps[i].bitmap.save(self.name_prefix + str(i)+'.jpg')
-                self.message('Saving', str(i) + '.jpg')
+                d = self.name
+                filename = '%s.%06d.p%03d.%s_%s_%d.jpg' % \
+                           (d['base'], int(d['iteration']), \
+                           int(d['presentation']), d['region'], d['type'],i)
+                           
+                #self.message('Saving', filename)
+                self.bitmaps[i].bitmap.save(filename)
+                self.files.append(filename)
 
-    def activation_save(self):
-        pg = self.pe.get_plot_group('Activation','ActivationPlotGroup')
-        self.name_prefix = 'activation_'
-        self.bitmaps = pg.load_images()
 
-    def weights_save(self,x,y,sheetname):
-        pg = self.pe.get_plot_group(('Weights',str(x),str(y)),
-                                    'WeightsPlotGroup',
-                                    sheetname)
-        self.name_prefix = 'Weights_'
+
+class ActivationFile(PlotFileSaver):
+    def __init__(self,region,**config):
+        super(ActivationFile,self).__init__(**config)
+        self.region = region
+        self.name['region'] = region
+        self.name['type'] = 'Activity'
+        self.create_bitmaps()
+        self.save_to_disk()
+
+    def create_bitmaps(self):
+        pg = self.pe.get_plot_group('Activation','ActivationPlotGroup',
+                                    self.region)
         self.bitmaps = pg.load_images()
         
 
-    def weightsarray_save(self):
-        pass
+
+class WeightsFile(PlotFileSaver):
+    def __init__(self,region,x,y,**config):
+        super(WeightsFile,self).__init__(**config)
+        self.region = region
+        self.name['region'] = '%s_%01.03f_%01.03f' % (region, x, y)
+        self.name['type'] = 'Weights'
+        self.plot_key = ('Weights',x,y)
+        self.create_bitmaps()
+        self.save_to_disk()
+
+    def create_bitmaps(self):
+        pg = self.pe.get_plot_group(self.plot_key,
+                                    'WeightsPlotGroup',
+                                    self.region)
+        self.bitmaps = pg.load_images()
+
+
+
+class WeightsArrayFile(PlotFileSaver):
+    def __init__(self,region,projection,density,**config):
+        super(WeightsArrayFile,self).__init__(**config)
+        self.region = region
+        self.name['region'] = '%s_%s' % (region, projection)
+        self.name['type'] = 'WeightsArray'
+        self.plot_key = ('WeightsArray',projection,density)
+        self.create_bitmaps()
+        self.save_to_disk()
+
+
+    def create_bitmaps(self):
+        pg = self.pe.get_plot_group(self.plot_key,
+                                    'WeightsArrayPlotGroup',
+                                    self.region)
+        pg.do_plot_cmd()
+        self.bitmaps = pg.load_images()
+
+
+
