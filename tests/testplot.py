888e38ba7823b3d1b5c76b3544077663d16cd4e6
Judah De Paula <judah@cs.utexas.edu>
2005-07-17 20:50:06 +0000

    Added test for SCC Plots

--- tests/testplot.py
+++ tests/testplot.py
@@ -2,7 +2,7 @@ import unittest
 from pprint import pprint
 from topo import plot
 from topo.sheet import *
-from topo.bitmap import RGBMap
+from topo.bitmap import RGBMap, HSVMap
 from topo.image import ImageGenerator
 import Numeric
 from Numeric import divide
@@ -29,6 +29,20 @@ class TestPlot(unittest.TestCase):
         p_tuple = plot1.plot()
         (r, g, b) = p_tuple.matrices
         map = RGBMap(r,g,b)
+        if SHOW_PLOTS: map.show()
+
+
+    def test_HSV_plot(self):
+        input = ImageGenerator(filename='tests/testsheetview.ppm',
+                         density=10000,
+                         bounds=BoundingBox(points=((-0.8,-0.8),(0.8,0.8))))
+        sv = input.sheet_view('Activation')
+
+        # Defined sheetview in the R channel
+        plot1 = plot.Plot((sv,sv,sv),plot.HSV)
+        p_tuple = plot1.plot()
+        (r, g, b) = p_tuple.matrices
+        map = HSVMap(r,g,b)
         if SHOW_PLOTS: map.show()
 
 
