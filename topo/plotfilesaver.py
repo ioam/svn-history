466b5a64e169b585daed0fb0595147cb94814d12
Judah De Paula <judah@cs.utexas.edu>
2005-09-02 10:01:59 +0000

    Updated file saving classes to use the PlotTemplates instead of the original mechanism.

--- topo/plotfilesaver.py
+++ topo/plotfilesaver.py
@@ -4,7 +4,9 @@ File saving routines for plots.
 STATUS 6/18/05: Little more than a stub, with one class that calls a
 subroutine depending on the type of plot requested.  Not yet clear if
 this class should be desined with the command-line interface in mind
-(IE: minimal typing, function parameters, etc.)
+(IE: minimal typing, function parameters, etc.)  Additional: With the
+new Plot Template mechanism the plot_keys are not as critical as they
+used to be.
 
 $Id$
 """
@@ -50,44 +52,55 @@ class ActivationFile(PlotFileSaver):
         self.save_to_disk()
 
     def create_bitmaps(self):
-        pg = self.pe.get_plot_group('Activation','BasicPlotGroup',
+        pg = self.pe.get_plot_group('Activation',
+                                    topo.plotengine.plotgroup_templates['Activity'],
                                     self.region)
         self.bitmaps = pg.load_images()
         
 
 
-class WeightsFile(PlotFileSaver):
+class UnitWeightsFile(PlotFileSaver):
     def __init__(self,region,x,y,**config):
-        super(WeightsFile,self).__init__(**config)
+        super(UnitWeightsFile,self).__init__(**config)
         self.region = region
         self.name['region'] = '%s_%01.03f_%01.03f' % (region, x, y)
         self.name['type'] = 'Weights'
         self.plot_key = ('Weights',self.region,x,y)
+
+        pt = topo.plotengine.plotgroup_templates['Unit Weights'].plot_templates['Unit Weights']
+        pt.channels['Sheet_name'] = region
+        pt.channels['Location'] = (x, y)
+
         self.create_bitmaps()
         self.save_to_disk()
 
     def create_bitmaps(self):
         pg = self.pe.get_plot_group(self.plot_key,
-                                    'WeightsPlotGroup',
+                                    topo.plotengine.plotgroup_templates['Unit Weights'],
                                     self.region)
         self.bitmaps = pg.load_images()
 
 
 
-class WeightsArrayFile(PlotFileSaver):
+class ProjectionFile(PlotFileSaver):
     def __init__(self,region,projection,density,**config):
-        super(WeightsArrayFile,self).__init__(**config)
+        super(ProjectionFile,self).__init__(**config)
         self.region = region
         self.name['region'] = '%s_%s' % (region, projection)
         self.name['type'] = 'WeightsArray'
         self.plot_key = ('WeightsArray',projection,density)
+
+        pt = topo.plotengine.plotgroup_templates['Projection'].plot_templates['Projection']
+        pt.channels['Density'] = density
+        pt.channels['Projection_name'] = region
+
         self.create_bitmaps()
         self.save_to_disk()
 
 
     def create_bitmaps(self):
         pg = self.pe.get_plot_group(self.plot_key,
-                                    'WeightsArrayPlotGroup',
+                                    topo.plotengine.plotgroup_templates['Projection'],
                                     self.region)
         pg.do_plot_cmd()
         self.bitmaps = pg.load_images()
