05011448a7f3f1d76d69043019100e32f76b4781
James A. Bednar <jbednar@inf.ed.ac.uk>
2005-10-01 05:25:07 +0000

    Simplified by eliminating extra function

--- topo/patterns/random.py
+++ topo/patterns/random.py
@@ -3,13 +3,6 @@ from topo.kernelfactory import KernelFactory
 import RandomArray
 
 
-def uniform_random(kernel_x, kernel_y,rmin,rmax):
-    """
-    Uniform random noise, independent for each pixel.
-    """
-    return RandomArray.uniform(rmin,rmax,kernel_x.shape) 
-
-
 class UniformRandomFactory(KernelFactory):
     """
     Uniform random noise pattern generator
@@ -20,6 +13,6 @@ class UniformRandomFactory(KernelFactory):
     max     = Number(default=1.0,bounds=(0.0,1.0),softbounds=(0.0,1.0))
     
     def function(self,**params):
-        return uniform_random( self.kernel_x, self.kernel_y,
-                               params.get('min',self.min),
-                               params.get('max',self.max)) 
+        return RandomArray.uniform( params.get('min',self.min),
+                                    params.get('max',self.max),
+                                    self.kernel_x.shape) 
