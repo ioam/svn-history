8ac1169498f918c662ff1ecaf6353d2bd6adc9b2
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2005-10-02 11:40:28 +0000

    Initial version

---
+++ topo/projections/__init__.py
@@ -0,0 +1,6 @@
+# $Id$
+
+# Automatically discover all .py files in this directory. 
+import re,os
+__all__ = [re.sub('\.py$','',f) for f in os.listdir(__path__[0])
+           if re.match('^[^_].*\.py$',f)]
