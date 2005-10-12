153b85f80a4c9a2e2b49eb5737ec1e5eb1046e42
James A. Bednar <jbednar@inf.ed.ac.uk>
2005-10-12 16:20:42 +0000

    Added comment

--- topo/base/__init__.py
+++ topo/base/__init__.py
@@ -1,6 +1,12 @@
 """
 Basic files needed by all Topographica programs.
 
+This package should be self-contained, i.e., should not refer to any
+other part of Topographica.  For instance, no file may include an
+import statement like 'from topo.package.module import' or 'import
+topo.package.module'.  This ensures that all of the packages outside
+of this one are optional.
+
 $Id$
 """
 
