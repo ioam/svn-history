4c0fe4e6df167180c8b7d347745fb54f1fd2dbf5
James A. Bednar <jbednar@inf.ed.ac.uk>
2005-10-14 22:35:11 +0000

    Removed unused compute_response functions

--- topo/base/utils.py
+++ topo/base/utils.py
@@ -7,7 +7,6 @@ $Id$
 
 from Numeric import sqrt,ones,dot,sum
 import __main__
-import weave
 import math
 
 inf = (ones(1)/0.0)[0]
@@ -151,80 +150,6 @@ def flatten(l):
                     stack.append((sequence, j+1))
                     sequence, j = sequence[j], 0
         return result
-
-
-def compute_response_mdot_py(input_activity, rows, cols, activity, cfs, strength):
-    """
-    The original mdot function that the inline C code based on. It is here just
-    for reference.
-    """
-
-    for r in xrange(rows):
-        for c in xrange(cols):
-            cf = cfs[r][c]
-            r1,r2,c1,c2 = cf.slice
-            X = input_activity[r1:r2,c1:c2]
-
-            a = X*cf.weights
-            activity[r,c] = sum(a.flat)
-    activity *= strength
-
-
-def compute_response_mdot_c(input_activity, rows, cols, activity, cfs, strength):
-    """
-    An optmized version that computes the mdot functions for all the conection
-    fields with the input_activity. It loops through each unit in the sheet
-    and therefore the loop in KernelProjection.activate is not needed.
-    """
-
-    temp_act = activity
-    len, len2 = input_activity.shape
-    X = input_activity.flat
-
-    code = """
-        double tot;
-        float *wi; 
-        double *xi, *xj;
-        double *tact = temp_act;
-        int *slice;
-        int rr1, rr2, cc1, cc2;
-        int i, j, r, l;
-        PyObject *cf, *cfsr;
-        PyObject *sarray = PyString_FromString("slice_array");
-        PyObject *weights = PyString_FromString("weights");
-
-        for (r=0; r<rows; ++r) {
-            cfsr = PyList_GetItem(cfs,r);
-            for (l=0; l<cols; ++l) {
-                cf = PyList_GetItem(cfsr,l);
-                wi = (float *)(((PyArrayObject*)PyObject_GetAttr(cf,weights))->data);
-                slice = (int *)(((PyArrayObject*)PyObject_GetAttr(cf,sarray))->data);
-                rr1 = *slice++;
-                rr2 = *slice++;
-                cc1 = *slice++;
-                cc2 = *slice;
-
-                tot = 0.0;
-
-                // computes the dot product
-                xj = X+len*rr1+cc1;
-                for (i=rr1; i<rr2; ++i) {
-                    xi = xj;
-                    for (j=cc1; j<cc2; ++j) {
-                        tot += *wi * *xi;
-                        ++wi;
-                        ++xi;
-                    }
-                    xj += len;
-                }
-
-                *tact = tot*strength;
-                ++tact;
-            }
-        }
-    """
-
-    weave.inline(code, ['X', 'strength', 'len', 'temp_act','cfs','cols','rows'])
 
 
 def eval_atof(in_string,default_val = 0):
