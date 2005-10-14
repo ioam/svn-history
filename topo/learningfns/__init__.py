1fea813cb3b4666b3d0461e36c229c83786bb0d1
James A. Bednar <jbednar@inf.ed.ac.uk>
2005-10-14 22:46:04 +0000

    Initial version

---
+++ topo/learningfns/__init__.py
@@ -0,0 +1,16 @@
+"""
+A family of learning functions for CFProjections.
+
+These function objects compute a new set of ConnectionFields when given an input
+and output pattern and a set of ConnectionField objects.
+
+Any new learning functions added to this directory will automatically
+become available for any model.
+
+$Id$
+"""
+
+# Automatically discover all .py files in this directory. 
+import re,os
+__all__ = [re.sub('\.py$','',f) for f in os.listdir(__path__[0])
+           if re.match('^[^_].*\.py$',f)]
