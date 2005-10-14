cae1b13d4b782cdf25d6d6399776dcc5907b34f1
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2005-10-14 17:39:49 +0000

    continued producing initial version

---
+++ tests/testfeaturemap.py
@@ -0,0 +1,121 @@
+"""
+Tests of featuremap.py
+
+$Id$
+"""
+
+# CEB:
+# This file is still being written
+
+# No test for relative_selectivity(), vector_selectivity()
+
+
+import unittest
+
+from topo.base.featuremap import FeatureMap
+
+
+from topo.base.utils import arg
+from math import pi
+from Numeric import array, exp
+
+class TestFeatureMap(unittest.TestCase):
+
+    def setUp(self):
+
+        self.a1 = array([[1,1], [1,1], [1,1]])
+        
+        # object to test for non-cyclic, non-keep_peak distributions
+        
+        self.fm0 = FeatureMap((0.0,1.0), False, False, (3,2) )
+        self.fm0.update(self.a1,0.5)
+
+        # object to test for non-cyclic, keep_peak distributions
+        
+        self.fm1 = FeatureMap((0.0,1.0), False, True, (3,2) )
+        self.fm1.update(self.a1,0.5)
+
+        # object to for cyclic, non-keep_peak distributions
+        
+        self.fm2 = FeatureMap((0.0,1.0), True, False, (3,2) )
+        self.fm2.update(self.a1,0.5)
+
+        # object to for cyclic, keep_peak distributions
+
+        self.fm3 = FeatureMap((0.0,1.0), True, True, (3,2) )
+        self.fm3.update(self.a1,0.5)
+
+
+
+
+    # add a test_update()    
+
+
+    def test_map(self):
+        
+        for i in range(3):
+            for j in range(2):
+                self.assertAlmostEqual(self.fm0.map()[i,j], 0.5)
+                self.assertAlmostEqual(self.fm1.map()[i,j], 0.5)
+                self.assertAlmostEqual(self.fm2.map()[i,j], 0.5)
+                self.assertAlmostEqual(self.fm3.map()[i,j], 0.5)
+
+
+             
+        self.fm0.update(self.a1,0.7)
+        self.fm1.update(self.a1,0.7)
+        self.fm2.update(self.a1,0.7)
+        self.fm3.update(self.a1,0.7)
+
+        for i in range(3):
+            for j in range(2):
+                self.assertAlmostEqual(self.fm0.map()[i,j], 0.6)
+                self.assertAlmostEqual(self.fm1.map()[i,j], 0.6)
+                self.assertAlmostEqual(self.fm2.map()[i,j], (arg(exp(0.7*2*pi*1j)+exp(0.6*2*pi*1j)))/(2*pi)) 
+                self.assertAlmostEqual(self.fm3.map()[i,j], arg(exp(0.7*2*pi*1j)+exp(0.6*2*pi*1j)))
+
+       
+        self.fm0.update(self.a1,0.7)
+        self.fm1.update(self.a1,0.7)
+        self.fm2.update(self.a1,0.7)
+        self.fm3.update(self.a1,0.7)
+
+        for i in range(3):
+            for j in range(2):
+                self.assertAlmostEqual(self.fm0.map()[i,j], (0.5+2*0.7)/2)
+                self.assertAlmostEqual(self.fm1.map()[i,j], 0.6)
+                self.assertAlmostEqual(self.fm2.map()[i,j], arg(exp(0.7*2*pi*1j)+2*exp(0.6*2*pi*1j)))
+                self.assertAlmostEqual(self.fm3.map()[i,j], arg(exp(0.7*2*pi*1j)+exp(0.6*2*pi*1j)))
+
+                
+        self.a2 = array([[2,2], [2,2], [2,2]])
+
+        
+        self.fm0.update(self.a2,0.7)
+        self.fm1.update(self.a2,0.7)
+        self.fm2.update(self.a2,0.7)
+        self.fm3.update(self.a2,0.7)
+
+        # add the test........def test_selectivity
+
+
+
+cases = [TestFeatureMap]
+
+suite = unittest.TestSuite()
+
+suite.addTests([unittest.makeSuite(case) for case in cases])
+              
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=1).run(suite)
+
+
+        
+
+        
+        
+
+                
+        
+
+   
--- topo/base/featuremap.py
+++ topo/base/featuremap.py
@@ -3,32 +3,89 @@ FeatureMap
 
 ** this file doesn't do anything yet **
 
-Id: distribution.py,v 1.4 2005/10/13 15:11:36 ceball Exp $
+$Id$
 """
 
-from Numeric import array 
+from Numeric import array, zeros, add
+from distribution import Distribution
+
+
+
+def fm_add(activity_matrix,stimulus_value):
+    """
+    Procedure that transforms a matrix of activity to a matrix of dictionnaries
+    with a single key equal to stimulus_value
+
+    This is used by the update FeatureMap method,and enables to simply use the __add__
+    method of the Distribution class
+    """
+    
+    new_matrix=zeros(activity_matrix.shape,'O')
+    for i in range(len(activity_matrix)):
+        for j in range(len(activity_matrix[i])):
+                       new_matrix[i,j] = {stimulus_value:activity_matrix[i,j]}
+    return new_matrix
+
+
 
 class FeatureMap(object):
-"""
-"""
+    """
+    """
 
-    def __init__(self, activity_matrix, cyclic, axis_range, keep_peak):
+    def __init__(self, axis_range, cyclic, keep_peak, dim_activity_matrix):
         """
-        """
-        distribution_matrix=[[]]
-        for i in activity_matrix:
-            distribution_matrix.append([])
-            for j in activity_matrix:
-                item = distribution(axis_range, cyclic, keep_peak)
-                distribution_matrix[i].append
+        The only attribute pf a FeatureMap is the matrix that stores the Distribution
+        object associating with the activity matrix that relates to this FeatureMap
 
+        
+        """
+        # get same Distribution object every time        
+        # self.distribution_matrix = array([[Distribution(axis_range, cyclic, keep_peak)]*cols]*rows)
+
+        rows, cols = dim_activity_matrix
+        self.distribution_matrix = zeros(dim_activity_matrix,'O')
+        for i in range(rows):
+            for j in range(cols):
+                self.distribution_matrix[i,j] = Distribution(axis_range,cyclic,keep_peak)
+       
 
     def update(self, activity_matrix, stimulus_value):
         """
         """
-        for i in activity_matrix:
-            for j in activity_matrix:
-                self.distribution_matrix[].add({stimulus_value:activity_matrix[i,j]})
+        
+        self.distribution_matrix + fm_add(activity_matrix,stimulus_value)
+
+    def map(self):
+        """
+        """
+
+        rows, cols = self.distribution_matrix.shape
+        preference_matrix=zeros((rows, cols),'O')
+
+        # preference_matrix = self.distribution_matrix
+        
+        for i in range(rows):
+            for j in range(cols):
+                preference_matrix[i,j]=self.distribution_matrix[i,j].weighted_average()
+
+        return preference_matrix
+        
+    def selectivity(self):
+        """
+        """
+
+        rows, cols = self.distribution_matrix.shape
+        selectivity_matrix=zeros((rows, cols),'O')
+
+        # preference_matrix = self.distribution_matrix
+        
+        for i in range(rows):
+            for j in range(cols):
+                selectivity_matrix[i,j]=self.distribution_matrix[i,j].selectivity()
+
+        return selectivity_matrix
+         
+
 
         
 
