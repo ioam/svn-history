0621fef959e88742893657b3bda1946588ea600b
James A. Bednar <jbednar@inf.ed.ac.uk>
2005-10-14 22:36:50 +0000

    Fixed documentation

--- topo/responsefns/__init__.py
+++ topo/responsefns/__init__.py
@@ -1,11 +1,10 @@
 """
-A family of functions mapping from a value into another of the same shape.
+A family of response functions for CFProjections.
 
-A set of endomorphic functions, i.e., functions mapping from an object
-into another of the same matrix shape.  These are useful for neuron
-output functions, normalization of matrices, etc.
+These function objects compute a response matrix when given an input
+pattern and a set of ConnectionField objects.
 
-Any new output functions added to this directory will automatically
+Any new response functions added to this directory will automatically
 become available for any model.
 
 $Id$
