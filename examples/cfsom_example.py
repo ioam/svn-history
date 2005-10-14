eb17a25cc4b8dd5a8903ec3a2568ade1f5d08c8e
James A. Bednar <jbednar@inf.ed.ac.uk>
2005-10-14 10:42:56 +0000

    Cleaned up

--- examples/cfsom_example.py
+++ examples/cfsom_example.py
@@ -2,24 +2,22 @@
 $Id$
 """
 
-from math import pi
 import random
-import pdb #debugger
-from topo.sheets.generatorsheet import *
-from topo.base.patterngenerator import *
-from topo.base.simulator import *
+
+from topo.sheets.generatorsheet import GeneratorSheet
+from topo.base.patterngenerator import BoundingBox
 from topo.projections.kernelprojection import KernelProjection
-from topo.plotting.plotfilesaver import ImageSaver
 from topo.base.parameter import Dynamic
 from topo.sheets.cfsom import CFSOM
 from topo.patterns.random import UniformRandomGenerator
 from topo.patterns.basic import FuzzyLineGenerator
+from topo.base.simulator import Simulator
 
 
 ###########################################
 # Set parameters
 
-# input generation params
+# input patterns
 GeneratorSheet.period = 1.0
 GeneratorSheet.density = 30
 
@@ -28,36 +26,27 @@ FuzzyLineGenerator.y = Dynamic(lambda : random.uniform(-0.5,0.5))
 
 FuzzyLineGenerator.theta = Dynamic(lambda :random.uniform(-pi,pi))
 FuzzyLineGenerator.width = 0.02
-#FuzzyLineGenerator.height = 0.9
 FuzzyLineGenerator.bounds = BoundingBox(points=((-0.8,-0.8),(0.8,0.8)))
 
-
-# rf som parameters
+# cortical sheet
 CFSOM.density = 50
 CFSOM.learning_length = 10000
 CFSOM.radius_0 = 0.1
 
-# Projection Parameters
 KernelProjection.weights_generator = UniformRandomGenerator(bounds=BoundingBox(points=((-0.1,-0.1),(0.1,0.1))))
 
-# image saver parameters
-ImageSaver.file_format='png'
-ImageSaver.time_format='%0.4d'
 
 ###########################################
 # build simulation
-
-topo.base.object.min_print_level = topo.base.object.MESSAGE
 
 s = Simulator()
 
 retina = GeneratorSheet(input_generator=FuzzyLineGenerator(),name='Retina')
 V1 = CFSOM(name='V1')
-save  = ImageSaver(name='CFSOM')
-
 s.connect(retina,V1,delay=1,projection_type=KernelProjection,projection_params={'name':'RtoV1'})
 
-#s.run(100)
+s.run(1)
+#s.run(10000)
 
 # import profile,pstats
 #
--- examples/gui_example.py
+++ examples/gui_example.py
@@ -3,23 +3,21 @@ $Id$
 """
 
 import random
-from math import pi
-import pdb #debugger
-from topo.sheets.generatorsheet import *
-from topo.base.patterngenerator import *
-from topo.base.simulator import *
+
+from topo.sheets.generatorsheet import GeneratorSheet
+from topo.base.patterngenerator import BoundingBox
 from topo.projections.kernelprojection import KernelProjection
-from topo.plotting.plotfilesaver import ImageSaver
 from topo.base.parameter import Dynamic
 from topo.sheets.cfsom import CFSOM
 from topo.patterns.random import UniformRandomGenerator
 from topo.patterns.basic import FuzzyLineGenerator
+from topo.base.simulator import Simulator
 
 
 ###########################################
 # Set parameters
 
-# input generation params
+# input patterns
 GeneratorSheet.period = 1.0
 GeneratorSheet.density = 20
 
@@ -30,34 +28,28 @@ FuzzyLineGenerator.theta = Dynamic(lambda :random.uniform(-pi,pi))
 FuzzyLineGenerator.width = 0.02
 FuzzyLineGenerator.bounds = BoundingBox(points=((-0.8,-0.8),(0.8,0.8)))
 
-
-# rf som parameters
+# cortical sheet
 CFSOM.density = 10
 CFSOM.learning_length = 10000
 CFSOM.radius_0 = 0.1
 
 KernelProjection.weights_generator = UniformRandomGenerator(bounds=BoundingBox(points=((-0.1,-0.1),(0.1,0.1))))
 
+#topo.base.object.min_print_level = topo.base.object.MESSAGE
 
-# image saver parameters
-ImageSaver.file_format='png'
-ImageSaver.time_format='%0.4d'
 
 ###########################################
 # build simulation
 
-topo.base.object.min_print_level = topo.base.object.MESSAGE
-
-s = topo.base.simulator.Simulator()
+s = Simulator()
 
 retina = GeneratorSheet(input_generator=FuzzyLineGenerator(),name='Retina')
 retina2 = GeneratorSheet(input_generator=FuzzyLineGenerator(),name='Retina2')
 V1 = CFSOM(name='V1')
 V2 = CFSOM(name='V2')
-save  = ImageSaver(name='CFSOM')
 
 s.connect(retina,V1,delay=0.5,projection_type=KernelProjection,projection_params={'name':'R1toV1'})
 s.connect(retina,V2,delay=0.5,projection_type=KernelProjection,projection_params={'name':'R1toV2'})
 s.connect(retina2,V2,delay=0.5,projection_type=KernelProjection,projection_params={'name':'R2toV2'})
 
-s.run(2)
+s.run(1)
