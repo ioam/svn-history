ce1932259022433008f79cb83227d71aa84a7a18
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2005-10-19 17:57:00 +0000

    Added code to allow test to be run independently of runtests script

--- tests/testdistribution.py
+++ tests/testdistribution.py
@@ -217,5 +217,8 @@ class TestDistribution(unittest.TestCase):
 
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestDistribution))
+
               
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite)
 
