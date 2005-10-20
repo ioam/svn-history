b8ee1a2e4ab80da7af97c221d12d141d9e4f797a
James A. Bednar <jbednar@inf.ed.ac.uk>
2005-10-20 19:51:45 +0000

    Made __main__ name match the one inside the Retinas

--- examples/gui_example.py
+++ examples/gui_example.py
@@ -43,13 +43,13 @@ KernelProjection.weights_generator = UniformRandomGenerator(bounds=BoundingBox(p
 
 s = Simulator()
 
-retina = GeneratorSheet(input_generator=FuzzyLineGenerator(),name='Retina')
-retina2 = GeneratorSheet(input_generator=FuzzyLineGenerator(),name='Retina2')
+Retina = GeneratorSheet(input_generator=FuzzyLineGenerator(),name='Retina')
+Retina2 = GeneratorSheet(input_generator=FuzzyLineGenerator(),name='Retina2')
 V1 = CFSOM(name='V1')
 V2 = CFSOM(name='V2')
 
-s.connect(retina,V1,delay=0.5,connection_type=KernelProjection,connection_params={'name':'R1toV1'})
-s.connect(retina,V2,delay=0.5,connection_type=KernelProjection,connection_params={'name':'R1toV2'})
-s.connect(retina2,V2,delay=0.5,connection_type=KernelProjection,connection_params={'name':'R2toV2'})
+s.connect(Retina,V1,delay=0.5,connection_type=KernelProjection,connection_params={'name':'R1toV1'})
+s.connect(Retina,V2,delay=0.5,connection_type=KernelProjection,connection_params={'name':'R1toV2'})
+s.connect(Retina2,V2,delay=0.5,connection_type=KernelProjection,connection_params={'name':'R2toV2'})
 
 s.run(1)
