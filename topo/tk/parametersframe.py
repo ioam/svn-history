7a0184926e001a6fcdcf86de9b7ab869f76951f7
James A. Bednar <jbednar@inf.ed.ac.uk>
2005-10-21 07:54:55 +0000

    Added ALERTs.  Removed unused code.

--- topo/tk/parametersframe.py
+++ topo/tk/parametersframe.py
@@ -1,7 +1,5 @@
 """
-Container for manipulating a PropertiesFrame so that a Parameter can
-be manipulated.  Written to remove code from inputparampanel.py with
-the goal of eventually turning into a more general file.
+ParametersFrame class.
 
 $Id$
 """
@@ -12,6 +10,21 @@ from topo.base.utils import eval_atof
 from copy import deepcopy
 
 class ParametersFrame(Frame):
+    """
+    Property list frame for the Parameters of any TopoObject.
+    
+    Automatically generates a PropertiesFrame containing all the
+    Parameters for the TopoObject.
+
+    Currently limited to supporting PatternGenerator objects only.
+    """
+
+    ### JABHACKALERT!  Is not actually coded in a general way; needs
+    ### massive rewriting to accept *any* TopoObject.  Nothing should
+    ### assume anything about Patterns or PatternGenerators,
+    ### everything mentioning sliders should be written for (and
+    ### described as) being for widgets instead, and so on.
+    
     def __init__(self, parent=None,**config):
         self.parent = parent
         self.prop_frame = propertiesframe.PropertiesFrame(self.parent,string_translator=eval_atof)
@@ -20,7 +33,18 @@ class ParametersFrame(Frame):
         self.default_values = self.prop_frame.get_values()
         self.prop_frame.pack(side=TOP,expand=YES,fill=X)
 
-
+    ### JABHACKALERT!  This should be made into part of a
+    ### PatternGeneratorParameter widget.
+    ### GeneratorSheet.input_generator would be declared as a
+    ### PatternGeneratorParameter, and such a widget would allow the
+    ### user to select a PatternGenerator from the known list.  The
+    ### code for that should be done very generally, because we'll
+    ### need to do something very similar for learning rules,
+    ### activation functions, etc.  Even then, it should probably not
+    ### be part of this class, because this class is for the
+    ### parameters of one single TopoObject.  At the moment it has a
+    ### selection box for choosing such an object, and also includes
+    ### its parameters, which is a mess.
     def pg_parameters(self,pg_classname):
         """
         Return the list of Parameter names and objects for the requested PatternGenerator name.
@@ -36,6 +60,13 @@ class ParametersFrame(Frame):
         return plist
     
 
+    ### JABHACKALERT!  Needs to be extended to support non-slider
+    ### parameters, making a slider only for Numbers, and other
+    ### types of widget for other types of Parameter.  Numbers
+    ### should work even if they don't have bounds, in which case
+    ### they just get a non-slider widget.  *Any* parameter type
+    ### should be allowed, using a default text entry expression
+    ### widget in the most general case.
     def make_sliders_from_params(self,params,slider_dict):
         """
         Make a new slider for each name/value in the params list.
@@ -77,7 +108,6 @@ class ParametersFrame(Frame):
         self.make_sliders_from_params(new_param_names,self.tparams)
         new_sliders = dict(new_param_names).keys()
         new_sliders.sort()
-#        new_sliders = self.relevant_parameters(new_name)
         for i in range(len(new_sliders)):
             (s,c) = self.tparams[new_sliders[i]]
             s.grid(row=i,column=0,padx=self.prop_frame.padding,
@@ -89,20 +119,11 @@ class ParametersFrame(Frame):
                    sticky=N+S+W+E)
 
 
-# Preparing to remove
-#    def relevant_parameters(self,kf_classname):
-#        """
-#        Pre:  kf_classname is the string name of a PatternGenerator subclass.
-#        Post: List of strings that is the Intersection of kf_classname's
-#              class member keys, and tparams.keys() entries.
-#        """
-#
-#        kf_class_keylist = topo.base.registry.pattern_generators[kf_classname].__dict__.keys()
-#        rlist = [s for s in self.tparams.keys() if s in kf_class_keylist]
-#
-#        return rlist
-
-
+    ### JABHACKALERT!  What is this doing in this file?  It has no relevance
+    ### to ParametersFrame, and is only useful for inputparamspanel, not other
+    ### types of ParametersFrame!  Ideally it would move out of tk/ altogether,
+    ### but back into inputparamspanel would at least make more sense.
+    ###
     ### JAB: It is not clear how this will need to be extended to support
     ### objects with different parameters in the different eyes, e.g. to
     ### test ocular dominance.
