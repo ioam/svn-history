c61ae7c4047a76bda3a7ce30dc66344f0a455b73
Julien Ciroux <julien.ciroux@gmail.com>
2005-11-02 18:20:27 +0000

    Got rid of unused comments; added some comments about JABHACKALERTs that I believe are obsolete.

--- topo/tk/preferencemappanel.py
+++ topo/tk/preferencemappanel.py
@@ -18,10 +18,16 @@ import topo.base.registry
 ### We want to support any featuremap type defined in that file, and so import all of them here.
 from topo.analysis.featuremap import *
 
+
+### JCALERT! This alert is now irrelevant: the class now work in general according to the map_name
+### you pass to it: you can in the future create a similar panel for Ocular Dominance
+### if there is a corresponding group template for that
+
 ### JABHACKALERT: This class fully supports only Orientation Preferences plots
 ### at present, and needs to be cleaned up a bit to work in general.
 ### E.g. it probably shouldn't have the mapname ComboBox, and should instead
 ### use the name from the pgt_name that it was called with.
+
 class PreferenceMapPanel(plotpanel.PlotPanel):
     def __init__(self,parent,pengine=None,console=None,plot_key='Activity',map_name='Orientation Preference',**config):
         plotpanel.PlotPanel.__init__(self,parent,pengine,console=console,plot_key=plot_key,pgt_name=map_name,**config)
@@ -29,17 +35,11 @@ class PreferenceMapPanel(plotpanel.PlotPanel):
         self.panel_num = self.console.num_orientation_windows
 
         self.pgt = topo.base.registry.plotgroup_templates[map_name]
-        #self.mapcmds = dict((('Orientation Preference',      'measure_or_pref()'),
-        #                     ('Ocular Preference',           'pass')))
-
+     
         # Name of the plotgroup to plot
         self.mapname = StringVar()
-
-        #self.mapname.set('Orientation Preference')
         self.mapname.set(self.pgt.name)
-        
-        
-        
+                
         # lissom command used to refresh the plot, if any
         self.cmdname = StringVar()
         
@@ -49,13 +49,8 @@ class PreferenceMapPanel(plotpanel.PlotPanel):
         
         params_frame = Frame(master=self)
         params_frame.pack(side=TOP,expand=YES,fill=X)
-        
-        #Pmw.ComboBox(params_frame,autoclear=1,history=1,dropdown=1,
-        #             entry_textvariable=self.mapname,
-        #             scrolledlist_items=(['Orientation Preference'])
-        #             ).pack(side=LEFT,expand=YES,fill=X)
-        ### There was 'Ocular Preference' in the list before, but does not work yet
 
+        ### JCALERT! I think the following comment is now irrelevant:     
         # Ideally, whenever self.mapname changes this selection would be 
         # updated automatically by looking in self.mapcmds.  However, I 
         # don't know how to set up such a callback. (jbednar@cs)
@@ -64,13 +59,10 @@ class PreferenceMapPanel(plotpanel.PlotPanel):
                      scrolledlist_items=([self.pgt.command])
                      ).pack(side=LEFT,expand=YES,fill=X)
 
-        #Pmw.ComboBox(params_frame,autoclear=1,history=1,dropdown=1,
-        #             entry_textvariable=self.cmdname,
-        #             scrolledlist_items=(['measure_or_pref()'])
-        #             ).pack(side=LEFT,expand=YES,fill=X)
-
 
         self.refresh()
+
+        # To make the auto-refresh button not on by default when opening the panel
         self.auto_refresh_checkbutton.invoke()
 
         ### JCHACKALERT! Rewrites that so it returns the appropriate plot keys 
@@ -85,27 +77,25 @@ class PreferenceMapPanel(plotpanel.PlotPanel):
         # l,b,r,t = ep.bounds.aarect().lbrt()
         self.plot_key = ('Activity','Activity','Activity')
 
-
+    ### JCALERT! I think now there is only the doc to be fixed
     ### JABHACKALERT!
     ### 
     ### This function needs to be fixed and/or documented; the old
     ### documentation was incorrect (copied from another file).
     def do_plot_cmd(self):
         """
-        Needs fixing.
+        Function that generates the plot for the panel.
+        It first executes the command typed in the command line.
+        Anytime refresh is performed, this method is executed.
         """
         if self.console.active_simulator().get_event_processors():
 
-            ## to fix so that it executes the command that have been typed instead of
-            ## at the moment it will only execute measure_or_pref()
             exec self.cmdname.get()
             pgt = topo.base.registry.plotgroup_templates[self.mapname.get()]
             self.pe_group = self.pe.get_plot_group(self.mapname.get(),pgt)
             self.pe_group.do_plot_cmd()
 
             
-            
-
     def display_labels(self):
         """
         Change the title of the grid group, then call PlotPanel's
