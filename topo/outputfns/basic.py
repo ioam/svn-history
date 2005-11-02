08807d773587a21638bd7a7782e9fe4a5e2a65dc
James A. Bednar <jbednar@inf.ed.ac.uk>
2005-11-02 20:42:09 +0000

    Added alerts and cleaned up documentation.  Changed clip to clip_in_place

--- topo/outputfns/basic.py
+++ topo/outputfns/basic.py
@@ -23,10 +23,13 @@ from topo.base.projection import OutputFunction
 from topo.base.projection import Identity
 
 
-## Rewrite the clip function of Numeric so that it does not create intermediate copy
-## Should go in topo/base/utils.py
-def clip(mat,lower_bound,upper_bound):
-    # What about the savespace being here
+### JABALERT!
+###
+### Should go in topo/base/utils.py, and if at all possible should be
+### rewritten to use matrix functions that eliminate the explicit for
+### loop.  The savespace() should probably also be eliminated.
+def clip_in_place(mat,lower_bound,upper_bound):
+    """Version of Numeric.clip that changes the argument in place, with no intermediate."""
     mat.savespace(1)
     for element,i in zip(mat.flat,range(len(mat.flat))):
         if element<lower_bound:
@@ -35,9 +38,6 @@ def clip(mat,lower_bound,upper_bound):
             mat.flat[i] = upper_bound
             
     
-### JABALERT!  Consider fixing these functions to modify their argument
-### in place, to avoid creating temporaries.
-
 class PiecewiseLinear(OutputFunction):
     """ 
     Piecewise-linear output function with lower and upper thresholds
@@ -49,17 +49,17 @@ class PiecewiseLinear(OutputFunction):
     def __init__(self,**params):
         super(PiecewiseLinear,self).__init__(**params)
 
-### JABHACKALERT! The x.savespace(1)  should not be here...
-### It would be nice to have it at an higher level        
+    ### JABHACKALERT! The x.savespace(1) should not be here, or elsewhere
+    ### in this file.  It needs to be at a higher level somewhere.
     def __call__(self,x):
         x.savespace(1)
         fact = 1.0/(self.upper_bound-self.lower_bound)        
         x -= self.lower_bound
         x *= fact
-        #x = (x-self.lower_bound)*fact
-        clip(x,0.0,1.0)
+        clip_in_place(x,0.0,1.0)
         return x
 
+### JABALERT! Should be renamed to DivisiveSumNormalize
 class DivisiveL1Normalize(OutputFunction):
     """
     OutputFunction that divides an array by its sum (aka its L1 norm).
@@ -82,6 +82,7 @@ class DivisiveL1Normalize(OutputFunction):
         return x
 
 
+### JABALERT! Should be renamed to DivisiveLengthNormalize
 class DivisiveL2Normalize(OutputFunction):
     """
     OutputFunction to divide an array by its Euclidean length (aka its L2 norm).
@@ -131,7 +132,8 @@ class DivisiveLpNormalize(OutputFunction):
     For a parameter p and a given array interpreted as a flattened
     vector, keeps the Lp-norm of the vector at a specified norm_value.
     Faster versions are provided separately for the typical L1-norm
-    and L2-norm cases.  Defaults to be the same as an L2-norm.
+    and L2-norm cases.  Defaults to be the same as an L2-norm, i.e.,
+    DivisiveL2Normalize.
     """
     p = Number(default=2)
     norm_value = Number(default=1.0)
