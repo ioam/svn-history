ea9ee0043ff49369d1c0b3c84361333f0d826c57
Julien Ciroux <julien.ciroux@gmail.com>
2005-11-07 12:30:44 +0000

    Added the __version__='$' line to the __init__.py file in the topo/ directory

--- topo/__init__.py
+++ topo/__init__.py
@@ -30,7 +30,7 @@ then show up in the GUI menus as potential input patterns.
 
 $Id$
 """
-
+__version__='$Revision $'
 __all__ = ['base','commands','sheets','projections','patterns','eps','plotting','analysis']
 
 # Enable automatic importing of .ty files, treating them just like .py
