d646ab2e6a2cae137301ad9f22886174152bcff4
James A. Bednar <jbednar@inf.ed.ac.uk>
2005-11-07 02:37:38 +0000

    Initial version

---
+++ doc/User_Manual/text.php
@@ -0,0 +1,14 @@
+Under construction.
+<!-- 
+Need to add:
+
+  - Figure A.1 from miikkulainen:book05.
+  - Network diagrams from ~/ueinf/research/papers/topographica/slides.tex
+  - Description from: ~/ueinf/research/papers/cns03-sw/paper.tex
+  - Some motivation from: ~/ueinf/research/papers/sfn02/field.tex
+  - Features from ~/ueinf/research/papers/sfn02/sw.tex
+
+Need to make a Downloads page, and add info about CVS version of
+Topographica to it, plus mention that there will eventually be 
+downloadable packages.
+-->
