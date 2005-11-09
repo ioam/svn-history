82dad4171d8a36a4cd1dbf25266be032991d269e
James A. Bednar <jbednar@inf.ed.ac.uk>
2005-11-09 03:01:34 +0000

    Minor cleanup

--- doc/Home/text.php
+++ doc/Home/text.php
@@ -2,7 +2,7 @@
      width="300" height="326" border="2" alt="Sample architecture" align="right"></A>
 
 <P><!--img src="../images/T-bg.png" width="43" height="50" align="left" alt="T" border="2"-->Topographica is a software package for computational modeling of
-cortical maps.  It is being developed by the 
+cortical maps, developed by the 
 <a target="_top" href="http://www.anc.ed.ac.uk">Institute for Adaptive and Neural Computation</a> 
 at the <a target="_top" href="http://www.ed.ac.uk">University of Edinburgh</a> and the 
 <a target="_top" href="http://www.cs.utexas.edu/users/nn">Neural Networks Research
@@ -15,10 +15,8 @@ maps that make up sensory and motor systems.
 
 <P>Topographica is intended to complement the many good low-level
 neuron simulators that are available, such as
-<A target="_top" HREF="http://www.genesis-sim.org/GENESIS/">Genesis</A>,
-<A target="_top" HREF="http://kacy.neuro.duke.edu/">Neuron</A>,
-<A target="_top" HREF="http://www.neosim.org">NeoSIM</A>, and
-<A target="_top" HREF="http://www.compneuro.org/catacomb/">Catacomb</A>.  
+<A target="_top" HREF="http://www.genesis-sim.org/GENESIS/">Genesis</A> and
+<A target="_top" HREF="http://kacy.neuro.duke.edu/">Neuron</A>.  
 Those simulators focus on modeling the detailed internal behavior of
 neurons and small networks of them.  Topographica instead focuses on
 the large-scale structure and function that is visible only when many
@@ -48,36 +46,14 @@ simulator, it will also support any other sensory modality that is
 organized into topographic maps, such as touch and hearing, as well as
 motor areas.
 
-<P><A name="releases">Topographica</A> is freely available for downloading, and is an
-open source project whose capabilities can be extended and modified by
-any user.  The first public release is planned for late 2005, although
-CVS access is available to the 
-<A target="_top" href="topographica/README.txt">development version</A> 
-in the meantime
-(<a target="_top" href="http://www.cs.utexas.edu/users/nn/web-pubs/lissom/screenshots/050423_Topographica_linuxscreenshot.png">Linux screenshot</a>).  
-Until the official release, please consider using the following free
-software packages: 
-
-<p>
-<ul>
-<p><li><a target="_top" href="http://nn.cs.utexas.edu/keyword?lissomsw"> LISSOM</a>
-     --- Hierarchical maps with self-organizing lateral connections. <br>
-
-(<b>New version of 
-<a target="_top" href="http://nn.cs.utexas.edu/keyword?lissomsw">LISSOM</a>
-released 9/2004</a></b>, with <a target="_top" href="http://homepages.inf.ed.ac.uk/jbednar/pytutorial.html">tutorial</a> and 
-screenshots for 
-<a target="_top" href="http://www.cs.utexas.edu/users/nn/web-pubs/lissom/screenshots/lissom.4.0_linuxscreenshot.gif">Linux</a>,
-<a target="_top" href="http://www.cs.utexas.edu/users/nn/web-pubs/lissom/screenshots/lissom.4.0_windowsscreenshot.gif">Windows</a>, and
-<a target="_top" href="http://www.cs.utexas.edu/users/nn/web-pubs/lissom/screenshots/lissom.4.0_macscreenshot.gif">Mac</a>.)
-
-<p><li><a target="_top" href="http://nn.cs.utexas.edu/keyword?pglissomsw"> PGLISSOM</a>
-     --- Maps with self-organizing lateral connections and spiking neurons.
-</ul>
-
-<!-- Links MUST be like 'a target="_top" href="..."' to make them show up properly in the URL field -->
+<P>Topographica is freely available for downloading, and is an open
+source project whose capabilities can be extended and modified by any
+user.  The project is under active development, but as shown in the
+Tutorial it is already suitable for running visual cortex simulations.
 
 Also see our 
 <A target="_top" HREF="http://nn.cs.utexas.edu/area-view.php?RECORD_KEY%28Areas%29=AreaID&AreaID(Areas)=11">
 research pages</a> for results from simulations run with Topographica
-and related tools.
+and related tools, and our 
+<A target="_top" HREF="http://computationalmaps.org">new book</A> for more details.
+
