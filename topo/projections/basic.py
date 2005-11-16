2611e2783f23596746a21a5a5f37e736f3040e58
Yiu Fai Sit <yfsit@yahoo.com>
2005-11-16 07:19:20 +0000

    added topo.projection.basic so that CFProjection can be imported from topo.projections.

---
+++ topo/projections/basic.py
@@ -0,0 +1,15 @@
+"""
+This simple file brings CFProjection into topo.projections so that users can
+select CFProjection under this name space which contains all useful 
+implementations of projections.
+
+Also, quoted from Jim:
+"Eventually, we will have a menu of things that are valid Projections, and it 
+will include only the things in topo.projections."
+
+$Id$
+"""
+
+__version__ = "$Revision$"
+
+from topo.base.connectionfield import CFProjection
