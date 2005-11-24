70dfd2a6760c66efd16b4ee148ae6a51017b83e6
Julien Ciroux <julien.ciroux@gmail.com>
2005-11-24 15:25:07 +0000

    Changed the name of testpreferencemappanel.py to be testbasicplotgrouppanel.py.

---
+++ topo/tests/tkgui/testbasicplotgrouppanel.py
@@ -0,0 +1,40 @@
+"""
+Test for BasicGroupPanel
+$Id$
+"""
+__version__='$Revision$'
+
+
+import unittest
+import topo
+import Numeric
+import Tkinter
+import Pmw
+import random
+from PIL import Image
+from topo.sheets.generatorsheet import *
+from topo.base.patterngenerator import *
+from topo.base.simulator import *
+from topo.base.sheetview import *
+from topo.plotting.plotengine import *
+from topo.tkgui.basicplotgrouppanel import *
+
+### JCALERT: This test has to be written in order to test the new change in
+### the BasicPlotGroupPanel file
+### (It would be nice to re-write it when performing at the same time a re-organisation and
+### clean-up of the tkgui directory)
+
+class TestBasicPlotGroupPanel(unittest.TestCase):
+
+
+    def test_preference_plot(self):
+        pass
+
+suite = unittest.TestSuite()
+#  Uncomment the following line of code, to not run the test if
+#  $DISPLAY is undefined.  Used mainly for GUI testing.
+suite.requires_display = True
+suite.addTest(unittest.makeSuite(TestBasicPlotGroupPanel))
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite)
