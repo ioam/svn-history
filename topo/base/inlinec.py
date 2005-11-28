e4bf27e3d416f379de0859233e1246e1d18a5139
Judah De Paula <judah@cs.utexas.edu>
2005-11-28 23:35:04 +0000

    Changed the comments to not reference the dictionary.

--- topo/base/inlinec.py
+++ topo/base/inlinec.py
@@ -1,14 +1,14 @@
 """
 Interface class for inline C functions in the SciPy Weave package.
 
-Weave is used for writing inline C code for code speedups.  It is
+Weave is used for writing inline C for code speedups.  Sometimes it is
 possible to run Topographica without having initially installed Weave,
-such as on a Windows platform.  Using this file as an interface,
-fallback non-optimized functions will be transparently called if Weave
-is not installed.
+such as on Windows.  Using this file as an interface, non-optimized
+functions will be transparently called as a fallback if Weave is not
+installed.
 
-The transparency is possible by the way Python allows function names
-to be overwritten.  For example:
+This transparency is possible by making use of the way Python allows
+function names to be overwritten.  For example:
 
 def x(y = 5): return y
 def x2(y = 6): return y*y
@@ -17,20 +17,21 @@ x = x2
 print 'Overwritten.'
 print 'x:', x(), 'x2:', x2()  # x: 36 x2: 36
 
-Then, when this file is imported, a dictionary is accessed that
-replaces the non-Weave functions with the Weave optimized functions.
-For example inline() would be overwritten to call inline_weave()
-instead.  This will require a non-optimized function assignment for
-each optimized function, even if that function is a message stating
-that the function is a stub.
+For example inline() will be overwritten to call inline_weave()
+instead.  This requires a non-optimized function assignment for each
+optimized function, even if that function is a message stating that
+the function is a stub.
 
 
 $Id$
 """
 
+# In case an outside package wants to know
 weave_imported = False
 
-def inline(*params): print "inline() not implemented."
+# Set 'somefunction = UNIMPLEMENTED' for a default failure statement.
+def UNIMPLEMENTED: print 'Function not implemented.'
+def inline(*params): print 'inline() not implemented.'
 
 try:
     import weave
