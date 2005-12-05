ec9c6bec80ee9b41d3c0c43ce72dbc05bb3b404b
Yoonsuck Choe <choe@tamu.edu>
2005-12-05 06:53:56 +0000

    Edited button color scheme.

--- doc/shared/config.php
+++ doc/shared/config.php
@@ -6,7 +6,7 @@ $color_scheme = "parchment";
 if ($color_scheme == "parchment") {
 
 	$main_bg_color = "#ffffff";
-	$button_color = "#f2dab7";
+	$button_color = "#e9d3a2";
 	$banner_bg_color  = "#f0e0c6";
 	$menu_bg_color = "#f0e0c4";
 	$frame_color  = $menu_bg_color;
