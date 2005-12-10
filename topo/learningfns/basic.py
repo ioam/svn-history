3ba97cb4c3805f3f73ad7a939b6efa662dfa9568
Yiu Fai Sit <yfsit@yahoo.com>
2005-12-10 00:14:22 +0000

    - tested Hebbian() and uncommented it.

--- topo/learningfns/basic.py
+++ topo/learningfns/basic.py
@@ -25,81 +25,80 @@ from topo.base.connectionfield import IdentityCFLF,GenericCFLF
 ### because they use weave.
 
 
-### JABALERT!  Untested.
-# class Hebbian(CFLearningFunction):
-#     """
-#     CF-aware Hebbian learning rule.
-# 
-#     Implemented in C for speed.  Should be equivalent to
-#     GenericCFLF(single_cf_fn=hebbian), except faster.  Callers can set
-#     the output_fn to perform normalization if desired.
-#     """
-#     output_fn = Parameter(default=Identity())
-#     
-#     def __init__(self,**params):
-#         super(Hebbian,self).__init__(**params)
-# 
-#     def __call__(self, cfs, input_activity, output_activity, learning_rate, **params):
-#         rows,cols = output_activity.shape
-#         len, len2 = input_activity.shape
-# 
-#         hebbian_code = """
-#             float *wi, *wj;
-#             double *x, *inpi, *inpj;
-#             int *slice;
-#             int rr1, rr2, cc1, cc2, rc;
-#             int i, j, r, l;
-#             PyObject *cf, *cfsr;
-#             PyObject *sarray = PyString_FromString("slice_array");
-#             PyObject *weights = PyString_FromString("weights");
-#             double load, delta;
-#             double totald;
-#     
-#             x = output_activity;
-#             for (r=0; r<rows; ++r) {
-#                 cfsr = PyList_GetItem(cfs,r);
-#                 for (l=0; l<cols; ++l) {
-#                     load = *x++;
-#                     if (load != 0) {
-#                         load *= learning_rate;
-#     
-#                         cf = PyList_GetItem(cfsr,l);
-#                         wi = (float *)(((PyArrayObject*)PyObject_GetAttr(cf,weights))->data);
-#                         wj = wi;
-#                         slice = (int *)(((PyArrayObject*)PyObject_GetAttr(cf,sarray))->data);
-#                         rr1 = *slice++;
-#                         rr2 = *slice++;
-#                         cc1 = *slice++;
-#                         cc2 = *slice;
-#     
-#                         totald = 0.0;
-#     
-#                         inpj = input_activity+len*rr1+cc1;
-#                         for (i=rr1; i<rr2; ++i) {
-#                             inpi = inpj;
-#                             for (j=cc1; j<cc2; ++j) {
-#                                 delta = load * *inpi;
-#                                 *wi += delta;
-#                                 totald += delta;
-#                                 ++wi;
-#                                 ++inpi;
-#                             }
-#                             inpj += len;
-#                         }
-#                     }
-#                 }
-#             }
-#         """
-#         
-#         inline(hebbian_code, ['input_activity', 'output_activity', 'rows', 'cols', 'len', 'cfs', 'learning_rate'],local_dict=locals())
-# 
-#         # Apply output_fn to each CF
-#         # (skipped entirely for no-op case, as an optimization)
-#         output_fn = self.output_fn
-#         if type(output_fn) is not Identity:
-#             for r in range(rows):
-#                 for c in range(cols):
-#                     cfs[r][c].weights = output_fn(cfs[r][c].weights)
+class Hebbian(CFLearningFunction):
+    """
+    CF-aware Hebbian learning rule.
+
+    Implemented in C for speed.  Should be equivalent to
+    GenericCFLF(single_cf_fn=hebbian), except faster.  Callers can set
+    the output_fn to perform normalization if desired.
+    """
+    output_fn = Parameter(default=Identity())
+    
+    def __init__(self,**params):
+        super(Hebbian,self).__init__(**params)
+
+    def __call__(self, cfs, input_activity, output_activity, learning_rate, **params):
+        rows,cols = output_activity.shape
+        len, len2 = input_activity.shape
+
+        hebbian_code = """
+            float *wi, *wj;
+            double *x, *inpi, *inpj;
+            int *slice;
+            int rr1, rr2, cc1, cc2, rc;
+            int i, j, r, l;
+            PyObject *cf, *cfsr;
+            PyObject *sarray = PyString_FromString("slice_array");
+            PyObject *weights = PyString_FromString("weights");
+            double load, delta;
+            double totald;
+    
+            x = output_activity;
+            for (r=0; r<rows; ++r) {
+                cfsr = PyList_GetItem(cfs,r);
+                for (l=0; l<cols; ++l) {
+                    load = *x++;
+                    if (load != 0) {
+                        load *= learning_rate;
+    
+                        cf = PyList_GetItem(cfsr,l);
+                        wi = (float *)(((PyArrayObject*)PyObject_GetAttr(cf,weights))->data);
+                        wj = wi;
+                        slice = (int *)(((PyArrayObject*)PyObject_GetAttr(cf,sarray))->data);
+                        rr1 = *slice++;
+                        rr2 = *slice++;
+                        cc1 = *slice++;
+                        cc2 = *slice;
+    
+                        totald = 0.0;
+    
+                        inpj = input_activity+len*rr1+cc1;
+                        for (i=rr1; i<rr2; ++i) {
+                            inpi = inpj;
+                            for (j=cc1; j<cc2; ++j) {
+                                delta = load * *inpi;
+                                *wi += delta;
+                                totald += delta;
+                                ++wi;
+                                ++inpi;
+                            }
+                            inpj += len;
+                        }
+                    }
+                }
+            }
+        """
+        
+        inline(hebbian_code, ['input_activity', 'output_activity', 'rows', 'cols', 'len', 'cfs', 'learning_rate'],local_dict=locals())
+
+        # Apply output_fn to each CF
+        # (skipped entirely for no-op case, as an optimization)
+        output_fn = self.output_fn
+        if type(output_fn) is not Identity:
+            for r in range(rows):
+                for c in range(cols):
+                    cfs[r][c].weights = output_fn(cfs[r][c].weights)
 
 
 class DivisiveHebbian(CFLearningFunction):
