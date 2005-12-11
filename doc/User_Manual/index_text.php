e56c6aa0c2bda6c8480b69df42e52e241450cd25
James A. Bednar <jbednar@inf.ed.ac.uk>
2005-12-11 16:15:21 +0000

    Added some notes for the manual

--- doc/User_Manual/index_text.php
+++ doc/User_Manual/index_text.php
@@ -5,7 +5,29 @@ To clarify these issues, a detailed user manual is currently under
 development (as of 11/2005), and will be posted here when it is
 usable.
 
-<!-- 
+<!--
+ First, general motivation and background (why use Topographica?  Why not?)
+
+ Sheets
+ sheet coordinates, densities, bounding boxes
+ event-driven simulator, time scale
+ class parameters
+ instance/constructor parameters
+ How to define a new simulator
+ Hierarchy diagram: the major classes that matter
+ Lots of examples
+
+ Starting point:
+  1) existing model, want to make a change, or
+  2) biological system, want to model it
+      - Need to choose sheets, projections, properties from existing list, then
+      - Need to analyze how it behaves (plotting, statistics, etc.), then
+      - Depending on how well it fills the needs, need to be able to add
+          new primitives or edit existing ones
+      - Produce figures, tables, etc. for publication
+
+    
+ 
 Need to add:
 
   - Figure A.1 from miikkulainen:book05.
