5e2602cfa26c7ab04e77767e45738f81631a3056
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-01-04 17:10:25 +0000

    Code to populate LearningFunctionParameter's range().

--- topo/learningfns/__init__.py
+++ topo/learningfns/__init__.py
@@ -14,3 +14,15 @@ __version__='$Revision$'
 import re,os
 __all__ = [re.sub('\.py$','',f) for f in os.listdir(__path__[0])
            if re.match('^[^_].*\.py$',f)]
+
+
+import topo
+from topo.base.connectionfield import LearningFunctionParameter
+def make_classes_from_all_imported_modules_available():
+    """
+    Add all LearningFunction classes from the currently imported modules in
+    the topo.learningfns namespace to the list of available LearningFunctions.
+
+    See topo.base.parameter.ClassSelectorParameter.range().
+    """
+    LearningFunctionParameter.packages.append(topo.learningfns)
