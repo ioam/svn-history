ebc768aa5d68aa4b614b2b7df7e2acc04a4dfe45
Alan Lindsay <alindsay@math.arizona.edu>
2006-01-13 17:52:42 +0000

    Lateral projections represented as dotted circles; labels appear when hovered over and properties set through the projection's sheet. In bounds calculations are determined using full extent of parallelogram or receptive field. Multiple connections have a representation, however labels need sorted.

--- topo/tkgui/editorobjects.py
+++ topo/tkgui/editorobjects.py
@@ -8,6 +8,9 @@ __version__='$Revision$'
 
 # JABALERT: Please try to eliminate import *
 from Tkinter import *
+import Pmw
+import math
+
 from parametersframe import ParametersFrame
 
 class EditorObject :
@@ -34,9 +37,11 @@ class EditorObject :
 	paramWindow = Toplevel()
 	Label(paramWindow, text = self.name).pack(side = TOP)
 	self.paramFrame = ParametersFrame(paramWindow)
+	self.buttonPanel = Frame(paramWindow)
+	self.buttonPanel.pack(side = BOTTOM)
         #JABHACKALERT: We also need an OK button that applies and then closes the window
-	updateButton = Button(paramWindow, text = 'Apply', command = self.updateParameters)
-	updateButton.pack(side = BOTTOM)
+	updateButton = Button(self.buttonPanel, text = 'Update', command = self.updateParameters)
+	updateButton.pack(side = LEFT)
 
     def updateParameters(self) :
 	self.paramFrame.set_obj_params()
@@ -78,11 +83,12 @@ class EditorNode(EditorObject) :
 
     ############ Connection methods ########################
 
-    def attatchCon(self, con, fromTo) : # add the connection to or from this node
+    def attachCon(self, con, fromTo) :
 	if (fromTo == self.FROM) :
 		self.fromCon = self.fromCon + [con]
 	else :
 		self.toCon = self.toCon + [con]
+	self.draw()
 	
     def removeCon(self, con, fromTo) : # remove a connection to or from this node
 	if (fromTo) :
@@ -91,18 +97,32 @@ class EditorNode(EditorObject) :
 			if (con == self.toCon[i]) : break
 		else : return
 		del self.toCon[i]
+		node = con.nodeFrom
 	else :
 		l = len(self.fromCon)
 		for i in range(l) :
 			if (con == self.fromCon[i]) : break
 		else : return
 		del self.fromCon[i]
+		node = con.nodeTo
+	index = con.drawIndex
+	# Decrease the indexes of the connections between the same nodes and with a higher index.
+	for con in self.fromCon :
+		if (node == con.nodeTo and con.drawIndex > index) :
+			con.drawIndex -= 1
+			if (not(con.nodeTo == con.nodeFrom)) :
+				con.connectToCoord(self.width)
+	self.draw()
 
-    def getLoopCount(self) :
-	return self.loopCount
-
-    def incrementLoopCount(self) :
-	self.loopCount += 1
+    def getConCount(self, node) :
+	count = 0
+	for con in self.fromCon :
+		if (con.nodeTo == node) :
+			count += 1
+	for con in self.toCon :
+		if (con.nodeFrom == node) :
+			count += 1
+	return count
 
     ############ Util methods ##############################
     
@@ -112,7 +132,27 @@ class EditorNode(EditorObject) :
     def showProperties(self) :
 	EditorObject.showProperties(self)
 	self.paramFrame.create_widgets(self.sheet)
+	lateralButton = Button(self.buttonPanel, text = 'Lateral', command = self.showLatParams)
+	lateralButton.pack(side = RIGHT)
 
+    def showLatParams(self) :
+	latList = []
+	for con in self.fromCon :
+		if (con.nodeTo == con.nodeFrom) :
+			latList += [con]
+	paramWindow = Toplevel()
+	for con in latList :
+		frame = Frame(paramWindow)
+		frame.pack(side = LEFT)
+		Label(frame, text = con.name).pack(side = TOP)
+		con.paramFrame = ParametersFrame(frame)
+		con.paramFrame.create_widgets(con.connection)
+		buttonPanel = Frame(frame)
+		buttonPanel.pack(side = BOTTOM)
+		Button(buttonPanel, text = 'Update', 
+				command = con.updateParameters).pack(side = LEFT)
+		Button(buttonPanel, text = 'Delete',
+				command = con.remove).pack(side = RIGHT)
 
 class EditorSheet(EditorNode) :
     """
@@ -126,9 +166,12 @@ class EditorSheet(EditorNode) :
 	EditorNode.__init__(self, canvas, pos, name)
 	self.sheet = sheet # the topo sheet that this object represents
 	sheet.guiX, sheet.guiY = self.x, self.y # store the ed coords in the topo sheet
-	self.width = 50.0 # set the width and height parameters for this object
-	self.height = 25.0
-	self.initDraw(self.colours[1]) # create a new paralellogram
+	self.width = 70.0 # set the width and height parameters for this object
+	self.height = 35.0
+	col = self.colours[1]
+	self.initDraw(col) # create a new paralellogram
+	self.currentCol = col
+	self.gradient = 1
 
     ############ Draw methods ############################
 
@@ -139,6 +182,14 @@ class EditorSheet(EditorNode) :
 	if (focus) : col = self.colours[0]
 	else : 	       col = self.colours[1]
 	self.initDraw(col) # create new one with correct colour
+	"""
+	Try something like this instead..
+	if focus : 
+		col = self.colours[0]
+	else :
+		col = self.colours[1]
+	self.canvas.config(self.id, fill = col)"""
+	self.currentCol = col
 	# redraw the connections
 	for con in self.toCon : 
 		con.move()
@@ -171,7 +222,8 @@ class EditorSheet(EditorNode) :
 	for con in self.toCon : 
 		con.move()
 	for con in self.fromCon :
-		con.move()
+		if (not(con.nodeFrom == con.nodeTo)) :
+			con.move()
 
     ############ Update methods ############################ 
 
@@ -196,13 +248,26 @@ class EditorSheet(EditorNode) :
 
     ############ Util methods ##############################
 
-    def inBounds(self, x, y) : # returns true if point lies in a bounding box
-	# current box is a rectangular compromise between the largest and smallest extent of the 
-	# parallelogram
-	if ((y > (self.y - self.height)) & (y < (self.y + self.height)) & 
-	(x > (self.x - self.width)) & (x < (self.x + self.width))) :
-		return 1
-	return 0
+    def inBounds(self, posx, posy) : # returns true if point lies in a bounding box
+	# if the coord is pressed within the parallelogram representation this 
+	# returns true.
+	# Get the paralellogram points and centers them around the given point
+	x = self.x - posx; y = self.y - posy
+	A = (x - (1.5 * self.width), y + self.height)
+	B = (x - (0.5 * self.width), y - self.height)
+	C = (x + (1.5 * self.width), y - self.height)
+	D = (x + (0.5 * self.width), y + self.height)
+	# calculate the line constants 	
+	# As the gradient of the lines is 1 the calculation is simple.
+	aAB = A[1] + A[0]
+	aCD = C[1] + C[0]
+	# The points are centered around the given coord, finding the intersects with line y = 0
+	# and ensuring that the left line lies on the negative side of the point and the right line
+	# lies on the positive side of the point determines that the point is within the parallelogram.
+	if ((D[1] >= 0) and (B[1] <= 0) and (aAB <= 0) and (aCD >= 0)) :
+		return True
+	return False
+
 
 ####################################################################
 
@@ -231,7 +296,8 @@ class EditorConnection(EditorObject) :
     ############ Update methods ############################ 
     
     def move(self) :
-	# if one of the nodes connected by this connection move, then move by redrawing
+	# if one of the nodes connected by this connection move, then move by redrawing	
+	self.gradient = self.calcGradient()
 	self.draw() 
 
     def updtPos(self, pos) : # update the temporary point
@@ -243,18 +309,24 @@ class EditorConnection(EditorObject) :
 	self.connection = con # store the topo connection this object represents
 	if (self.name == "") :
 		self.name = con.name
-	if (self.nodeFrom == nodeTo) : # if lateral connection, update that obects counter
-		self.drawIndex = nodeTo.getLoopCount()
-		nodeTo.incrementLoopCount()
+	self.drawIndex = self.nodeFrom.getConCount(nodeTo)
+	if (not (self.nodeFrom == nodeTo)) :
+		self.connectToCoord(self.nodeFrom.width - 10)
 	self.nodeTo = nodeTo # store a reference to the node this is connected to
-	self.nodeFrom.attatchCon(self, self.FROM) # tell the sheets that they are connected.
-	self.nodeTo.attatchCon(self, self.TO)
+	self.nodeFrom.attachCon(self, self.FROM) # tell the sheets that they are connected.
+	self.nodeTo.attachCon(self, self.TO)
 	self.posTo = None
+	self.gradient = self.calcGradient()
 
     ############ Util methods ##############################
     def showProperties(self) :
 	EditorObject.showProperties(self)
 	self.paramFrame.create_widgets(self.connection)
+
+    def connectToCoord(self, width) :
+	n = self.drawIndex
+	sign = math.pow(-1, n)
+	self.dev = sign * width + (-sign) * math.pow(0.5, math.ceil(0.5 * (n + 1))) * width
 
 
 class EditorProjection(EditorConnection) :
@@ -270,9 +342,13 @@ class EditorProjection(EditorConnection) :
 	EditorConnection.__init__(self, name, canvas, nodeFrom)
 	# if more than one connection between nodes, 
 	# this will reflect how to draw this connection
-	self.drawIndex = 0 
+	self.drawIndex = 0
+	self.dev = 0
+	self.radius = 10
+	self.gradient = (1,1)
 	self.id = (None,None)
 	self.label = None
+	self.balloon = Pmw.Balloon(canvas)
 
     ############ Draw methods ############################
     def draw(self, receptiveFields = True) :
@@ -283,8 +359,10 @@ class EditorProjection(EditorConnection) :
 	posFrom = self.nodeFrom.getPos() # get the centre points of the two nodes
 	if (self.nodeTo == None) :  # if not connected yet, use temporary point.
 		posTo = self.posTo
+		latCol = self.nodeFrom.currentCol
 	else :
 		posTo = self.nodeTo.getPos()
+		latCol = self.nodeTo.currentCol
 
 	# set the colour to be used depending on whether connection has the focus.
 	if (self.focus) : col = 'slate blue'
@@ -292,6 +370,8 @@ class EditorProjection(EditorConnection) :
 	# midpoint of line
 	mid = self.getMid(posFrom, posTo)
 	if (posTo == posFrom) : # connection to and from the same node
+		"""
+		AL - OLD: Shows lateral connections as loops (here to see if still useful)
 		# change the size of circle depending how many lateral connections there are.
 		dev = self.drawIndex * 15 
 	    	x1 = posTo[0] - (20 + dev)
@@ -305,20 +385,37 @@ class EditorProjection(EditorConnection) :
 		# draw name label beside arrow head
 		self.label = self.canvas.create_text(mid[0] - 
 			     (20 + len(self.name)*3), mid[1] - (30 + dev) , text = self.name)
+		"""
+		n = 0.7 * self.drawIndex
+		fact = (15 / (1 + n))  
+		x1 = posTo[0] - (2 * fact)
+		y1 = posTo[1] + fact
+		x2 = posTo[0] + (2 * fact)
+		y2 = posTo[1] - fact
+		self.id = (self.canvas.create_oval(x1, y1, x2, y2, fill = latCol, 
+						dash = (2,2), outline = 'black'), None)
+		self.balloon.tagbind(self.canvas, self.id[0], self.name)
+
 	else :  # connection between distinct nodes
 		if (receptiveFields) : # if receptive fields are to be drawn
 			x1, y1 = posTo
 			x2, y2 = posFrom
-			self.id = (self.canvas.create_line(x1,y1,x2-10,y2, fill = col),
-				   self.canvas.create_line(x1,y1,x2+10,y2, fill = col),
-				   self.canvas.create_oval(x2-10,y2-5,x2+10,y2+5))
+			x1 += self.dev
+			x2 += self.dev
+			rad = self.radius
+			self.id = (self.canvas.create_line(x1,y1,x2-rad,y2, fill = col),
+				   self.canvas.create_line(x1,y1,x2+rad,y2, fill = col),
+				   self.canvas.create_oval(x2-rad,y2-(0.5*rad),x2+rad,y2+(0.5*rad)))
 		else :
 			# create a line between the nodes - use 2 to make arrow in centre.
 	    		self.id = (self.canvas.create_line(posFrom, mid , arrow = LAST, fill = col),
 	    			  self.canvas.create_line(mid, posTo, fill = col))
-		# draw name label beside arrow head
-		self.label = self.canvas.create_text(mid[0] - 
-			     (10 + len(self.name)*3), mid[1], text = self.name)
+		# draw name label
+		dX = (10 + len(self.name)*3)
+		dY = self.drawIndex * 20
+		self.label = self.canvas.create_text(mid[0] - dX,
+			     mid[1] - dY, text = self.name)
+	
 	
     ############ Update methods ############################ 
     def remove(self) :
@@ -333,8 +430,48 @@ class EditorProjection(EditorConnection) :
     ############ Util methods ##############################
     def getMid(self, pos1, pos2) : # returns the middle of two points
 	return (pos1[0] + (pos2[0] - pos1[0])*0.5, pos1[1] + (pos2[1] - pos1[1])*0.5)
-	
-    def inBounds(self, x, y) : # returns true if point lies in a bounding box
+
+    def calcGradient(self) :
+	posTo = (self.nodeTo.x, self.nodeTo.y)
+	posFrom = (self.nodeFrom.x, self.nodeFrom.y)
+	A = (posTo[0] + self.dev ,posTo[1])
+	T = (posFrom[0] + self.dev ,posFrom[1])
+	B = (T[0] - self.radius, T[1])
+	C = (T[0] + self.radius, T[1])
+	try :
+		MBA = (A[1] - B[1]) / (A[0] - B[0])
+		MCA = (A[1] - C[1]) / (A[0] - C[0])
+	except ZeroDivisionError :
+		return 99999 # AL - this should be a big number
+	return (MBA, MCA)
+
+    def inBounds(self, x, y, receptiveFields = True) : # returns true if point lies in a bounding box
+	# returns true if x, y lie inside the triangular receptive field representing this projection
+	if (receptiveFields) :
+		if (self.nodeTo == None or self.nodeTo == self.nodeFrom) :
+			return False
+		# get the points of the triangular receptive field, centered around the x, y point given
+		posTo = (self.nodeTo.x, self.nodeTo.y)
+		posFrom = (self.nodeFrom.x, self.nodeFrom.y)
+		A = (posTo[0] + self.dev - x, posTo[1] - y)
+		T = (posFrom[0] + self.dev - x, posFrom[1] - y)
+		B = (T[0] - self.radius, T[1])
+		C = (T[0] + self.radius, T[1])
+		# if the y coords lie outwith the boundaries, return false
+		if (((A[1] < B[1]) and (B[1] < 0 or A[1] > 0)) or 
+		    ((A[1] >= B[1]) and (B[1] > 0 or A[1] < 0))) :
+			return False
+		# calculate the constant for the lines of the triangle
+		aBA = A[1] - (self.gradient[0] * A[0])
+		aCA = A[1] - (self.gradient[1] * A[0])
+		# The points are centered around the given coord, finding the intersects with line y = 0
+		# and ensuring that the left line lies on the negative side of the point and the right line
+		# lies on the positive side of the point determines that the point is within the triangle.
+		if (((0 - aCA) / self.gradient[1] >= 0) and ((0 - aBA) / self.gradient[0] <= 0)) :
+			return True
+		return False	
+
+	# If connections are represented as lines
 	# currently uses an extent around the arrow head.
 	posTo = self.nodeTo.getPos()
 	posFrom = self.nodeFrom.getPos()
@@ -345,5 +482,5 @@ class EditorProjection(EditorConnection) :
 		mid = self.getMid(posFrom, posTo)
 
 	if ((x < mid[0] + 10) & (x > mid[0] - 10) & (y < mid[1] + 10) & (y > mid[1] - 10)) :
-		return 1
-	return 0
+		return True
+	return False
