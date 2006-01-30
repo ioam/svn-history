7c536d36ca711ea3e8594fb30802a0db75a9413a
James A. Bednar <jbednar@inf.ed.ac.uk>
2006-01-30 01:10:39 +0000

    Made RandomGenerator base class to make it simpler to add other Random classes.  Added GaussianRandom

--- topo/patterns/random.py
+++ topo/patterns/random.py
@@ -14,14 +14,19 @@ from topo.base.sheet import bounds2slice
 import RandomArray
 
 
-class UniformRandom(PatternGenerator):
-    """2D uniform random noise pattern generator."""
+class RandomGenerator(PatternGenerator):
+    """2D random noise pattern generator abstract class."""
 
     # The standard x, y, and orientation variables are currently ignored,
     # so they aren't shown in auto-generated lists of parameters (e.g. in the GUI)
     x       = Number(hidden = True)
     y       = Number(hidden = True)
     orientation   = Number(hidden = True)
+
+
+    def _distrib(self,shape,**params):
+        """Method for subclasses to override with a particular random distribution."""
+        raise NotImplementedError
     
     # Optimization: We use a simpler __call__ method here to skip the
     # coordinate transformations (which would have no effect anyway)
@@ -39,7 +44,32 @@ class UniformRandom(PatternGenerator):
         r1,r2,c1,c2 = bounds2slice(bounds,bounds,xdensity,ydensity)
         shape = (r2-r1,c2-c1)
 
+        return self._distrib(shape)
+
+
+class UniformRandom(RandomGenerator):
+    """2D uniform random noise pattern generator."""
+
+    def _distrib(self,shape,**params):
         offset_=params.get('offset',self.offset)
         scale_=params.get('scale',self.scale)
-     
         return RandomArray.uniform( offset_, offset_+scale_, shape)
+
+
+class GaussianRandom(RandomGenerator):
+    """
+    2D Gaussian random noise pattern generator.
+
+    Each pixel is chosen independently from a Gaussian distribution
+    of zero mean and unit variance, then multiplied by the given
+    scale and adjusted by the given offset.
+    """
+
+    scale  = Number(default=0.25,softbounds=(0.0,2.0))
+    offset = Number(default=0.50,softbounds=(-2.0,2.0))
+
+    def _distrib(self,shape,**params):
+        offset_=params.get('offset',self.offset)
+        scale_ =params.get('scale', self.scale)
+        return offset_+scale_*RandomArray.standard_normal(shape)
+
