bc1b97d49422ce7d2dc199f5de350ddd6c525ddb
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-02-18 15:57:22 +0000

    Continued writing...

--- doc/User_Manual/parameter_text.php
+++ doc/User_Manual/parameter_text.php
@@ -3,47 +3,69 @@
 <!--CEBHACKALERT: still being written. Needs reorganizing
 and updating (already!). -->
 
-<P>Simulation parameters are handled 
-specially in Topographica, to allow specific behaviours.
-This special handling is provided by TopoObject - the base class
-for most objects in Topographica - in conjunction
-with the Parameter class. 
+<P>Simulation parameters are handled specially in Topographica, to
+allow specific behaviours.  This special handling is provided by
+ParameterizedObject - the base class for most objects in Topographica
+- in conjunction with the Parameter class.
 
-<P>For example, take the TopoObject instance
-G=topo.patterns.basic.Gaussian().  This is a two-dimensional Gaussian
-pattern generator. You can see in a Test Pattern window that a
-Gaussian is specified by scale, offset, x, y, size, aspect_ratio, and
-orientation. These are all Parameters of G.  You can see one feature
-of Parameters immediately by trying to enter <i>-1</i> in the
-<i>aspect_ratio</i> box: a ValueError occurs. The bounds of a
-Parameter can be specified so that an error is raised if a user
-attempts to set a value outside these bounds; in this case, the
-<i>aspect_ratio</i> cannot be negative. 
+<H2>Introduction to Parameters</H2>
 
-<P>Looking at the source code
-for topo.patterns.basic.Gaussian, you can see that <i>aspect_ratio</i> is declared to be a Number parameter. There are several different types of Parameter; which to use depends on what behaviour you want: a DynamicNumber, for example, allows you to generate a new value each time it is accessed, whereas a Constant's value cannot be changed.
+<P>Consider the ParameterizedObject instance
+<code>G=topo.patterns.basic.Gaussian()</code>.  This is a
+two-dimensional Gaussian pattern generator. You can see in a Test
+Pattern window that a Gaussian is specified by <code>scale</code>,
+<code>offset</code>, <code>x</code>, <code>y</code>,
+<code>size</code>, <code>aspect_ratio</code>, and
+<code>orientation</code>. These are all Parameters of <code>G</code>.
+You can see one feature of Parameters immediately by trying to enter
+<code>-1</code> in the <code>aspect_ratio</code> box: a ValueError
+occurs. The bounds of a Parameter can be specified so that an error is
+raised if a user attempts to set a value outside these bounds; in this
+case, the <code>aspect_ratio</code> cannot be negative.
 
-<P>You might also notice in the source file that not all the parameters needed to specify a Gaussian pattern are isent. Most, such as <i>x</i> and <i>y</i> are inhertied from the base class, PatternGenerator: Parameters themselves are inherited up the TopoObject class hierarchy. In addition to TopoObjects inheriting Parameters, Parameters themselves can inherit attributes. Let's say I want the default value of <i>x</i> to be different for the Gaussian. I declare a new Number parameter in the Gaussian source code <i>x</i> with a different default value:
-<br>
-<i>x = Number(default=0.5)</i>
-<br>
-Gaussian patterns will have <i>x</i> values that default to 0.5 instead of 0.0. In PatternGenerator, you can see that as well as a default, softbounds are also specified for <i>x</i>. the purpose of these is to provide an indication for something such as a the graphical interface to Topographica what kind of scale ... CEBHACKALERT: (1) softbounds is a bad example (2) inheritance of bounds doesn't work: see HACKALERT in TopoObject.
+<P>Looking at the source code for
+<code>topo.patterns.basic.Gaussian</code>, you can see that
+<code>aspect_ratio</code> is declared to be a <code>Number</code>
+parameter. There are several different types of
+<code>Parameter</code>; which to use depends on what behaviour you
+want: a <code>DynamicNumber</code>, for example, allows you to
+generate a new value each time it is accessed, whereas a
+<code>Constant</code>'s value cannot be changed.
+
+<P>You might also notice in the source file that not all the
+parameters needed to specify a Gaussian pattern are isent. Most, such
+as <code>x</code> and <code>y</code> are inhertied from the base
+class, PatternGenerator: Parameters themselves are inherited up the
+TopoObject class hierarchy. In addition to TopoObjects inheriting
+Parameters, Parameters themselves can inherit attributes. Let's say I
+want the default value of <code>x</code> to be different for the
+Gaussian. I declare a new Number parameter in the Gaussian source code
+<code>x</code> with a different <code>default</code> value: 
+
+<pre> x=Number(default=0.5) </pre> 
+
+Gaussian patterns will have <code>x</code> values that default to
+<code>0.5</code> instead of <code>0.0</code>. In
+<code>PatternGenerator</code>, you can see that as well as a
+<code>default</code> value, there are <code>softbounds</code>,
+<code>doc</code>, and <code>precedence</code>. What these do is
+explained in <code>Parameter</code>'s specific documentation, but
+taking the example of <code>doc</code> (which describes what the
+parameter is), after re-defining <code>x</code> and setting a new
+<code>default</code> in <code>Gaussian</code>, <code>Gaussian.x</code>
+will contain the same string for <code>doc</doc> as
+<code>PatternGenerator.x</code>.
+
+
+<H2>Using Parameters</H2>
 <P>
-<pre>
- - TopoObject: what does it provide?
- - Where should parameters for TopoObjects be set?
-    - at the class level?
-    - in the constructor for a TopoObject?
-    - on a constructed instance?
-   What is the meaning of setting in each of those places, and when is
-   one better than the other?
- - How does Parameter inheritance work?
-</pre>
+In Python, attributes declared at the class level are shared by all
+instances of that class, unless an instance overwrites the attribute
+with its own copy of the variable. For example: 
 
-<p>
-In Python, attributes declared at the class level are shared by all instances of that class, unless an instance overwrites the attribute with its own copy of the variable. For example:
-<pre>
-class Example(object):
+<pre> 
+class
+Example(object):
 
     a = 10
 
@@ -51,7 +73,7 @@ class Example(object):
         self.b = b
 
 </pre>
-<i>a</i> is a class attribute; creating two instances of Example demonstrates the important behaviour:
+<code>a</code> is a class attribute; creating two instances of Example demonstrates the important behaviour:
 <pre>
 Topographica> example1 = Example(b=12)
 Topographica> example2 = Example(b=5)
@@ -131,15 +153,15 @@ be instantiated for each new object - overriding the default
 behaviour. This is useful when the Parameter will hold a
 mutable object that sharing between instances would lead to
 confusion. An example of such an object is the response function
-<i>PiecewiseLinear</i>, which has the attributes <i>upper_bound</i>
-and <i>lower_bound</i>. A user might want to declare that all his
-<i>CFSheet</i>s will have response functions that are 
-<i>PiecewiseLinear</i>. Under usual Python circumstances,  
-instances of the class <i>CFSheet</i>, with its attribute 
-<i>output_fn</i>, would share a single <i>PiecewiseLinear</i>
-object. A user with a number of <i>CFSheet</i>s might be
-surprised to find that setting the <i>lower_bound</i> on one
-particular <i>CFSheet</i> would change it on them all:
+<code>PiecewiseLinear</code>, which has the attributes <code>upper_bound</code>
+and <code>lower_bound</code>. A user might want to declare that all his
+<code>CFSheet</code>s will have response functions that are 
+<code>PiecewiseLinear</code>. Under usual Python circumstances,  
+instances of the class <code>CFSheet</code>, with its attribute 
+<code>output_fn</code>, would share a single <code>PiecewiseLinear</code>
+object. A user with a number of <code>CFSheet</code>s might be
+surprised to find that setting the <code>lower_bound</code> on one
+particular <code>CFSheet</code> would change it on them all:
 <pre>
 Topographica> CFSheet.output_fn = PiecewiseLinear()
 Topographica> LGNOn = CFSheet()
@@ -150,7 +172,7 @@ Topographica> LGNOn.output_fn.lower_bound = 0.05
 Topographica> LGNOff.output_fn.lower_bound
 0.05
 </pre>
-To avoid this confusion, the author of <i>CFSheet</i> can
+To avoid this confusion, the author of <code>CFSheet</code> can
 declare that the output_fn Parameter always be 
 instantiated:
 <pre>
@@ -160,3 +182,15 @@ In this case, each instance of CFSheet will have its own
 instance of PiecewiseLinear, independent of other
 CFSheets' PiecewiseLinear() instances.
 
+<br><br>
+
+<pre>
+ - TopoObject: what does it provide?
+ - Where should parameters for TopoObjects be set?
+    - at the class level?
+    - in the constructor for a TopoObject?
+    - on a constructed instance?
+   What is the meaning of setting in each of those places, and when is
+   one better than the other?
+ - How does Parameter inheritance work?
+</pre>
