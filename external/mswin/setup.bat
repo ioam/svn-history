21eaab3249a5bee8f496955a478225d16f318631
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-03-04 13:36:44 +0000

    Added note on how to get weave.

--- external/mswin/setup.bat
+++ external/mswin/setup.bat
@@ -1,6 +1,24 @@
 REM invoke as:
 REM setup "c:\chris\topographica\"
 
+REM to get weave, rename python242_topo_weave.tar.gz to python242_topo.tar.gz
+REM to run topographica with weave, you need gcc somewhere in your path and
+REM you need to edit some files:
+REM
+REM topo/misc/inlinec.py
+REM change: 
+REM  import weave
+REM to:
+REM  from scipy import weave
+REM
+REM topo/misc/commandline.py
+REM change:
+REM  exec "import weave" in __main__.__dict__
+REM to:
+REM  exec "from scipy import weave" in __main__.__dict__ 
+
+
+
 if "%1"=="" goto end
 
 util\gunzip -c python242_topo.tar.gz > python242_topo.tar
