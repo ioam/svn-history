1c9a1d4f33634d31382df9f50c5509609c010c80
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-03-06 17:04:59 +0000

    Various changes to the scripts.

--- external/mswin/setup.bat
+++ external/mswin/setup.bat
@@ -1,12 +1,41 @@
 @echo off
-echo
+
+REM Note that this script assumes it's starting two levels 
+REM above the topographica directory.
+
+echo.
 echo Topographica installation script
-echo
+echo --------------------------------
+echo.
 
-REM must be given a path
-if "%1"=="" goto bad_arg
+set installed="False"
 
+REM get directories
+set startdir=%CD%
+cd ..\..
+set instdir=%CD%
+cd %startdir%
+
+REM check user wants to go ahead
+ :start
+echo This will install Topographica to %instdir%.
+echo.
+echo (No other part of your system will be altered.)
+echo.
+set choice=
+set /p choice=Enter '1' to proceeed or '2' to quit:
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' goto install
+if '%choice%'=='2' goto end
+echo "%choice%" is not valid; please enter '1' or '2'
+echo.
+goto start
+
+
+:install
 REM unzip and untar, leaving original gz file alone
+echo.
+echo Unpacking and installing Python...
 util\gunzip -c python_topo.tar.gz > python_topo.tar
 util\tar xvf python_topo.tar
 del /F python_topo.tar
@@ -15,18 +44,25 @@ REM move the python stuff to the topographica directory
 move /Y python_topo ..\..\
 
 REM create startup scripts for topographica
-..\..\python_topo\python.exe setup.py %1
+echo.
+echo Creating startup scripts...
+..\..\python_topo\python.exe setup.py "%instdir%"
+set installed="True"
 goto end
 
 
-:bad_arg
-echo
-echo Invoke as:
-echo  setup path\to\topographica
-echo
-echo E.g. If you downloaded Topographica to c:\topographica, type:
-echo  setup c:\topographica
-echo
-
 :end
-echo
+echo.
+echo Topographica setup script finished.
+echo.
+if %installed%=="False" goto exit
+cd ..\..
+echo To start, type 
+echo    topographica
+echo or
+echo    topographica -g 
+echo to get a graphical interface.
+echo.
+echo.
+
+:exit
--- external/mswin/topographica.bat
+++
@@ -1,3 +0,0 @@
-REM something like but not this coz paths build up!
-set path=%PATH%;c:\topographica\python_topo;c:\topographica\python_topo\MinGW\bin;
-python_topo\python.exe topographica.py %*
