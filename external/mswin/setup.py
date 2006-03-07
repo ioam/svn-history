8d2348116940b36eeb0bb7c73b9d60b3356c16c4
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-03-07 11:33:36 +0000

    Now generates a script that looks more like the one on unix. Fixed a problem with quotation marks.

--- external/mswin/setup.py
+++ external/mswin/setup.py
@@ -5,15 +5,26 @@
 import sys
 import os
 
+# argument comes from setup.bat
 path = os.path.abspath(sys.argv[1])
+
 compiler_path = "python_topo\\mingw\\bin"
 
-# topographica.ty
-f = open(os.path.join(path,"topographica.py"),'w')
-f.write('import os'+'\n')
-f.write("os.environ['PATH']='"+repr(os.path.join(path,compiler_path))+";'\n")
-### should keep current path, too
-f.write("""os.environ['TOPORELEASE']='0.8.2'"""+'\n')
+# topographica script
+f = open(os.path.join(path,"topographica"),'w')
+
+f.write("# Startup script for Topographica\n")
+f.write("\n")
+f.write("# for gcc compiler\n")
+# Should keep current path, too? Or work out how to set gcc compiler path directly.
+f.write("from os import environ\n")
+f.write("environ['PATH']="+repr(os.path.join(path,compiler_path))+"\n")
+f.write("\n")
+f.write('import topo'+'\n')
+# This is hard-coded twice: should be read from somewhere.
+f.write("topo.release='0.8.2'\n")
+f.write("\n")
+f.write("# Process the command-line arguments\n")
 f.write('from sys import argv\n')
 f.write('from topo.misc.commandline import process_argv\n')
 f.write('process_argv(argv[1:])\n')
@@ -22,6 +33,6 @@ f.close()
 # topographica.bat
 f = open(os.path.join(path,'topographica.bat'),'w')
 f.write("""@echo off"""+'\n')
-f.write("""python_topo\python.exe topographica.py %*"""+'\n')
+f.write("""python_topo\python.exe topographica %*"""+'\n')
 f.write("""@echo on"""+'\n')
 f.close()
