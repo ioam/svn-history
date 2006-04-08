1e85f3ee9ab7cbe9feb0afe781304113e0c59481
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-04-08 11:21:39 +0000

    Reduced density of sheets in test_measurefeaturemap(). It runs a lot faster now.

--- topo/tests/testfeaturemap.py
+++ topo/tests/testfeaturemap.py
@@ -194,15 +194,13 @@ class TestMeasureFeatureMap(unittest.TestCase):
         Create a CFSOM sheet ('V1') connected to a GeneratorSheet ('Retina').
         """
         self.s = Simulator()
-        self.retina = GeneratorSheet(name='Retina')
-        self.V1 = CFSOM(name='V1')
+        self.retina = GeneratorSheet(name='Retina',density=4.0)
+        self.V1 = CFSOM(name='V1',density=4.0)
+        self.V2 = CFSOM(name='V2',density=4.0)
+
         self.s.connect(self.retina,self.V1,delay=0.5,connection_type=CFProjection,
                        name='RtoV1',learning_fn=HebbianSOMLF())
 
-        self.V2 = CFSOM(name='V2')
-
-
-        #self.s.connect(self.V1,self.V2,delay=1,connection_type=CFProjection,name='V1toV2')
         self.s.connect(self.retina,self.V2,delay=0.5,connection_type=CFProjection,
                        name='RtoV2',learning_fn=HebbianSOMLF())
 
