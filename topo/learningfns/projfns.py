a8c4e5c1f9e82dc82466f61fa74cbaf13a240128
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-04-08 23:28:38 +0000

    Initial version.

---
+++ topo/learningfns/projfns.py
@@ -0,0 +1,15 @@
+"""
+Learning functions for Projections.
+
+
+For example, CFProjectionLearningFunctions compute a new set of
+ConnectionFields when given an input and output pattern and a set of
+ConnectionField objects.
+
+$Id$
+"""
+__version__ = "$Revision$"
+
+# Imported here so that all ProjectionLearningFns will be in the same package
+from topo.base.connectionfield import CFProjectionIdentityLearningFn,CFProjectionGenericLearningFn
+
