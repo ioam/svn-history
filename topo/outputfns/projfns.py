ef1908c7d9f7f3baefe14b0428917f988130f95b
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-04-08 18:55:33 +0000

    Initial version.

---
+++ topo/outputfns/projfns.py
@@ -0,0 +1,11 @@
+"""
+Output functions (see basic.py) that apply to a whole Projection. For example, for a CFProjection this involves iterating through all the CFs and applying an output function to each.
+
+$Id$
+"""
+__version__='$Revision$'
+
+
+# imported here so that all projection-level output functions are in the
+# same package
+from topo.base.connectionfield import GenericCFOF
