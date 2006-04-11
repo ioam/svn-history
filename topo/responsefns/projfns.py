47c13d42070c79438991984c52caee3c170a9627
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-04-11 09:07:31 +0000

    Renamed basic.py to projfns.py since it contains only projection-level response functions.

---
+++ topo/responsefns/projfns.py
@@ -0,0 +1,64 @@
+"""
+Projection-level response functions
+
+For CFProjections, these function objects compute a response matrix
+when given an input pattern and a set of ConnectionField objects.
+
+$Id$
+"""
+__version__='$Revision$'
+
+from Numeric import zeros, Float, ravel
+
+from topo.base.arrayutils import L2norm
+from topo.base.connectionfield import CFProjectionResponseFn
+from topo.base.parameterclasses import Parameter
+from topo.base.parameterizedobject import ParameterizedObject
+
+from topo.misc.inlinec import inline, optimized
+
+class CFProjectionDotProduct(CFProjectionResponseFn):
+    """
+    Dot-product response function.
+
+    Written entirely in Python; see CFProjectionDotProduct_opt1 for a much faster
+    (but otherwise equivalent) version.
+    """
+    def __init__(self,**params):
+        super(CFProjectionDotProduct,self).__init__(**params)
+
+    def __call__(self, cfs, input_activity, activity, strength, **params):
+        rows,cols = activity.shape
+        for r in xrange(rows):
+            for c in xrange(cols):
+                cf = cfs[r][c]
+                r1,r2,c1,c2 = cf.slice_tuple()
+                X = input_activity[r1:r2,c1:c2]
+        
+                a = X*cf.weights
+                activity[r,c] = sum(a.flat)
+        activity *= strength
+
+
+class CFProjectionEuclideanDistance(CFProjectionResponseFn):
+    """
+    Euclidean-distance--based response function.
+    """
+    def __init__(self,**params):
+        super(CFProjectionEuclideanDistance,self).__init__(**params)
+
+    def __call__(self, cfs, input_activity, activity, strength, **params):
+        rows,cols = activity.shape
+	euclidean_dist_mat = zeros((rows,cols),Float)
+        for r in xrange(rows):
+            for c in xrange(cols):
+                cf = cfs[r][c]
+                r1,r2,c1,c2 = cf.slice_tuple()
+                X = input_activity[r1:r2,c1:c2]
+		diff = ravel(X) - ravel(cf.weights)
+		euclidean_dist_mat[r,c] = L2norm(diff)
+
+        max_dist = max(euclidean_dist_mat.flat)
+        activity *= 0.0
+        activity += (max_dist - euclidean_dist_mat)
+        activity *= strength
