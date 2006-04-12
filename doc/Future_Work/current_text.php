e9cec135207485ee0b74b22e313849c0a8afe065
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-04-12 11:25:36 +0000

    Updated a task.

--- doc/Future_Work/current_text.php
+++ doc/Future_Work/current_text.php
@@ -21,7 +21,7 @@ was first added to the list, or a change was made.)
 </li>
 
 <li>
-2006/04/12: (CB) Finish reorganizing response functions and CFProjection response functions; rename CFProjectionGenericOutputFn to GenericCFPOutputFn, etc.; rename connectionfield.py to cf.py; rename Identity to IdentityOutputFn; create Identity learning function; create topo/base/functionfamilies.py and move LearningFns etc into it; rename SharedCFProjection SharedWeightCFProjection  
+2006/04/12: (CB) Finish reorganizing response functions and CFProjection response functions; rename CFProjectionGenericOutputFn to GenericCFPOutputFn, etc.; rename connectionfield.py to cf.py; rename Identity to IdentityOutputFn; create Identity learning function; create topo/base/functionfamilies.py and move LearningFns etc into it; rename SharedCFProjection SharedWeightCFProjection; make sure GUI lists are populated (and try to make that simpler).
 </li>
 
 <li> 
