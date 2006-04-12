dd931b3aef4ab3d5bd5d204151272ec4787925a8
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-04-12 15:03:27 +0000

    Removed unnecessary __init__()s. Use ResponseFnParameter for single_cf_fn.

--- topo/responsefns/projfns.py
+++ topo/responsefns/projfns.py
@@ -1,5 +1,5 @@
 """
-Projection-level response functions
+Projection-level response functions.
 
 For CFProjections, these function objects compute a response matrix
 when given an input pattern and a set of ConnectionField objects.
@@ -11,12 +11,12 @@ __version__='$Revision$'
 from Numeric import zeros, Float, ravel
 
 from topo.base.arrayutils import L2norm
-from topo.base.connectionfield import CFProjectionResponseFn
-from topo.base.parameterclasses import Parameter
-from topo.base.parameterizedobject import ParameterizedObject
+from topo.base.connectionfield import CFProjectionResponseFn,ResponseFnParameter,Mdot
 
 from topo.misc.inlinec import inline, optimized
 
+
+# CEBHACKALERT: This is CFProjectionGenericResponseFn(single_cf_fn=Mdot()).
 class CFProjectionDotProduct(CFProjectionResponseFn):
     """
     Dot-product response function.
@@ -24,9 +24,8 @@ class CFProjectionDotProduct(CFProjectionResponseFn):
     Written entirely in Python; see CFProjectionDotProduct_opt1 for a much faster
     (but otherwise equivalent) version.
     """
-    def __init__(self,**params):
-        super(CFProjectionDotProduct,self).__init__(**params)
-
+    single_cf_fn = ResponseFnParameter(Mdot(),constant=True)
+    
     def __call__(self, cfs, input_activity, activity, strength, **params):
         rows,cols = activity.shape
         for r in xrange(rows):
@@ -34,7 +33,8 @@ class CFProjectionDotProduct(CFProjectionResponseFn):
                 cf = cfs[r][c]
                 r1,r2,c1,c2 = cf.slice_tuple()
                 X = input_activity[r1:r2,c1:c2]
-        
+
+                # "Mdot()"
                 a = X*cf.weights
                 activity[r,c] = sum(a.flat)
         activity *= strength
@@ -44,9 +44,6 @@ class CFProjectionEuclideanDistance(CFProjectionResponseFn):
     """
     Euclidean-distance--based response function.
     """
-    def __init__(self,**params):
-        super(CFProjectionEuclideanDistance,self).__init__(**params)
-
     def __call__(self, cfs, input_activity, activity, strength, **params):
         rows,cols = activity.shape
 	euclidean_dist_mat = zeros((rows,cols),Float)
