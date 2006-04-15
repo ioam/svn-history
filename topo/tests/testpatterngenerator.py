c29c15d079843a9205850b1ca65f374efe41f923
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-04-15 12:59:43 +0000

    Updated test.

--- topo/tests/testpatterngenerator.py
+++ topo/tests/testpatterngenerator.py
@@ -17,7 +17,6 @@ from utils import assert_array_equal
 
 # CEBHACKALERT: needs writing so that it tests PatternGenerator properly!
 
-
 class TestPatternGenerator(unittest.TestCase):
 
     def test_orientation(self):
@@ -30,16 +29,18 @@ class TestPatternGenerator(unittest.TestCase):
                         [0, 0, 1, 1, 0, 0],
                         [0, 0, 1, 1, 0, 0],
                         [0, 0, 0, 0, 0, 0]])
-        bounds = BoundingBox(points=((-0.8,-0.8),(0.8,0.8)))
-        density = 16
 
-        rect_width = 0.3
-        rect_height = 0.9
-        rect = topo.patterns.basic.Rectangle(scale=0.9,
-                                             aspect_ratio=(rect_width/rect_height),
-                                             density=4,bounds=bounds)
+        bounds = BoundingBox(radius=0.3)
+        density = 10
+        width = 2.0/density
+        height = 4.0/density
+        
+        rect = topo.patterns.basic.Rectangle(size=height,
+                                             aspect_ratio=width/height,
+                                             density=density,bounds=bounds)
+
         assert_array_equal(rect(),target)
-#        print rect()
+
         
 
 
