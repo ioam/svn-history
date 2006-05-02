5c444b3ce4d791483e5a21bc5edb6bebc2dc6c64
James A. Bednar <jbednar@inf.ed.ac.uk>
2006-05-02 14:50:38 +0000

    Renamed Simulator to Simulation

--- topo/base/simulator.py
+++ topo/base/simulator.py
@@ -1,7 +1,7 @@
 """
-The topographica simulator module.  
+The Topographica Simulation class.  
 
-The Simulator object is the central object of a Topographica
+The Simulation object is the central object of a Topographica
 simulation.  It handles the simulation clock and maintains
 communication between the components of the simulation.
 
@@ -10,10 +10,10 @@ called EventProcessors (EPs).  EventProcessors generate data-carrying
 events, which are routed through the graph to other EventProcessors
 via delayed connections.
 
-The simulator is modular: EventProcessors should inherit from the root
+The simulation is modular: EventProcessors should inherit from the root
 EventProcessor class.  This class manages the EP's connections to
 other EPs, as well as the mechanics of sending events to other EPs
-(through the simulator).  The EventProcessor class defines the basic
+(through the simulation).  The EventProcessor class defines the basic
 EP programming interface.
 
 Formally, a simulation event is a tuple:
@@ -33,7 +33,7 @@ Formally, a simulation event is a tuple:
 A simulation begins by giving each EventProcessor an opportunity to
 send any initial events.  It then proceeds by processing and
 delivering events to EventProcessors in time order.  After all events
-for the current time are processed, the simulator gives each
+for the current time are processed, the simulation gives each
 EventProcessor a chance to do any final computation, after which
 simulation time skips to the time of the earliest event remaining in
 the queue.
@@ -63,10 +63,10 @@ events to itself than it sends out to other EPs.  In this case the
 self connections might have src_port = 'recurrent'.
 
 
-Current Simulator
+Current Simulation
 
-The current Simulator, which can be accessed through the instance
-of the Singleton class SimSingleton(), is the last Simulator to
+The current Simulation, which can be accessed through the instance
+of the Singleton class SimSingleton(), is the last Simulation to
 have been created with register=True (the default).
 
 
@@ -80,7 +80,7 @@ from copy import copy, deepcopy
 from fixedpoint import FixedPoint
 
 SLEEP_EXCEPTION = "Sleep Exception"
-STOP = "Simulator Stopped"
+STOP = "Simulation Stopped"
 
 Forever = FixedPoint(-1)
 
@@ -90,7 +90,7 @@ Forever = FixedPoint(-1)
 
 def get_active_sim():
     """
-    Return the active simulator.
+    Return the active simulation.
     """
     return SimSingleton()
 
@@ -122,7 +122,7 @@ class Singleton(object):
 class SimSingleton(Singleton):
     """
     A singleton class, the instance of which allows access to
-    the current simulator.
+    the current simulation.
     """
     actual_sim = None
 
@@ -130,28 +130,28 @@ class SimSingleton(Singleton):
     # SimSingleton, as they do at the moment?
     def __repr__(self):
         """
-        Return the simulator's __repr__().
+        Return the simulation's __repr__().
         """
         return self.actual_sim.__repr__()
 
     def __str__(self):
         """
-        Return the simulator's __str__().
+        Return the simulation's __str__().
         """
         return self.actual_sum.__str__()
     
     def init(self):
         """
-        Create a new simulator.
+        Create a new simulation.
         """
-        # Simulator will call this object's change_sim()
-        Simulator()
+        # Simulation will call this object's change_sim()
+        Simulation()
 
     def __getattribute__(self,attribute):
         """
         If the SimSingleton object has the attribute, return it; if the
         actual_sim has the attribute, return it; otherwise, an AttributeError
-        relating to Simulator will be raised (as usual).
+        relating to Simulation will be raised (as usual).
         """
         try:
             return object.__getattribute__(self,attribute)
@@ -183,18 +183,18 @@ class SimSingleton(Singleton):
         """
         Set actual_sim to be new_sim.
         """
-        assert isinstance(new_sim,Simulator), "Can only change to a Simulator instance."
+        assert isinstance(new_sim,Simulation), "Can only change to a Simulation instance."
         self.actual_sim=new_sim
             
     def __getitem__(self,item_name):
         """
-        Allow dictionary-style access to the simulator.
+        Allow dictionary-style access to the simulation.
         """
         return self.actual_sim[item_name]
 
     def __setitem__(self,item_name,item_value):
         """
-        Allow dictionary-style access to the simulator.
+        Allow dictionary-style access to the simulation.
         """
         self.actual_sim[item_name]=item_value
 
@@ -220,17 +220,17 @@ class EventProcessor(ParameterizedObject):
         self.in_connections = []
         self.out_connections = {None:[]}
 
-        # The simulator link is not set until the call to add()
+        # The simulation link is not set until the call to add()
         self.simulator = None
 
 
     # if extra parameters are required for an EP subclass, a
-    # dictionary could be added to Simulator.connect() to hold
+    # dictionary could be added to Simulation.connect() to hold
     # them, and passed on here
     def _connect_to(self,conn):
         """
         Add a connection to dest/port with a delay (default=0).
-        Should only be called from Simulator.connect().
+        Should only be called from Simulation.connect().
         """
 
         if not conn.src_port in self.out_connections:
@@ -242,7 +242,7 @@ class EventProcessor(ParameterizedObject):
     def _connect_from(self,conn):
         """
         Add a connection from src/port with a delay (default=0).
-        Should only be called from Simulator.connect().  The extra
+        Should only be called from Simulation.connect().  The extra
         keyword arguments in **args contain arbitrary connection
         parameters that can be interpreted by EP subclasses as
         needed.  
@@ -253,9 +253,9 @@ class EventProcessor(ParameterizedObject):
 
     def start(self):
         """        
-        Called by the simulator when the EventProcessor is add()ed to the simulator.
+        Called by the simulation when the EventProcessor is add()ed to the simulation.
 
-        If an EventProcessor needs to have any code run when it is added to the simulator,
+        If an EventProcessor needs to have any code run when it is added to the simulation,
         it should be in this method.
         """
         pass
@@ -270,14 +270,14 @@ class EventProcessor(ParameterizedObject):
 
     def input_event(self,src,src_port,dest_port,data):
         """
-        Called by the simulator to dispatch an event on the given port
+        Called by the simulation to dispatch an event on the given port
         from src.  (By default, does nothing.)
         """
         pass
     
     def pre_sleep(self):
         """
-        Called by the simulator before sleeping.  Allows the event processor
+        Called by the simulation before sleeping.  Allows the event processor
         to send any events that must be sent before time advances.
         (By default, does nothing.)
         """
@@ -383,28 +383,28 @@ class CommandEvent(Event):
 # things and their locations to be saved?
 # e.g.
 # save_state({"__main__.__dict__": [BoundingBox, x]})
-# where save_state() also calls save_snapshot to get the simulator.
+# where save_state() also calls save_snapshot to get the simulation.
 
 
 
             
 # CEBHACKALERT: need to rename to simulation, etc.
 
-# Simulator stores its events in a linear-time priority queue (i.e., a
+# Simulation stores its events in a linear-time priority queue (i.e., a
 # sorted list.) For efficiency, e.g. for spiking neuron simulations,
 # we'll probably need to replace the linear priority queue with a more
 # efficient one.  Jeff has an O(log N) minheap implementation, but
 # there are likely to be many others to select from.
 #
-class Simulator(ParameterizedObject):
+class Simulation(ParameterizedObject):
     """
-    A simulator class that uses a simple sorted event list (instead of
+    A simulation class that uses a simple sorted event list (instead of
     e.g. a sched.scheduler object) to manage events and dispatching.
 
     The Parameter 'register' indicates whether or not to register the
-    Simulator as being part of Topographica; that is, register would
-    be false for use of the Simulator outside Topographica. With
-    register True, the Simulator is set to be the 'active_sim', which
+    Simulation as being part of Topographica; that is, register would
+    be false for use of the Simulation outside Topographica. With
+    register True, the Simulation is set to be the 'active_sim', which
     other Topographica commands and classes might look for or want to
     be informed about.
     """
@@ -419,18 +419,18 @@ class Simulator(ParameterizedObject):
         default=[],
         doc="""
             List of string commands that will be exec'd in
-            __main__.__dict__ before this simulator is unpickled.
+            __main__.__dict__ before this simulation is unpickled.
             """)
               
 
     def __init__(self,**config):
         """
-        The simulator constructor takes one keyword parameter:
+        The simulation constructor takes one keyword parameter:
         
-           step_mode = debugging flag, causing the simulator to stop
+           step_mode = debugging flag, causing the simulation to stop
                        before each tick.
         """
-        super(Simulator,self).__init__(**config)
+        super(Simulation,self).__init__(**config)
 
         # time is a fixed-point number
         # JABHACKALERT! Shouldn't this be at least 4 decimal places by default?
@@ -449,41 +449,41 @@ class Simulator(ParameterizedObject):
     def __getitem__(self,item_name):
         """
         Return item_name if it exists as an EventProcessor in
-        the Simulator. See objects().
+        the Simulation. See objects().
         """
         if not isinstance(item_name,str):
-            raise TypeError("Expected string (objects in the Simulator are indexed by name).")
+            raise TypeError("Expected string (objects in the Simulation are indexed by name).")
         try:
             return self.objects()[item_name]
         except KeyError:
-            raise AttributeError("Simulator doesn't contain '"+item_name+"'.")
+            raise AttributeError("Simulation doesn't contain '"+item_name+"'.")
 
 
     def __setitem__(self,ep_name,ep):
         """
-        Add ep to the simulator, setting its name to ep_name.
+        Add ep to the simulation, setting its name to ep_name.
         ep must be an EventProcessor.
 
-        If ep_name already exists in the simulator, ep overwrites
+        If ep_name already exists in the simulation, ep overwrites
         the original object (as for a dictionary).
 
-        If the ep itself already exists in the simulator, a
+        If the ep itself already exists in the simulation, a
         warning is printed and the ep is not added.
 
         Note, EventProcessors do not necessarily have to be added to
-        the simulator to be used in a simulation, but they will not
-        receive the start() message.  Adding a node to the simulator
-        also sets the backlink node.simulator, so that the node can
-        enqueue events and read the simulator clock.
+        the simulation to be used, but they will not receive the
+        start() message.  Adding a node to the simulator also sets the
+        backlink node.simulator, so that the node can enqueue events
+        and read the simulator clock.
         """
         if not isinstance(ep_name,str):
-           raise TypeError("Expected string for item name (EPs in the Simulator are indexed by name).")
+           raise TypeError("Expected string for item name (EPs in the Simulation are indexed by name).")
 
         if not isinstance(ep,EventProcessor):
-           raise TypeError("Expected EventProcessor: objects in the Simulator must be EPs.")
+           raise TypeError("Expected EventProcessor: objects in the Simulation must be EPs.")
 
         if ep in self._event_processors.values():
-            self.warning("EventProcessor "+str(ep)+" () already exists in the simulator and will not be added.")
+            self.warning("EventProcessor "+str(ep)+" () already exists in the simulation and will not be added.")
         else:
             ep.name=ep_name
             # CEBHACKALERT: if this is overwriting an existing EP,
@@ -505,11 +505,11 @@ class Simulator(ParameterizedObject):
     
     def run(self,duration=Forever,until=Forever):
         """
-        Run the simulator by starting the event scheduler.
+        Run the simulation by starting the event scheduler.
 
         parameters:
-          duration = time to run in simulator time. Default: run indefinitely.
-          until    = time to stop in simulator time. Default: run indefinitely.
+          duration = time to run in simulation time. Default: run indefinitely.
+          until    = time to stop in simulation time. Default: run indefinitely.
           (note if both duration and until are used, they both will apply.)
         """
         # Complicated expression for min(time+duration,until)
@@ -600,7 +600,7 @@ class Simulator(ParameterizedObject):
 
     def enqueue_event_abs(self,time,src,dest,src_port=None,dest_port=None,data=None):
         """
-        Enqueue an event at an absolute simulator clock time.
+        Enqueue an event at an absolute simulation clock time.
         """
         self.debug("Enqueue absolute: from", (src,src_port),"to",(dest,dest_port),
                    "at time",self._time,"for time",time)
@@ -621,7 +621,7 @@ class Simulator(ParameterizedObject):
 
     def enqueue_event_rel(self,delay,src,dest,src_port=None,dest_port=None,data=None):
         """
-        Enqueue an event at a time relative to the current simulator clock.
+        Enqueue an event at a time relative to the current simulation clock.
         """
         self.enqueue_event_abs(self._time+delay,
                                src,dest,src_port,dest_port,data)
@@ -677,7 +677,7 @@ class Simulator(ParameterizedObject):
         Connect the source to the destination, at the appropriate ports,
         if any are given.
 
-        src and dest should already be part of the simulator.
+        src and dest should already be part of the simulation.
 
         Returns the connection that was created.
 
@@ -696,18 +696,15 @@ class Simulator(ParameterizedObject):
     # CEBHACKALERT: why not just event_processors()?
     ### It might be possible to come up with a more expressive name
     ### for this function.  It should mean 'anything that exists in
-    ### the simulator universe, i.e. all EventProcessors'.
+    ### the simulation universe, i.e. all EventProcessors'.
     ###
     def objects(self,baseclass=EventProcessor):
         """
-        Return a list of simulator objects having the specified base
-        class.  All simulator objects have a base class of
+        Return a list of simulation objects having the specified base
+        class.  All simulation objects have a base class of
         EventProcessor, and so the baseclass must be either
         EventProcessor or one of its subclasses.
         """
         return dict([(ep_name,ep)
                      for (ep_name,ep) in self._event_processors.items()
                      if isinstance(ep,baseclass)])
-        
-### Temporary: Simulation is an alias for Simulator
-Simulation=Simulator
