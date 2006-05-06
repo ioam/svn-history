cb15ebec3e9f8d7594a2eeaa1701e62dfdceffb6
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-05-06 23:56:13 +0000

    SharedWeightCFProjection needs to wrap its cfs list in a better way, so we don't need separate response fns etc. But for now, I've done this enough to allow a C++ replacement of the shared weight response fn, which we badly need to improve performance.

--- topo/projections/basic.py
+++ topo/projections/basic.py
@@ -9,6 +9,7 @@ $Id$
 
 __version__ = "$Revision$"
 
+import copy
 import Numeric
 
 # So all Projections are present in this package
@@ -25,13 +26,24 @@ from topo.outputfns.basic import IdentityOF
 
 
 
+# CEBHACKALERT: This file contains numerous hacks, all around
+# to allow there to be a collection of cfs which all share one
+# set of weights. The classes in here could be implemented much
+# better, it just hasn't been done yet.
+# SharedWeightCFProjection should have the list of 'cfs', and
+# it should contain CFs that allow the standard CFP functions to work as
+# normal with the list, but these CFs should not each have a set of weights.
+# ConnectionField could e.g. be subclassed. At the moment,
+# a shared ConnectionField is in a list of 'DummyCF' wrappers.
+
+
 class SharedWeightCFPOutputFn(CFPOutputFn):
     single_cf_fn = OutputFnParameter(default=IdentityOF())
     
     def __call__(self, cfs, output_activity, norm_values=None, **params):
         """Apply the specified single_cf_fn to every CF."""
         if type(self.single_cf_fn) is not IdentityOF:
-            cf = cfs[0]
+            cf = cfs[0][0]
             self.single_cf_fn(cf.weights)
 
 
@@ -40,27 +52,45 @@ class SharedWeightCFPResponseFn(CFPResponseFn):
     Response function accepting a single CF applied to all units.
     Otherwise similar to GenericCFResponseFn.
     """
-    ### JABALERT: It may be possible to eliminate this, and instead
-    ### implemented a wrapper around the cfs argument of
-    ### GenericCFResponseFn, so that GenericCFResponseFn will work
-    ### whether there is an actual full set of cfs or not.
     single_cf_fn = ResponseFnParameter(default=Mdot())
     
-    def __call__(self, cf, cf_slice_and_bounds, input_activity, activity, strength):
+    def __call__(self, cfs, input_activity, activity, strength):
         rows,cols = activity.shape
         single_cf_fn = self.single_cf_fn
+        shared_weights = cfs[0][0].weights
+        
         for r in xrange(rows):
             for c in xrange(cols):
                 # get right submatrix from input_activity
-                act_r1,act_r2,act_c1,act_c2 = (cf_slice_and_bounds[r][c])[0]
+                act_r1,act_r2,act_c1,act_c2 = cfs[r][c].slice_array
                 X = input_activity[act_r1:act_r2,act_c1:act_c2]
                 # get right submatrix from weights
-                w_r1,w_r2,w_c1,w_c2 = (cf_slice_and_bounds[r][c])[2]
-                weights = cf.weights[w_r1:w_r2,w_c1:w_c2]
+                w_r1,w_r2,w_c1,w_c2 = cfs[r][c].weights_slice
+                weights = shared_weights[w_r1:w_r2,w_c1:w_c2]
 
                 activity[r,c] = single_cf_fn(X,weights)
         activity *= strength
+
+
+# CEBHACKALERT: this is a temporary implementation!
+# It isn't the right way to allow a collection of CFs
+# sharing one set of weights.
+class DummyCF(ParameterizedObject):
+
+    weights = property(lambda self: self.cf.weights)
+    
+    def __init__(self,cf,x,y,bounds_template):
+        cf.x=x; cf.y=y
+        cf.offset_bounds(bounds_template)
+
+        self.bounds = copy.deepcopy(cf.bounds)
+        self.slice_array =  copy.deepcopy(cf.slice_array)
+        self.weights_slice = cf.get_slice(bounds_template)
         
+        self.cf = cf
+        self.x=x; self.y=y
+
+
 
 # CEBHACKALERT: users should not access .sharedcf or .cfs directly,
 # but should use .cf(r,c). That all needs to be cleaned up, here and
@@ -97,56 +127,42 @@ class SharedWeightCFProjection(CFProjection):
         # do want anything that Projection defines.
         super(SharedWeightCFProjection,self).__init__(initialize_cfs=False,**params)
 
-        self.sharedcf=self.cf_type(0,0,
-                                   self.src,
-                                   self.bounds_template,
-                                   self.weights_generator,
-                                   self.mask_template,
-                                   self.weights_output_fn.single_cf_fn)
+        self.__sharedcf=self.cf_type(0,0,
+                                     self.src,
+                                     self.bounds_template,
+                                     self.weights_generator,
+                                     self.mask_template,
+                                     self.weights_output_fn.single_cf_fn)
 
-        # CEBHACKALERT: Calculate and store the the slice of the
-        # sheet, the bounds, and the slice of the weights matrix for
-        # each unit. Although there is only one cf here in the code,
-        # it is used to represent units all round the sheet - and
-        # units around the edge might not be able to have the full
-        # matrix. To avoid calculating the correct slice of the
-        # weights matrix to take at each activation, it's stored
-        # in cf.slice_and_bounds[2]. The bounds are also stored for
-        # access by plotting routines in [1]. The slice of the sheet to
-        # which these bounds correspond is stored in [0].
-
-        self.cf_slice_and_bounds = []
-        
-        cf = self.sharedcf
+        cflist = []
+        scf = self.__sharedcf
+        bounds_template = self.bounds_template
         for y in self.dest.sheet_rows()[::-1]:
             row = []
             for x in self.dest.sheet_cols():
-                cf.x,cf.y = x,y
-                cf.offset_bounds(self.bounds_template)
-                weights_slice = cf.get_slice(self.bounds_template)
-                row.append((cf.slice_array,cf.bounds,weights_slice))
-            self.cf_slice_and_bounds.append(row)
+                cf = DummyCF(scf,x,y,bounds_template)
+                row.append(cf)
+            cflist.append(row)
 
         ### JABHACKALERT: cfs is a dummy, here only so that learning will
         ### run without an exception
-        self.cfs = [self.sharedcf]
-
+        #self.cfs = [self.sharedcf]
+        self.cfs = cflist
+        
     
     def cf(self,r,c):
         """Return the shared ConnectionField, for all coordinates."""
         # CEBHACKALERT: there's probably a better way to do this than
         # just replacing the sharedcf's bounds and slice_array. 
-        bounds = self.cf_slice_and_bounds[r][c][1]
-        slice_array = self.cf_slice_and_bounds[r][c][0]
-        self.sharedcf.bounds = bounds
-        self.sharedcf.slice_array = slice_array
-        return self.sharedcf
+        self.__sharedcf.bounds = self.cfs[r][c].bounds
+        self.__sharedcf.slice_array = self.cfs[r][c].slice_array
+        return self.__sharedcf
 
     
     def activate(self,input_activity):
         """Activate using the specified response_fn and output_fn."""
         self.input_buffer = input_activity
-        self.response_fn(self.sharedcf, self.cf_slice_and_bounds, input_activity, self.activity, self.strength)
+        self.response_fn(self.cfs, input_activity, self.activity, self.strength)
         self.activity = self.output_fn(self.activity)
 
 
