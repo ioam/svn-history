e20ef52b5f427689c24da59e938f802f8a3e2ab1
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-05-16 22:21:03 +0000

    Added a comment.

--- setup.bat
+++ setup.bat
@@ -1,3 +1,9 @@
 @ECHO OFF
+
+REM *********************************************************
+REM This script sets up a CVS-controlled copy of Topographica
+REM on Windows, using files in the external\mswin directory.
+REM *********************************************************
+
 cd external\mswin
 setup.bat
