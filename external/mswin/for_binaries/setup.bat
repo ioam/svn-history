d48044097fb6f2644a0b8da6cebb8c7ebe4a8325
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-05-19 10:42:58 +0000

    Updated with changes to setup.py.

--- external/mswin/for_binaries/setup.bat
+++ external/mswin/for_binaries/setup.bat
@@ -5,6 +5,6 @@ REM Called by installation program after it's done copying files.
 
 cd ..
 set instdir=%CD%
-python_topo\python.exe setup\setup.py "%instdir%" "notcvs"
+python_topo\python.exe setup\setup.py "%instdir%"
 
 rmdir /Q /S setup\
