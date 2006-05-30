f6c9472b1a8e5a8e1e59b2aa266291a8a0c69db3
Yoonsuck Choe <choe@tamu.edu>
2006-05-30 01:46:30 +0000

    Added DynamicThreshold_CFPRF_DotProduct_opt, a CFPRF_DotProduct_opt with dynamic thresholding. Right now, it only has a fixed threshold. It uses the optimized __call__ in its parent, while the thresholding part is plain Python code.

--- topo/responsefns/basic.py
+++ topo/responsefns/basic.py
@@ -6,3 +6,27 @@ $Id$
 __version__='$Revision$'
 
 from topo.base.functionfamilies import DotProduct,ResponseFn
+from topo.responsefns.optimized import CFPRF_DotProduct_opt
+from topo.base.parameterclasses import Number
+
+class DynamicThreshold_CFPRF_DotProduct_opt(CFPRF_DotProduct_opt):
+    """CFPRF_DotProduct_opt with dynamic thresholding"""
+
+    threshold = Number(default=0.3,bounds=(0,None), doc="Baseline threshold")
+    decay_rate = Number(default=1.0,bounds=(0,None), doc="Dynamic threshold decay rate")
+    absolute_refractory = Number(default=0.0,bounds=(0,None), doc="Absolute refractory period")
+      
+
+    def __call__(self, cfs, input_activity, activity, strength, **params):
+	# call parent's version
+	super(DynamicThreshold_CFPRF_DotProduct_opt,self).__call__(cfs, input_activity, activity, strength, **params)
+ 
+	# apply threshold
+        rows,cols = activity.shape
+        for r in xrange(rows):
+            for c in xrange(cols):
+		if (activity[r,c] > self.threshold):
+                    activity[r,c] = 1.0
+		else:
+                    activity[r,c] = 0.0
+
