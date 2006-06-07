157c3f3aec0fdeeb6b87b5641f15453e08079957
James A. Bednar <jbednar@inf.ed.ac.uk>
2006-06-07 20:30:01 +0000

    Added oldnews page

---
+++ doc/Home/oldnews_text.php
@@ -0,0 +1,13 @@
+<P>Older news:
+
+<p><b>22 December 05:</b> Version 0.8.1 released, including numerous
+bugfixes, more flexible plotting (including weight colorization),
+user-controllable optimization, properties panels, more-useful
+<A TARGET="_top" href="../Reference_Manual/index.html">reference manual</A>,
+image input patterns, and a prototype graphical
+model editor.  <!-- Plus SOMs with selectable Projections -->
+
+<p><b>8 November 05:</b> New site launched with Topographica version
+0.8.0, including a new
+ <a target="_top" href="../Tutorials/lissom_or.html">LISSOM tutorial</a>.
+(<a target="_top" href="../images/051107_topographica_screen_shot_white.png">Linux screenshot</a>).
