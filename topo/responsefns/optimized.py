08b0835c40febcacc759ebb372be0e284f92f25e
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-06-07 22:58:37 +0000

    Added comment to an ALERT.

--- topo/responsefns/optimized.py
+++ topo/responsefns/optimized.py
@@ -128,6 +128,8 @@ class CFPRF_EuclideanDistance_opt(CFPResponseFn):
                         float *wi = wj;
                         for (int j=cc1; j<cc2; ++j) {
 			    // JCALERT! find power notation in C.
+                            // CB: weave doesn't support power notation
+                            // (according to weave's manual).
                             tot += (*wi - *xi) * (*wi - *xi);
                             ++wi;
                             ++xi;
