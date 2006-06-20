8eb91e7822d80204e6e527e37778444fca9a6411
James A. Bednar <jbednar@inf.ed.ac.uk>
2006-06-20 22:56:31 +0000

    Clarified what the problem is for disks and rings

--- topo/misc/patternfns.py
+++ topo/misc/patternfns.py
@@ -9,22 +9,26 @@ $Id$
 """
 __version__='$Revision$'
 
+
 from math import pi
+
 from Numeric import where,maximum,cos,sin,sqrt,less_equal,divide,greater_equal,bitwise_xor
+
 from topo.base.arrayutils import exp
 
-# CEB:
-# Divide is imported from Numeric so that mathematical expressions with scalars
-# (such as exp(-(3.0/0.0)) ) are evaluated correctly. Unfortunately this makes
-# such expressions more difficult to read. How can Numeric's / operator
-# be made to override Python's / operator for scalars?
+
+# CEB: Divide is imported from Numeric so that mathematical
+# expressions with scalars (such as exp(-(3.0/0.0)) ) are evaluated
+# correctly. Unfortunately this makes such expressions more difficult
+# to read. How can Numeric's / operator be made to override Python's /
+# operator for scalars?
 
 
 def gaussian(x, y, xsigma, ysigma):
     """
     Two-dimensional oriented Gaussian pattern (i.e., 2D version of a
-    bell curve, like a normal distribution but without necessarily
-    summing to 1.0).
+    bell curve, like a normal distribution but not necessarily summing
+    to 1.0).
     """
     x_w = divide(x,xsigma)
     y_h = divide(y,ysigma)
@@ -52,18 +56,11 @@ def line(y, thickness, gaussian_width):
     return where(gaussian_y_coord<=0, 1.0, falloff)
 
 
-# CEBHACKALERT:
-# I think I have something wrong with gaussian falloffs for ring() and disk()
-# (c.f. line())
-
-# CEBHACKALERT:
-# I have used 'ellipse' in a confusing way. Change the variable and function names.
-
 def disk(x, y, width, height, gaussian_width):
     """
     Elliptical disk with Gaussian fall-off after the solid central region.
     """
-    disk_perimeter = __ellipse(x,y,width/2.0,height/2.0)  
+    disk_perimeter = __ellipse_dist(x,y,width/2.0,height/2.0)  
     disk = greater_equal(disk_perimeter,0)
 
     sigmasq = gaussian_width*gaussian_width
@@ -76,10 +73,10 @@ def ring(x, y, width, height, thickness, gaussian_width):
     """
     Elliptical ring (annulus) with Gaussian fall-off after the solid ring-shaped region.
     """    
-    ellipse = __ellipse(x,y,width/2.0,height/2.0)  
+    ellipse_dist = __ellipse_dist(x,y,width/2.0,height/2.0)  
 
-    inner_perimeter = ellipse - thickness
-    outer_perimeter = ellipse + thickness
+    inner_perimeter = ellipse_dist - thickness
+    outer_perimeter = ellipse_dist + thickness
 
     ring = bitwise_xor(greater_equal(inner_perimeter,0.0),greater_equal(outer_perimeter,0.0)) 
 
@@ -90,9 +87,21 @@ def ring(x, y, width, height, thickness, gaussian_width):
     return maximum(inner_falloff,maximum(outer_falloff,ring))
 
 
-def __ellipse(x,y,a,b):
+# JABHACKALERT:  The __ellipse_dist function needs to be reimplemented
+# with a function that actually computes the distance from point (x,y)
+# to the nearest point on the ellipse; this one does not.  The result
+# is that the falloffs for ring() and disk() are not correct; try 
+# comparing them with those for line().
+
+def __ellipse_dist(x,y,a,b):
     """
-    Return the ellipse specified by (x/a)^2 + (y/b)^2 = 1.
+    For an ellipse consisting of all points (x1,y1) where 
+    (x1/a)^2 + (y1/b)^2 = 1, should return the distance from (x,y) to the
+    nearest point on the ellipse.  Negative distance represents points
+    outside of the ellipse.
+    
+    Bug: the value returned is currently related to the distance, but is
+    not the same quantity; it needs to be corrected.
     """
     x_a = divide(x,float(a))
     y_b = divide(y,float(b))
