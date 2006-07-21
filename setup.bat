2b3e1e05d48b995537e2ec2d4edf56af1c532804
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-07-21 21:46:25 +0000

    Moved to topographica-win module or deleted if unused.

--- setup.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-REM *********************************************************
-REM This script sets up a CVS-controlled copy of Topographica
-REM on Windows, using files in the external\mswin directory.
-REM *********************************************************
-
-cd external\mswin
-setup.bat
