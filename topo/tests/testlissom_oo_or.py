b7e09afcab7bc4b5721af8164af78b0a8062d3ea
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-10-06 11:04:52 +0000

    Tests V1 activities at t=1, t=100, and t=210.

--- topo/tests/testlissom_oo_or.py
+++ topo/tests/testlissom_oo_or.py
@@ -1,9 +1,15 @@
 """
 Slow test to check that the results of examples/lissom_oo_or.ty
 have not changed.
+
+The current simulation's results are checked against the results
+stored in testlissom_oo_or.data.
+
+You can generate a new testlissom_oo_or.data file with the following command:
+  ./topographica -c "from topo.tests.testlissom_oo_or import GenerateData; GenerateData()"
 """
 
-import pickle, unittest, copy
+import pickle, unittest, copy, __main__
 
 import topo
 
@@ -11,26 +17,46 @@ from topo.tests.utils import assert_array_equal
 
 
 def GenerateData():
-    execfile("examples/lissom_oo_or.ty")
+
+    #### call the lissom_oo_or.ty example with density 8
+    default_density=8.0
+    execfile("examples/lissom_oo_or.ty")        
+    # CEBHACKALERT: I found the following line was necessary, so presumably
+    # I'm doing something wrong with execfile and where it runs the script.
+    exec "from topo.base.boundingregion import BoundingBox" in __main__.__dict__
+    ####
+
+    # t=1 to indicate likely activity calculation error
+    # t=100 to indicate learning errors (e.g. rate)
+    # t=210 to indicate bounds changing errors
+    
     topo.sim.run(1)
     t1=copy.deepcopy(topo.sim['V1'].activity)
-    topo.sim.run(1)
-    t2=copy.deepcopy(topo.sim['V1'].activity)
+    topo.sim.run(99)
+    t100=copy.deepcopy(topo.sim['V1'].activity)
+    topo.sim.run(110)
+    t210=copy.deepcopy(topo.sim['V1'].activity)
 
-    pickle.dump((t1,t2),open('topo/tests/testlissom_oo_or.data','wb'),2)
+    pickle.dump((t1,t100,t210),open('topo/tests/testlissom_oo_or.data','wb'),2)
 
 
 class Test_lissom_oo_or(unittest.TestCase):
 
     def test_activity(self):
         """
-        
-        ('true' means what it was last time GenerateData was called)
-        """
+        Check that the current simulation's activity values match those from the last
+        time GenerateData() was called.
+        """        
         trueV1activities = pickle.load(open("topo/tests/testlissom_oo_or.data","r"))
-        
-        execfile("examples/lissom_oo_or.ty")
-        
+
+        #### call the lissom_oo_or.ty example with density 8
+        default_density=8.0
+        execfile("examples/lissom_oo_or.ty")        
+        # CEBHACKALERT: I found the following line was necessary, so presumably
+        # I'm doing something wrong with execfile and where it runs the script.
+        exec "from topo.base.boundingregion import BoundingBox" in __main__.__dict__
+        ####
+
         topo.sim.run(1)
         # CEBHACKALERT: the single line below ought to work instead of
         # the 4 beneath it!
@@ -39,13 +65,21 @@ class Test_lissom_oo_or(unittest.TestCase):
         for i in range(topo.sim['V1'].activity.shape[0]):
             for j in range(topo.sim['V1'].activity.shape[1]):
                 self.assertEqual(topo.sim['V1'].activity[i,j],trueV1activities[0][i,j])
-        
-        topo.sim.run(1)
-        #assert_array_equal(trueV1activities[0],topo.sim['V1'].activity)
+
+        topo.sim.run(99)
+        #assert_array_equal(trueV1activities[1],topo.sim['V1'].activity)
         self.assertEqual(topo.sim['V1'].activity.shape,trueV1activities[1].shape)
         for i in range(topo.sim['V1'].activity.shape[0]):
             for j in range(topo.sim['V1'].activity.shape[1]):
                 self.assertEqual(topo.sim['V1'].activity[i,j],trueV1activities[1][i,j])
+
+        
+        topo.sim.run(110)
+        #assert_array_equal(trueV1activities[2],topo.sim['V1'].activity)
+        self.assertEqual(topo.sim['V1'].activity.shape,trueV1activities[2].shape)
+        for i in range(topo.sim['V1'].activity.shape[0]):
+            for j in range(topo.sim['V1'].activity.shape[1]):
+                self.assertEqual(topo.sim['V1'].activity[i,j],trueV1activities[2][i,j])
 
         
 
