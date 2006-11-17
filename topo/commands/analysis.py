55f0eefaa95912284d62f32dde1d42a91af921f1
James A. Bednar <jbednar@inf.ed.ac.uk>
2006-11-17 17:26:36 +0000

    Cleaned up comments, imports, and formatting, without altering the function

--- topo/commands/analysis.py
+++ topo/commands/analysis.py
@@ -3,44 +3,42 @@ User-level analysis commands, typically for measuring or generating SheetViews.
 
 $Id$
 """
+__version__='$Revision$'
 
 
 from Numeric import array, zeros, Float,size, shape
 from math import pi
-
+from copy import deepcopy
 
 import topo
-import random
-import copy
 
 from topo.analysis.featuremap import MeasureFeatureMap
 from topo.base.arrayutils import octave_output, centroid
 from topo.base.cf import CFSheet, CFProjection, Projection
-from topo.base.sheet import Sheet
-from topo.sheets.lissom import LISSOM
-from topo.base.sheetview import SheetView, ProjectionView
-import topo.base.patterngenerator
-from topo.commands.basic import pattern_present
-from topo.patterns.basic import SineGrating, Gaussian
-from topo.sheets.generatorsheet import GeneratorSheet
-from topo.base.projection import ProjectionSheet
-from topo.misc.numbergenerators import UniformRandom
 from topo.base.parameterclasses import DynamicNumber
+from topo.base.projection import ProjectionSheet
+from topo.base.sheet import Sheet
 from topo.base.sheet import Sheet
 from topo.base.sheetview import SheetView
-
+from topo.base.sheetview import SheetView, ProjectionView
+from topo.commands.basic import pattern_present
+from topo.misc.numbergenerators import UniformRandom
 from topo.misc.utils import frange
+from topo.patterns.basic import SineGrating, Gaussian
+from topo.sheets.generatorsheet import GeneratorSheet
+from topo.sheets.lissom import LISSOM
+
 
 class Feature(object):
     """
-    Stores the feature parameters required for generating one map.
+    Stores the parameters requires for generating a map of one input feature.
     """
 
     def __init__(self, name, range=None, step=0.0, values=None, cyclic=False):
          """
          Users can provide either a range and a step size, or a list of values.
-         If a list of values is supplied a range is optional, it defaults to
-         the min and max in the list of values.
+         If a list of values is supplied, the range can be omitted unless the
+         default of the min and max in the list of values is not appropriate.
          """ 
          self.name=name
          self.step=step
@@ -83,8 +81,8 @@ class PatternPresenter(object):
         input_pattern=gen_list.keys()
 
         inputs = dict().fromkeys(topo.sim.objects(GeneratorSheet),self.gen)
-        gen_copy1=copy.deepcopy(self.gen)
-        gen_copy2=copy.deepcopy(self.gen)
+        gen_copy1=deepcopy(self.gen)
+        gen_copy2=deepcopy(self.gen)
 
         ###TRALERT: for disparity maps 
         if features_values.has_key("disparity") and len(input_pattern)==2:
@@ -119,6 +117,8 @@ class PatternPresenter(object):
         pattern_present(inputs, self.duration, learning=False,
                         apply_output_fn=self.apply_output_fn)
 
+
+### JABALERT: user_function needs a more informative name, such as patternpresenter
 def measure_or_pref(num_phase=18,num_orientation=4,frequencies=[2.4],
                     scale=0.3,offset=0.0,display=False,
                     user_function=PatternPresenter(pattern_generator=SineGrating(),apply_output_fn=False,duration=0.175)):
@@ -132,15 +132,15 @@ def measure_or_pref(num_phase=18,num_orientation=4,frequencies=[2.4],
     num_phase, num_orientation, and frequencies parameters.  The
     particular pattern used is determined by the user_function
     argument, which defaults to a sine grating presented for a short
-    duration.  By convention, the Topographica example files
+    duration.  By convention, most Topographica example files
     are designed to have a suitable activity pattern computed by
     that time, but the duration will need to be changed for other
     models that do not follow that convention.
     """
-
     # Could consider having scripts set a variable for the duration,
     # based on their own particular model setup, and to have it read
     # from here.  Instead, assumes a fixed default duration right now...
+
     if num_phase <= 0 or num_orientation <= 0:
         raise ValueError("num_phase and num_orientation must be greater than 0")
 
@@ -153,17 +153,31 @@ def measure_or_pref(num_phase=18,num_orientation=4,frequencies=[2.4],
                           Feature(name="frequency",values=frequencies)]     
    
         x=MeasureFeatureMap(feature_values)
-
         param_dict = {"scale":scale,"offset":offset}
-
         x.measure_maps(user_function, param_dict, display, feature_values)
 
 
-###TRALERT: Function for OD map measurement
+
+### JABALERT: Shouldn't there be a num_ocularities argument as well, to
+### present various combinations of left and right eye activity?        
 def measure_od_pref(num_phase=18,num_orientation=4,frequencies=[2.4],
                     scale=0.3,offset=0.0,display=True,
 		    user_function=PatternPresenter(pattern_generator=SineGrating(),
                                                    apply_output_fn=False,duration=0.175)):
+    """
+    Measure ocular dominance maps, using a sine grating by default.
+
+    Measures maps by collating the responses to a set of input
+    patterns controlled by some parameters.  The parameter ranges and
+    number of input patterns in each range are determined by the
+    num_phase, num_orientation, and frequencies parameters.  The
+    particular pattern used is determined by the user_function
+    argument, which defaults to a sine grating presented for a short
+    duration.  By convention, most Topographica example files
+    are designed to have a suitable activity pattern computed by
+    that time, but the duration will need to be changed for other
+    models that do not follow that convention.
+    """
     
     if num_phase <= 0 or num_orientation <= 0:
         raise ValueError("num_phase and num_orientation must be greater than 0")
@@ -172,30 +186,35 @@ def measure_od_pref(num_phase=18,num_orientation=4,frequencies=[2.4],
         step_phase=2*pi/num_phase
         step_orientation=pi/num_orientation
 
-
         feature_values = [Feature(name="phase",range=(0.0,2*pi),step=step_phase,cyclic=True),
                           Feature(name="orientation",range=(0.0,pi),step=step_orientation,cyclic=True),
                           Feature(name="frequency",values=frequencies),
                           Feature(name="ocular",range=(0.0,1.0),values=[0.0,1.0])]  
                           
-
-#        feature_values = {"phase": ( (0.0,2*pi),step_phase,True),
-#                          "orientation": ( (0.0,pi), step_orientation, True),
-#                          "frequency": ((min(frequencies),max(frequencies)),frequencies,False),
-#                          "ocular":((0.0,1.0),[0.0,1.0],False)}
-
         x=MeasureFeatureMap(feature_values)
-        
         param_dict = {"scale":scale,"offset":offset}
-
         x.measure_maps(user_function, param_dict, display, feature_values)
 
-###TRALERT: Function for disparity map measurement
-def measure_disparity(num_phase=12,num_orientation=4,num_disparity=12,frequencies=[2.4],
-                    scale=0.3,offset=0.0,display=True,
-		    user_function=PatternPresenter(pattern_generator=SineGrating(),
-                                                   apply_output_fn=False,duration=0.175)):
 
+
+def measure_disparity(num_phase=12,num_orientation=4,num_disparity=12,frequencies=[2.4],
+                      scale=0.3,offset=0.0,display=True,
+                      user_function=PatternPresenter(pattern_generator=SineGrating(),
+                                                     apply_output_fn=False,duration=0.175)):
+    """
+    Measure disparity maps, using a sine grating by default.
+
+    Measures maps by collating the responses to a set of input
+    patterns controlled by some parameters.  The parameter ranges and
+    number of input patterns in each range are determined by the
+    num_phase, num_orientation, num_disparity, and frequencies
+    parameters.  The particular pattern used is determined by the
+    user_function argument, which defaults to a sine grating presented
+    for a short duration.  By convention, most Topographica example
+    files are designed to have a suitable activity pattern computed by
+    that time, but the duration will need to be changed for other
+    models that do not follow that convention.
+    """
 
     if num_phase <= 0 or num_orientation <= 0 or num_disparity <= 0:
         raise ValueError("num_phase, num_disparity and num_orientation must be greater than 0")
@@ -211,18 +230,19 @@ def measure_disparity(num_phase=12,num_orientation=4,num_disparity=12,frequencie
                           Feature(name="disparity",range=(0.0,2*pi),step=step_disparity,cyclic=True)]    
 
         x=MeasureFeatureMap(feature_values)
-
         param_dict = {"scale":scale,"offset":offset}
-
         x.measure_maps(user_function, param_dict, display, feature_values)
 
-###TRALERT: Function for disparity map analysis purposes. Used in conjunction with measure_maps_modified() in topo.analysis.featuremap
+
+
+
+###JABALERT: Can this code be deleted?
+###TR: Function for disparity map analysis purposes. Used in conjunction with measure_maps_modified() in topo.analysis.featuremap
 '''
 def measure_disparity_modified(num_phase=12,num_orientation=4,num_disparity=12,frequencies=[2.4],
                     scale=0.3,offset=0.0,display=True,
 		    user_function=PatternPresenter(pattern_generator=SineGrating(),
                                                    apply_output_fn=False,duration=0.175)):
-
 
     if num_phase <= 0 or num_orientation <= 0 or num_disparity <= 0:
         raise ValueError("num_phase, num_disparity and num_orientation must be greater than 0")
@@ -238,48 +258,58 @@ def measure_disparity_modified(num_phase=12,num_orientation=4,num_disparity=12,f
                           Feature(name="orientation",range=(0.0,pi),step=step_orientation,cyclic=True),
                           Feature(name="frequency",values=frequencies),
                           Feature(name="disparity",range=(0.0,2*pi),step=step_disparity,True)]          
-        
-        
-#        feature_values = {"phase": ( (0.0,2*pi),step_phase,True),
-#                          "orientation": ( (0.0,pi), step_orientation, True),
-#                          "frequency": ((min(frequencies),max(frequencies)),frequencies,False),
-#                          "disparity":((0.0,2*pi),step_disparity,True)}
 
         x=MeasureFeatureMap(feature_values)
-
         param_dict = {"scale":scale,"offset":offset}
-
         disp_pref,orient_pref,disp_sel,orient_sel=x.measure_maps_modified(user_function, param_dict, display, feature_values)
-
         return disp_pref,orient_pref,disp_sel,orient_sel
 '''
+
 
 def measure_position_pref(divisions=6,size=0.5,scale=0.3,offset=0.0,display=False,
                           user_function=PatternPresenter(Gaussian(aspect_ratio=1.0),False,1.0),
                           x_range=(-0.5,0.5),y_range=(-0.5,0.5)):
-    """Measure position preference map, using Gaussian patterns by default."""
+    """
+    Measure position preference map, using a circular Gaussian by default.
+
+    Measures maps by collating the responses to a set of input
+    patterns controlled by some parameters.  The parameter ranges and
+    number of input patterns in each range are determined by the
+    divisions parameter.  The particular pattern used is determined by the
+    size, scale, offset, and user_function arguments.
+    """
 
     if divisions <= 0:
         raise ValueError("divisions must be greater than 0")
 
     else:
         # JABALERT: Will probably need some work to support multiple input regions
-        
         feature_values = [Feature(name="x",range=x_range,step=1.0*(x_range[1]-x_range[0])/divisions),
                           Feature(name="y",range=y_range,step=1.0*(y_range[1]-y_range[0])/divisions)]          
-        
-#        feature_values = {"x": ( x_range, 1.0*(x_range[1]-x_range[0])/divisions, False),
-#                          "y": ( y_range, 1.0*(y_range[1]-y_range[0])/divisions, False)}
                           
         x=MeasureFeatureMap(feature_values)
-        param_dict = {"size":size,"scale":scale,"offset":offset}        
+        param_dict = {"size":size,"scale":scale,"offset":offset}
         x.measure_maps(user_function, param_dict, display, feature_values)
 
 
-
+###JABALERT: The name display_projection should be changed to e.g. proj_name
 def measure_cog(display_projection ="Afferent"):    
-    """Calculate center of gravity for each CF of each unit in each CFSheet."""
-    
+    """
+    Calculate center of gravity (CoG) for each CF of each unit in each CFSheet.
+
+    Unlike measure_position_pref and other measure_ commands, this one
+    does not work by collate the responses to a set of input patterns.
+    Instead, the CoG is calculated directly from each set of afferent
+    weights.  The CoG value thus is an indirect estimate of what
+    patterns the neuron will prefer, but is not limited by the finite
+    number of test patterns as the other measure_ commands are.
+
+    At present, the name of the projection to use must be specified
+    in the argument to this function, and a model using any other
+    name must specify that explicitly when this function is called.
+    """
+    ### JLHACKALERT: Should be updated to support multiple projections
+    ### to each sheet, not requiring the name to be specified.
     
     f = lambda x: hasattr(x,'measure_maps') and x.measure_maps
     measured_sheets = filter(f,topo.sim.objects(CFSheet).values())
@@ -302,10 +332,6 @@ def measure_cog(display_projection ="Afferent"):
 			xpref[r][c]= xcentroid
 			ypref[r][c]= ycentroid
                     
-		    ### JLHACKALERT: The default display works if there is a projection 
-                    ### called 'Afferent'otherwise each projection will need to be typed separately
-		    ##	only one projection from each sheet is displayed in the gui window at one time
-                    
 			new_view = SheetView((xpref,sheet.bounds), sheet.name,sheet.precedence)
 			sheet.sheet_view_dict['XCoG']=new_view
 			new_view = SheetView((ypref,sheet.bounds), sheet.name,sheet.precedence)
@@ -315,9 +341,12 @@ def measure_cog(display_projection ="Afferent"):
 
 def update_activity():
     """
-    Measure an activity map. Command called when opening an activity plot group panel.
-
-    To be precise, just add the activity sheet_view for Sheets objects of the simulation.
+    Make a map of neural activity available for each sheet, for use in template-based plots.
+    
+    This command simply asks each sheet for a copy of its activity
+    matrix, and then makes it available for plotting.  Of course, for
+    some sheets providing this information may be non-trivial, e.g. if
+    they need to average over recent spiking activity.
     """
     for sheet in topo.sim.objects(Sheet).values():
         activity_copy = array(sheet.activity)
@@ -326,18 +355,17 @@ def update_activity():
         sheet.sheet_view_dict['Activity']=new_view
     
 
-# JABALERT: Module variables for passing values to the commands; should be
-# changed to a cleaner mechanism.
+
+# Module variables for passing values to the commands.
 coordinate = (0,0)
 sheet_name = ''
 proj_coords=[(0,0)]
 proj_name =''
+### JABALERT: This mechanism for passing values is a bit awkward, and
+### should be changed to something cleaner.  It might also be better
+### to access a Sheet instance directly, rather than searching by name.
 
 
-### JACALERT! This function, as well as update_projections have to be simplified.
-### Particularly, having to retrieve the Sheet object from the sheet_name is an issue:
-### It might be better to have access to the Sheet object directly rather then the name 
-### in the CFSheetPlotpanels.
 def update_connectionfields():
     """
     Add SheetViews for the weights of one unit in a CFSheet, 
