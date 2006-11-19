66220e20b03b880a918c46bb09c98e2dc158435f
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2006-11-19 15:54:51 +0000

    Adds weave and a c compiler.

--- topographica-win/create_python_topo/setup.bat
+++ topographica-win/create_python_topo/setup.bat
@@ -7,11 +7,21 @@ set startdir=%CD%
 ..\util\tar xvf ..\..\external\Pmw.tar
 copy ..\..\external\Pmw .
 
+
+..\util\gunzip weave.tar.gz
+..\util\tar xvf weave.tar
+..\util\gunzip scipy_test.tar.gz
+..\util\tar xvf scipy_test.tar
+
+
+
 cd ..
 xcopy /E /I create_python_topo c:\create_python_topo
+xcopy /E /I util c:\create_python_topo\util
 REM CEBHACKALERT: because the msi won't run from this directory, at least
 REM on my PC! Why? Is it a path problem?
 c:
+cd \
 cd create_python_topo
 
 
@@ -19,15 +29,44 @@ start /w msiexec /i python-2.4.2.msi ALLUSERS=0 TARGETDIR=c:\python24 ADDLOCAL=D
 start /w Numeric-24.2.win32-py2.4.exe
 start /w matplotlib-0.81.win32-py2.4.exe
 start /w PIL-1.1.5.win32-py2.4.exe
+REM start /w scipy-0.5.1.win32-py2.4.exe
 
 move Pmw c:\python24\Lib\site-packages
+
+REM this is weave that I think I compiled with mingw from
+REM scipy 0.3.2, since none was already compiled for 
+REM python 2.4 and Numeric 24. The way scipy is organized
+REM has changed in later releases (notably weave has changed location; 
+REM when topographica
+REM switches to use a recent scipy, we should just be able
+REM to use the scipy*.exe provided online (as for Numeric, etc).
+REM and remove these.
+move weave c:\python24\Lib\site-packages
+move scipy_test c:\python24\Lib\site-packages
+
 
 REM ** For fixedpoint, the patched version from unix is used.
 REM ** CEBHACKALERT: use the patch executable in utils to create the
 REM patch from the diff and fixedpoint.tgz file in external\
 move fixedpoint-0.1.2_patched.py c:\python24\Lib\site-packages\fixedpoint.py
 
-cd ..
+
+xcopy /E /I mingw c:\python24\mingw\
+util\gunzip c:\python24\mingw\*.gz 
+copy util\tar.exe c:\python24\mingw
+
+cd \python24\mingw
+tar xvf "c:\python24\mingw\gcc-core-3.4.2-20040916-1.tar" 
+tar xvf "c:\python24\mingw\gcc-g++-3.4.2-20040916-1.tar" 
+tar xvf "c:\python24\mingw\gcc-g77-3.4.2-20040916-1.tar" 
+tar xvf "c:\python24\mingw\w32api-3.6.tar" 
+tar xvf "c:\python24\mingw\mingw-runtime-3.9.tar" 
+tar xvf "c:\python24\mingw\binutils-2.15.91-20040904-1.tar" 
+del *.tar
+del tar.exe
+
+cd ..\..
+
 rmdir /Q /S create_python_topo
 
 echo.
