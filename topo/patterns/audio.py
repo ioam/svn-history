ddb226d680482e157a030498a7f1209c650c0b96
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-02-24 07:55:51 +0000

    Audio(PatternGenerator) is now Audio(OneDPowerSpectrum).

--- topo/patterns/audio.py
+++ topo/patterns/audio.py
@@ -6,101 +6,40 @@ $Id$
 """
 __version__='$Revision$'
 
-import numpy
-import numpy.fft
 # CEBHACKALERT: you need to build pyaudiolab to use this file.
 # Should we have pyaudiolab built by default now? (I haven't
 # tested it on Windows or OS X.)
 import pyaudiolab
 
-from topo.base.patterngenerator import PatternGenerator
 from topo.base.parameterclasses import Filename,Integer,Parameter
 
+from topo.patterns.basic import OneDPowerSpectrum
 
 
-# CEBALERT: will split into topo.patterns.basic.OneDPowerSpectrum(PatternGenerator)
-# and topo.patterns.audio.Audio(OneDPowerSpectrum).
 
-
-# CB: being a PatternGenerator gives this lots of parameters that don't really
-# mean anything.
-class Audio(PatternGenerator):
+class Audio(OneDPowerSpectrum):
     """
-    Performs a discrete Fourier transform each time it's called,
-    on a rolling window of the signal.
-    
-    Over time, outputs a power spectrum.
-
-    ** This class has not been tested, and is not documented **
     """
-    # CB: find an example sound (I haven't listened to this one)
-    filename = Filename(default='lib/python2.4/site-packages/pyaudiolab/test_data/test.flac',precedence=0.9,doc=
+    # CB: find an example sound (I haven't listened to this one;
+    # there's no signal in it for the first hundred or so frames)
+    filename = Filename(
+        default='lib/python2.4/site-packages/pyaudiolab/test_data/test.flac',
+        precedence=0.9,doc=
         """
         File path (can be relative to Topographica's base path) to an audio file.
         The audio can be in any format accepted by pyaudiolab, e.g. WAV, AIFF, or FLAC.
         """)
 
-    windowing_function = Parameter(default=numpy.hanning)
-
-    # CEBALERT: parameters are not sampling-rate independent
-    window_length = Integer(default=12)
-
-    window_overlap = Integer(default=2)
-    
-    # CB: add option to select a section of the file to use; use all
-    # by default.
     def __init__(self,**params):
         """
         Read the audio file into an array.
         """
-        super(Audio,self).__init__(**params)
         self._source = pyaudiolab.sndfile(params.get('filename',self.filename),'read')
-
-        # CB: add pre-processing options (e.g. remove DC) here? Or users should do them
-        # first if they want them?
-
         n_frames = self._source.get_nframes()        
-        self.signal = self._source.read_frames(n_frames,dtype=numpy.float32)
+        sig =  self._source.read_frames(n_frames,dtype=numpy.float32)
+        spacing = 1.0/self._source.get_samplerate()
 
-        # current position of 'read pointer' in the signal
-        self.location = 0
-
-        self.sample_rate = self._source.get_samplerate()
-        
-
-    def __call__(self,**params):
-        """
-        Perform a DFT (FFT) of the current sample from the signal multiplied
-        by the smoothing window.
-        """
-        # CB: How much to explain in docstring? numpy.fft.fft has
-        # documentation e.g. numpy.fft.fft most efficient if
-        # window_length 'is a power of 2 (or decomposable into low
-        # prime factors)', and what the output represents.
-
-
-        # currently these can all be changed each call: is that useful?
-        # (unusual for creating a spectrogram)
-        n_frames = params.get('window_length',self.window_length)
-        overlap = params.get('window_overlap',self.window_overlap)
-        w_func = params.get('windowing_function',self.windowing_function)
-        
-        start = max(self.location-overlap,0)
-        end = start+n_frames
-        signal_sample = self.signal[start:end]
-
-        self.location+=n_frames
-
-        # make the frequencies easily available; would be calc'd only once
-        # if the above parameters were constant
-        self.last_freq = numpy.fft.fftfreq(n_frames,d=1.0/self.sample_rate)
-
-        # again, would store if above parameters were constant
-        smoothing_window = w_func(n_frames)  
-
-        return numpy.fft.fft(signal_sample*smoothing_window)
-    
-
-
+        super(Audio,self).__init__(signal=sig,sample_spacing=spacing,**params)
+                
 
 
