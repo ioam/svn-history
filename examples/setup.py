0d544cd2b750040c430dc685a559cd79a0285917
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-03-01 14:55:46 +0000

    Added ability to handle multiple targets with one label (not sure of Makefile terminology). Still a draft.

--- examples/setup.py
+++ examples/setup.py
@@ -3,68 +3,89 @@
 # Not sure "setup.py" is the right name.
 # Just translating the Makefile...but we definitely have topographica
 # by now so we could do this completely differently.
-# The spawn commands in here show what commands in the main Makefile
-# could be replaced with (once python itself has been built).
 
-# currently run from the examples directory like this:
-# ../bin/python setup.py X
-# where X is all_quick or saved_examples
 
 from os import system, spawnv, P_WAIT, getcwd
 from sys import argv,exit,exec_prefix
 from os.path import join
 
-def usage():
-    print """
-instructions for running...
-"""
-# CEBALERT: replace all this with some kind of option parsing
-if len(argv)< 2:
-    usage()
-    exit(-1)
-arg = argv[1]
+# pretend we got these args...
+args = ["all_quick","saved_examples","hierarchical"]  #(argv[1], argv[2])
 
-targets = dict( all_quick=("hierarchical","cfsom_or"),
-		saved_examples=("lissom_oo_or_20000.typ",)
+
+targets = dict( all_quick=["hierarchical","cfsom_or"],
+		saved_examples=["lissom_oo_or_20000.typ"]
 		)
-
 
 
 # location of the topographica script
 topographica = join(exec_prefix,"topographica")
 
 # location of examples dir
-# CBALERT: won't work in general, use one reliable way to get
-# the topographica path and build the others from that.
-examples = getcwd()
+examples = join(exec_prefix,"examples")
 
 
 SNAPSHOT = "from topo.commands.basic import save_snapshot ; save_snapshot"
 
+OR_ANALYSIS = "from topo.commands.analysis import measure_or_pref,measure_position_pref,measure_cog,measure_or_tuning_fullfield; \
+measure_or_pref(); \
+measure_position_pref(); \
+measure_cog(); \
+measure_or_tuning_fullfield(); \
+"
+
+RETINOTOPY_ANALYSIS = "from topo.commands.analysis import measure_position_pref,measure_cog ;\
+measure_position_pref(); \
+measure_cog(); \
+"
+
+
 
 commands = {
-	"cfsom_or":("topographica",
-		    '-c',"default_density=4",join(examples,"cfsom_or.ty"),
-		    '-c','topo.sim.run(1)',
-		    '-c','print "cf"'),
+    "cfsom_or":("topographica",
+                '-c',"default_density=4",join(examples,"cfsom_or.ty"),
+                '-c','topo.sim.run(1)'),
+    
+    "hierarchical":("topographica",
+                    '-c',"default_density=4",join(examples,"hierarchical.ty"),
+                    '-c','topo.sim.run(1)'),
+    
+    "lissom_or":("topographica",
+                 '-c',"default_density=4",join(examples,"lissom_or.ty"),
+                 '-c','topo.sim.run(1)'),
+    
+    "lissom_oo_or":("topographica",
+                    '-c',"default_density=4",join(examples,"lissom_oo_or.ty"),
+                    '-c','topo.sim.run(1)'),
+    
+    "som_retinotopy":("topographica",
+                      '-c',"default_density=4",join(examples,"som_retinotopy.ty"),
+                      '-c','topo.sim.run(1)'),
+    
+    "lissom_oo_or_20000.typ":("topographica",
+                              '-c',"default_density=4",join(examples,"lissom_oo_or.ty"),
+                              '-c','topo.sim.run(2)',
+                              '-c',SNAPSHOT+"('"+join(examples,'lissom_oo_or_20000.typ')+"')")
+    }
 
-	"hierarchical":("topographica",
-		    '-c',"default_density=4",join(examples,"hierarchical.ty"),
-		    '-c','topo.sim.run(1)',
-		    '-c','print "h"'),
 
-	"lissom_oo_or_20000.typ":("topographica",
-				  '-c',"default_density=4",join(examples,"lissom_oo_or.ty"),
-				  '-c','topo.sim.run(2)',
-				  '-c',SNAPSHOT+"('"+join(examples,'lissom_oo_or_20000.typ')+"')")
-	}
+# Create the list of commands to execute either by getting the
+# command labels from a target, or by inserting the command label
+# CEBALERT: I don't know any string methods; I'm sure this can
+# be simplified.
+command_labels=[]
+for a in args:
+    if a in targets:
+        for X in targets[a]:
+            command_labels.append(X)
+    else:
+        command_labels.append(a)
 
 
-
-# CB: temp.
-for t in targets[arg]:
+for cmd in command_labels:
 	# CB: spawnv gives us new process,
 	# P_WAIT means the script waits until new process returns
-	spawnv(P_WAIT,topographica,commands[t])
+        print commands[cmd]
+	spawnv(P_WAIT,topographica,commands[cmd])
 
 print "(done)"
