3b2d83ee4523769f07b1ff3ad7d17d7b812a7c99
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-03-01 14:06:20 +0000

    Updated to be a reasonable test of snapshots.

--- topo/tests/testsnapshots.py
+++ topo/tests/testsnapshots.py
@@ -10,21 +10,18 @@ __version__='$Revision$'
 # the others, but runs fine on its own.
 # So, we need to eliminate such things as
 # "topo.patterns.basic.Gaussian.x=lambda..." from the test files.
-
-
-# To do:
-# - test saving scheduled actions
-
-
+#
+# Meanwhile, run as:
+#./topographica -c 'import topo.tests.testsnapshots; topo.tests.run(test_modules=[topo.tests.testsnapshots])'    
 import unittest, copy
 
 import topo
+import __main__
 
 from topo.base.sheet import Sheet
-from topo.base.simulation import Simulation
-from topo.base.cf import CFSheet,CFProjection
 from topo.sheets.generatorsheet import GeneratorSheet
 from topo.commands.basic import save_snapshot,load_snapshot
+from topo.patterns.basic import Gaussian, Line
 
 from utils import assert_array_equal
 
@@ -33,29 +30,38 @@ class TestSnapshots(unittest.TestCase):
 
     def test_basic_save_load_snapshot(self):
         """
-        Simple test to check the activity matrix of a Sheet
-        comes back ok.
+        Very basic test to check the activity matrix of a GeneratorSheet
+        comes back ok, and that class attributes are pickled.
         """
-        test_sim_name = "testing_sim"
         Sheet.nominal_density = 2.0
-        topo.sim.name = test_sim_name
-        topo.sim['Retina'] = GeneratorSheet()
-        topo.sim['V1'] = CFSheet()
-        topo.sim.connect('Retina','V1',delay=0.5,connection_type=CFProjection,name='Afferent')
+
+        topo.sim['R']=GeneratorSheet(input_generator=Gaussian())
         topo.sim.run(1)
-        v1_activity = copy.deepcopy(topo.sim['V1'].activity)
-    
-        save_snapshot("temp_test")
 
-        Simulation()
-        self.assertNotEqual(topo.sim.name,test_sim_name)
+        R_act = copy.deepcopy(topo.sim['R'].activity)
+        Line.x = 12.0
+        topo.sim.startup_commands.append("z=99")
 
-        load_snapshot("temp_test")
+        save_snapshot("testsnapshot.typ") # CEBALERT: should delete at end of test
 
-        self.assertEqual(topo.sim.name,test_sim_name)
-        assert_array_equal(v1_activity,topo.sim['V1'].activity)
+        Line.x = 9.0
+        exec "z=88" in __main__.__dict__
         
+        topo.sim['R'].set_input_generator(Line())
+        topo.sim.run(1)
 
+        load_snapshot("testsnapshot.typ")
+
+        # CEBALERT: should also test that unpickling order is correct
+        # (i.e. startup_commands, class attributes, simulation)
+        assert_array_equal(R_act,topo.sim['R'].activity)
+        self.assertEqual(Line.x,12.0)
+        self.assertEqual(__main__.__dict__['z'],99)
+
+
+# CB: longer to run test should additionally quit the simulation
+# and start again. Should also test scheduled actions.
+   
         
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestSnapshots))
