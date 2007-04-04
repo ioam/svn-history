508a7f35672b06e2d7cc7155e5e3e65f013d7fc1
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-04-04 12:12:49 +0000

    Continued working on quickly translating the Makefile. Probably finished, but needs simplifying.

--- examples/setup.py
+++ examples/setup.py
@@ -1,21 +1,20 @@
-# Commands for running the examples files.
+"""
+Commands for running the examples files.
+"""
+
 # CEBHACKALERT! Still being written!
-# Not sure "setup.py" is the right name.
-# Just translating the Makefile...but we definitely have topographica
-# by now so we could do this completely differently.
+# Not sure "setup.py" is the right name.  Just translating the
+# Makefile for now...but we definitely have topographica by now so we
+# could do this differently.
+
+# Has none of the Makefile's dependency processing, so just does
+# what you tell it (i.e. over-writes existing files).
 
 
-from os import system, spawnv, P_WAIT, getcwd
-from sys import argv,exit,exec_prefix
+
+from os import spawnv, P_WAIT
+from sys import argv, exec_prefix
 from os.path import join
-
-# pretend we got these args...
-args = ["all_quick","saved_examples","hierarchical"]  #(argv[1], argv[2])
-
-
-targets = dict( all_quick=["hierarchical","cfsom_or"],
-		saved_examples=["lissom_oo_or_20000.typ"]
-		)
 
 
 # location of the topographica script
@@ -24,68 +23,110 @@ topographica = join(exec_prefix,"topographica")
 # location of examples dir
 examples = join(exec_prefix,"examples")
 
+# first arg is script name
+command_names = argv[1:len(argv)]
 
-SNAPSHOT = "from topo.commands.basic import save_snapshot ; save_snapshot"
 
-OR_ANALYSIS = "from topo.commands.analysis import measure_or_pref,measure_position_pref,measure_cog,measure_or_tuning_fullfield; \
+### Convenience functions
+def snapshot(filename):
+    """Return a command for saving a snapshot named filename."""
+    return "from topo.commands.basic import save_snapshot ; save_snapshot('"+join(examples,filename)+"')"
+    
+def or_analysis():
+    """Return a command for orientation analysis."""
+    return "from topo.commands.analysis import measure_or_pref,measure_position_pref,measure_cog,measure_or_tuning_fullfield; \
 measure_or_pref(); \
 measure_position_pref(); \
 measure_cog(); \
-measure_or_tuning_fullfield(); \
-"
+measure_or_tuning_fullfield()"
 
-RETINOTOPY_ANALYSIS = "from topo.commands.analysis import measure_position_pref,measure_cog ;\
+def retinotopy_analysis():
+    """Return a command for retinotopy analysis."""
+    return "from topo.commands.analysis import measure_position_pref,measure_cog ;\
 measure_position_pref(); \
-measure_cog(); \
-"
+measure_cog()"
+###
+
+
+def run(name,density=4,commands=["topo.sim.run(1)"]):
+    """
+    Return a command formatted in a suitable way for input to spawnv.
+    """
+    cmd_list = ["topographica"]
+    cmd_list.append("-c")
+    cmd_list.append("default_density=%d"%density)
+
+    cmd_list.append(join(examples,name))
+    
+    for c in commands:
+        cmd_list.append("-c")
+        cmd_list.append(c)
+
+    return tuple(cmd_list)
 
 
 
-commands = {
-    "cfsom_or":("topographica",
-                '-c',"default_density=4",join(examples,"cfsom_or.ty"),
-                '-c','topo.sim.run(1)'),
-    
-    "hierarchical":("topographica",
-                    '-c',"default_density=4",join(examples,"hierarchical.ty"),
-                    '-c','topo.sim.run(1)'),
-    
-    "lissom_or":("topographica",
-                 '-c',"default_density=4",join(examples,"lissom_or.ty"),
-                 '-c','topo.sim.run(1)'),
-    
-    "lissom_oo_or":("topographica",
-                    '-c',"default_density=4",join(examples,"lissom_oo_or.ty"),
-                    '-c','topo.sim.run(1)'),
-    
-    "som_retinotopy":("topographica",
-                      '-c',"default_density=4",join(examples,"som_retinotopy.ty"),
-                      '-c','topo.sim.run(1)'),
-    
-    "lissom_oo_or_20000.typ":("topographica",
-                              '-c',"default_density=4",join(examples,"lissom_oo_or.ty"),
-                              '-c','topo.sim.run(2)',
-                              '-c',SNAPSHOT+"('"+join(examples,'lissom_oo_or_20000.typ')+"')")
+
+# shortcuts for executing multiple targets
+group_targets = dict( all_quick=["hierarchical","cfsom_or","som_retinotopy","lissom_oo_or"],
+                      all_long=["lissom_oo_or_10000.typ","som_retinotopy_40000.typ",
+                                "lissom_or_10000.typ","lissom_fsa_10000.typ"],
+                      saved_examples=["lissom_oo_or_10000.typ"])
+
+
+# update the times!
+targets = {
+    "cfsom_or":run("cfsom_or.ty"),
+    "hierarchical":run("hierarchical.ty"),
+    "lissom_or":run("lissom_or.ty"),
+    "lissom_oo_or":run("lissom_oo_or.ty"),
+    "som_retinotopy":run("som_retinotopy.ty"),
+    "lissom_oo_or_10000.typ":run("lissom_oo_or.ty",
+                                 commands=["topo.sim.run(1)",
+                                           or_analysis(),
+                                           snapshot("lissom_oo_or_10000.typ")]),
+    "lissom_or_10000.typ":run("lissom_or.ty",
+                              commands=["topo.sim.run(1)",
+                                        or_analysis(),
+                                        snapshot("lissom_or_10000.typ")]),
+    "lissom_fsa_10000.typ":run("lissom_fsa.ty",
+                               commands=["topo.sim.run(1)",
+                                         or_analysis(),
+                                         snapshot("lissom_fsa_10000.typ")]),
+    "obermayer_pnas90_30000.typ":run("obermayer_pnas90_30000.ty",
+                                     commands=["topo.sim.run(1)",
+                                               or_analysis(),
+                                               snapshot("obermayer_pnas90_30000.typ")]),
+    "som_retinotopy_40000.typ":run("som_retinotopy.ty",
+                                   commands=["topo.sim.run(1)",
+                                             retinotopy_analysis(),
+                                             snapshot("som_retinotopy_40000.typ")])
+                              
     }
 
 
-# Create the list of commands to execute either by getting the
-# command labels from a target, or by inserting the command label
+
+
+
+
+
+### Create the list of commands to execute either by getting the
+### command labels from a target, or by inserting the command label
 # CEBALERT: I don't know any string methods; I'm sure this can
-# be simplified.
+# be simplified!
 command_labels=[]
-for a in args:
-    if a in targets:
-        for X in targets[a]:
+for a in command_names:
+    if a in group_targets:
+        for X in group_targets[a]:
             command_labels.append(X)
     else:
         command_labels.append(a)
 
-
+### Execute the commands
 for cmd in command_labels:
 	# CB: spawnv gives us new process,
 	# P_WAIT means the script waits until new process returns
-        print commands[cmd]
-	spawnv(P_WAIT,topographica,commands[cmd])
+        print targets[cmd]
+	spawnv(P_WAIT,topographica,targets[cmd])
 
 print "(done)"
