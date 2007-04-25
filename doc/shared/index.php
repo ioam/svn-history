8aaf5b8f24917887c81b0b157cb31504967e08ae
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-04-25 03:26:11 +0000

    Correct minor typo.

--- doc/shared/index.php
+++ doc/shared/index.php
@@ -106,7 +106,7 @@
 </table>
 </center>
 
-<P>Binaries will be available soon; in the meantime; this release 
+<P>Binaries will be available soon; in the meantime, this release 
 is available via <A HREF="../Downloads/cvs.html">cvs</a>.
   
 <p><A target="_top" href="../Home/oldnews.html"><i>Older news</i></A>
