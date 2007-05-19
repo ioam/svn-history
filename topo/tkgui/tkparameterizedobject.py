464e1f91344ea89c91d003b23f548955e68559b2
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-05-19 11:13:16 +0000

    Updates to documentation, some renaming, and some cleaning.

--- topo/tkgui/tkparameterizedobject.py
+++ topo/tkgui/tkparameterizedobject.py
@@ -34,50 +34,80 @@ parameters_to_tkwidgets = {
 
 
 
-
+# CB: need to rename - not sure what to choose.
 class TkPO(object):
     """
-    A PO but with additional Tkinter Variables available representing the Parameters.
+    Stores a ParameterizedObject and creates Tkinter.Variable shadows
+    (proxies) of its Parameters.
 
-    When a Parameter is __set__() (as an attribute,
-    e.g. this_obj.param_name=val), sets the parameter value (which is
-    stored in THIS object), AND set()s the corresponding Tkinter variable.
-    (e.g. this_obj._tk_vars[param_name].set(val)).
+    The Parameters of the parameterized_object are available as
+    attributes of this object; the Tkinter.Variable shadows are
+    available under their corresponding parameter name in the _tk_vars
+    dictionary.
+
+
+    So, for a ParameterizedObject po with a Parameter p, t=TkPO(po) allows
+    access to po.p via t.p, i.e. setting t.p sets po.p, and getting
+    t.p returns po.p.
+
+    t stores a Tkinter Variable representing p as t._tk_vars[p];
+    setting t.p not only sets po.p, but also the Variable representing
+    p. Conversely, setting the Tkinter Variable t._tk_vars[p] also
+    sets po.p. In this way, a Parameter is kept in sync with a Tkinter
+    Variable.
+
+
+    ** Important Note **
+    If the parameterized_object itself is modified elsewhere (e.g. po.p=7),
+    the Tkinter Variable shadow is NOT updated. Requesting t.p will correctly
+    return 7, but the Tkinter Variable is still set to the old value.  
     """
+    # if the above becomes a problem, we could have some autorefresh of the vars
+    # or a callback of some kind in the parameterized object itself.
+    
     _tk_vars = None
     _parameterized_object = None
 
-    def __init__(self,parameterized_object,**params):
-        """
-        tkmaster is the tk widget onto which widgets will be placed.
-        """
+    def __init__(self,parameterized_object):
+        super(TkPO,self).__init__()
         self._parameterized_object = parameterized_object
-        self._tk_vars = {} # tk variables corresponding to this tkpo's parameters
-        super(TkPO,self).__init__(**params)
+        self._tk_vars = {}
         self.__setup_tk_vars()
 
 
     def __setup_tk_vars(self):
         """
-        Create Tk Variables corresponding to parameterized_object's Parameter types, and store them in the
+        Create Tkinter Variables corresponding to
+        parameterized_object's Parameters, and store them in the
         _tk_vars dictionary under the corresponding parameter name.
 
-        The tkinter Variable defaults to StringVar if no corresponding variable for the
-        Parameter type is found in parameters_to_vars.
+        The Tkinter Variable defaults to StringVar if no corresponding
+        Variable for the Parameter type is found in
+        parameters_to_vars.
         
-        A tk var is traced so that if its value changes the parameter value can
-        be set on this object. If setting the parameter value fails (e.g. inappropriate
-        value), the tk var is reverted.
+        Each Tkinter Variable is traced so that when its value changes
+        the corresponding parameter is set on parameterized_object.
         """
         for name,param in self._parameterized_object.params().items():
             self._tk_vars[name]=parameters_to_tkvars.get(type(param),StringVar)()
             self._tk_vars[name].set(getattr(self._parameterized_object,name))
             self._tk_vars[name]._last_good_val=self._tk_vars[name].get() # for reverting
-            self._tk_vars[name].trace_variable('w',lambda a,b,c,p_name=name: self.update_param(p_name))     
-  
-    def update_param(self,param_name):
+            self._tk_vars[name].trace_variable('w',lambda a,b,c,p_name=name: self.__update_param(p_name))        
+
+        # Instead of a trace, could override the Variable's set() method i.e. trace it ourselves.
+        # Additionally, maybe we should override the Variable's get() method so that it first updates
+        # the var to match the parameter?
+
+    def __update_param(self,param_name):
         """
-        Attempt to set param_name to the value of its correspinding tk_var.
+        Attempt to set the parameterized_object's param_name to the
+        value of its correspinding Tkinter Variable.
+
+        If setting the parameter fails (e.g. an inappropriate value
+        is set for that Parameter type), the Variable is reverted to
+        its previous value.
+
+        (Called by the Tkinter Variable's trace_variable() method.)
         """
         tk_var = self._tk_vars[param_name]
         val = tk_var.get()
@@ -93,6 +123,14 @@ class TkPO(object):
 
 
     def __getattribute__(self,name):
+        """
+        Return self.name if that attribute exists, otherwise return
+        self._parameterized_object.name.
+
+        If neither self.name nore self._parameterized_object.name
+        exists, an AttributeError is raised for the
+        parameterized_object.
+        """
         try:
             return object.__getattribute__(self,name)
         except AttributeError:
@@ -100,12 +138,6 @@ class TkPO(object):
             return getattr(parameterized_object,name)
 
     def __setattr__(self,name,value):
-
-        # read like:
-        #  if hasattr(self,name):
-        #      setattr(self,name,value)
-        #  else:
-        #      setattr(self.actual_sim,name,value)
         try:
             object.__getattribute__(self, name) 
             object.__setattr__(self, name, value)
@@ -115,42 +147,46 @@ class TkPO(object):
             if name in self._tk_vars: self._tk_vars[name].set(value)
                     
 
-
+import _tkinter # (required to catch tcl exception)
+# Needs renaming. Could be a Frame, too. 
 class WidgetDrawingTkPO(TkPO):
 
     def __init__(self,po,tkmaster,**params):
         super(WidgetDrawingTkPO,self).__init__(po,**params)
-        # for widget creation
-        assert tkmaster is not None
+        assert tkmaster is not None # need tkmaster for widget creation
         self.tkmaster = tkmaster
         self.balloon = Pmw.Balloon(tkmaster)
 
-    def create_widget(self,param_name,**kw):
+    def pack_param(self,name,widget_options={},**pack_options):
         """
-        Return a widget of the requested type, with ...
-        """
-        param = self.params()[param_name]
-        widget_type = parameters_to_tkwidgets.get(type(param),Entry)
-        tk_var = self._tk_vars[param_name]
+        Create a widget for Parameter name, configured according to
+        widget_options, and pack()ed according to the pack_options.
 
-        ### CB: obviously I'm just joking
-        import _tkinter
+        Balloon help is automatically set from the Parameter's doc.
+
+        Returns the widget in case further processing of it is required.
+        
+        Examples:
+        self.pack_param(name)
+        self.pack_param(name,side='left')
+        self.pack_param(name,{'width':50},side='top',expand='yes',fill='y')
+        """
+        param = self.params()[name]
+        widget_type = parameters_to_tkwidgets.get(type(param),Entry)
+        tk_var = self._tk_vars[name]
+
         try:
-            w = widget_type(self.tkmaster,variable=tk_var,**kw)
+            w = widget_type(self.tkmaster,variable=tk_var,**widget_options)
         except _tkinter.TclError:
             try:
-                w = widget_type(self.tkmaster,textvariable=tk_var,**kw)
+                w = widget_type(self.tkmaster,textvariable=tk_var,**widget_options)
             except _tkinter.TclError:
-                try:
-                    w = widget_type(self.tkmaster,tagvariable=tk_var,**kw)
-                except _tkinter.TclError:
-                    raise _tkinter.TclError("no variable, textvariable, tagvariable")
-        ### end of joke
+                raise # meaning the widget doesn't support variable or textvariable
         
-        self.balloon.bind(w,getdoc(self.params()[param_name]))
+        self.balloon.bind(w,getdoc(self.params()[name]))
 
-        # could pack the widget, could pass in pack options, etc
-        return w
+        w.pack(pack_options)
+        return w 
 
 
 class SomeFrame(WidgetDrawingTkPO,Frame):
@@ -161,8 +197,7 @@ class SomeFrame(WidgetDrawingTkPO,Frame):
         
         for name in po.params().keys():
             Tkinter.Label(master,text=name).pack()
-            self.create_widget(param_name=name).pack()
-
+            self.pack_param(name)
 
 
 from topo.patterns.basic import Gaussian        
@@ -170,7 +205,7 @@ g = Gaussian()
 f = SomeFrame(g,Tkinter.Toplevel())
 
 
-# ** direct changes to g are not reflected in f **
+# ** direct changes to g are not reflected in f's tkinter shadows **
 
 
 
