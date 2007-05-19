d0f5944953e5d51db73de99ba39b9605ad6bcbe5
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-05-19 14:50:57 +0000

    t=TkPO() provides access to its parameterized_object's parameter p via t.p__param, rather than as t.p (since t could also be another type of object with its own attributes). This is just for testing: might revert. Also, Tkinter Variables (shadows of Parameters) are now guaranteed never to return out-of-date values. Adding a refresh method that gets called e.g. on focus in for a WidgetDrawingTkPO would keep the GUI pretty well in sync with the underlying ParameterizedObject.

--- topo/tkgui/tkparameterizedobject.py
+++ topo/tkgui/tkparameterizedobject.py
@@ -19,6 +19,7 @@ from translatorwidgets import TaggedSlider
 
 
 
+
 # (Duplicates PropertiesFrame/ParamtersFrame)
 parameters_to_tkvars = {
     BooleanParameter:BooleanVar,
@@ -33,6 +34,12 @@ parameters_to_tkwidgets = {
 
 
 
+# ParameterizedObject's params() could be a property or something. How much
+# do we use it in the rest of topographica?
+
+
+# CB: documentation hasn't been updated regarding parameter access
+# (currently t.p__param).
 
 # CB: need to rename - not sure what to choose.
 class TkPO(object):
@@ -59,15 +66,16 @@ class TkPO(object):
 
     ** Important Note **
     If the parameterized_object itself is modified elsewhere (e.g. po.p=7),
-    the Tkinter Variable shadow is NOT updated. Requesting t.p will correctly
-    return 7, but the Tkinter Variable is still set to the old value.  
+    the Tkinter Variable shadow is NOT updated until the shadow's value is
+    requested. Thus requesting t.p will correctly return 7, but any GUI
+    display of the Variable will display the old value until a refresh takes
+    place.
     """
     # if the above becomes a problem, we could have some autorefresh of the vars
     # or a callback of some kind in the parameterized object itself.
-    
-    _tk_vars = None
-    _parameterized_object = None
 
+    _param_suffix = "__param"
+  
     def __init__(self,parameterized_object):
         super(TkPO,self).__init__()
         self._parameterized_object = parameterized_object
@@ -93,11 +101,28 @@ class TkPO(object):
             self._tk_vars[name].set(getattr(self._parameterized_object,name))
             self._tk_vars[name]._last_good_val=self._tk_vars[name].get() # for reverting
             self._tk_vars[name].trace_variable('w',lambda a,b,c,p_name=name: self.__update_param(p_name))        
+            # Instead of a trace, could we override the Variable's set() method i.e. trace it ourselves?
+            # Or does too much happen in tcl/tk for that to work?
 
-        # Instead of a trace, could we override the Variable's set() method i.e. trace it ourselves?
-        # Or does too much happen in tcl/tk for that to work?
-        # Additionally, maybe we should override the Variable's get() method so that it first updates
-        # the var to match the parameter?
+            # override the Variable's get() method to guarantee an out-of-date value is never returned.
+            tk_var = self._tk_vars[name]
+            tk_var._original_get = tk_var.get
+            tk_var.get = lambda x=name: self.__get_tk_val(x) 
+
+
+    def __get_tk_val(self,param_name):
+        """
+        Before returning a tk variable's value, ensure it's up to date. 
+        """
+        tk_var = self._tk_vars[param_name]
+        tk_val = tk_var._original_get()
+        po_val = getattr(self._parameterized_object,param_name)
+        
+        if not tk_val==po_val:
+            # tk var needs to be updated
+            tk_var.set(po_val)
+        return po_val #tk_var.get()
+    
 
     def __update_param(self,param_name):
         """
@@ -132,21 +157,25 @@ class TkPO(object):
         exists, an AttributeError is raised for the
         parameterized_object.
         """
-        try:
-            return object.__getattribute__(self,name)
-        except AttributeError:
+        l = name.find(object.__getattribute__(self,'_param_suffix'))
+        if l>0:
+            n=name[0:l]
             parameterized_object = object.__getattribute__(self,'_parameterized_object')
-            return getattr(parameterized_object,name)
+            return getattr(parameterized_object,n)
+        else:
+            return object.__getattribute__(self,name)
+
 
     def __setattr__(self,name,value):
-        try:
-            object.__getattribute__(self, name) 
-            object.__setattr__(self, name, value)
-        except AttributeError:
-            object.__setattr__(self._parameterized_object, name, value)
+        l = name.find(object.__getattribute__(self,'_param_suffix'))
+        if l>0:
+            n=name[0:l]
+            object.__setattr__(self._parameterized_object, n, value)
             # why 'if name'? better have it, no?
-            if name in self._tk_vars: self._tk_vars[name].set(value)
-                    
+            if n in self._tk_vars: self._tk_vars[n].set(value)
+        else:
+            object.__setattr__(self, name, value)
+
 
 import _tkinter # (required to get tcl exception class)
 
@@ -164,6 +193,9 @@ class WidgetDrawingTkPO(TkPO,Frame):
         Frame.__init__(self,master,**config)
         self.balloon = Pmw.Balloon(self)
 
+        # a refresh-the-widgets-on-focus-in would make the gui in sync with the actual object
+        
+
     def pack_param(self,name,widget_options={},**pack_options):
         """
         Create a widget for Parameter name, configured according to
@@ -179,10 +211,10 @@ class WidgetDrawingTkPO(TkPO,Frame):
         self.pack_param(name,{'width':50},side='top',expand='yes',fill='y')
         """
         # CB: should probably do some labeling, too
-        param = self.params()[name]
+        param = self._parameterized_object.params()[name]
         widget_type = parameters_to_tkwidgets.get(type(param),Entry)
         tk_var = self._tk_vars[name]
-
+        
         ### Tkinter widgets use either variable or textvariable
         try:
             w = widget_type(self,variable=tk_var,**widget_options)
@@ -193,7 +225,7 @@ class WidgetDrawingTkPO(TkPO,Frame):
                 raise # meaning the widget doesn't support variable or textvariable
         ###
             
-        self.balloon.bind(w,getdoc(self.params()[name]))
+        self.balloon.bind(w,getdoc(param))
 
         w.pack(pack_options)
         return w 
@@ -201,20 +233,20 @@ class WidgetDrawingTkPO(TkPO,Frame):
 
 
 ### some demo
-## class SomeFrame(WidgetDrawingTkPO):
+class SomeFrame(WidgetDrawingTkPO):
 
-##     def __init__(self,po,master,**config):
-##         WidgetDrawingTkPO.__init__(self,po,master)
-        
-##         for name in po.params().keys():
-##             Tkinter.Label(self,text=name).pack()
-##             self.pack_param(name)
+    def __init__(self,po,master,**config):
+        WidgetDrawingTkPO.__init__(self,po,master)
+    
+        for name in po.params().keys():
+            Tkinter.Label(self,text=name).pack()
+            self.pack_param(name)
 
 
-## from topo.patterns.basic import Gaussian        
-## g = Gaussian()
-## f = SomeFrame(g,Tkinter.Toplevel())
-## f.pack()
+from topo.patterns.basic import Gaussian        
+g = Gaussian()
+f = SomeFrame(g,Tkinter.Toplevel())
+f.pack()
 
 # ** direct changes to g are not reflected in f's tkinter shadows **
 
