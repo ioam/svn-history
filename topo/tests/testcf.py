043910ab65407769d361a3c7bd6defb1aecdef7d
Jefferson Provost <jefferson.provost@gmail.com>
2007-06-22 17:31:00 +0000

    Tests for CFs and CFIters.

---
+++ topo/tests/testcf.py
@@ -0,0 +1,80 @@
+import unittest
+import numpy
+
+from topo.base.simulation import Simulation
+from topo.base.cf import CFIter,MaskedCFIter,CFProjection,CFSheet
+
+class TestIter(unittest.TestCase):
+
+    def setUp(self):
+
+        self.sim = Simulation()
+
+        self.sim['Dest'] = CFSheet(nominal_density=10)
+        self.sim['Src'] = CFSheet(nominal_density=10)
+
+        self.sim.connect('Src','Dest',
+                         connection_type = CFProjection,
+                         )
+
+    def test_iterate_all(self):
+        """
+        Test to make sure the iterator hits every CF
+        """
+        total = 0
+        dest = self.sim['Dest']
+        proj = dest.projections()['SrcToDest']
+        for cf,r,c in self.iter_type(proj)():
+            total += 1
+            self.failUnless(0 <= r < 10, "CFIter generated bogus CF row index")
+            self.failUnless(0 <= c < 10, "CFIter generated bogus CF col index")
+            self.failUnlessEqual((cf.x,cf.y),dest.matrixidx2sheet(r,c))
+        self.failUnlessEqual(total,100)
+
+
+    def test_iterate_some_nil(self):
+        """
+        Test to make sure iterator skips nil CFs (i.e cf == None)
+        """
+        dest = self.sim['Dest']
+        proj = dest.projections()['SrcToDest']
+        total = 0
+        proj._cfs[5][5] = None
+        for cf,r,c in self.iter_type(proj)():
+            total += 1
+            self.failIfEqual((r,c),(5,5))
+        self.failUnlessEqual(total,99)
+
+class TestCFIter(TestIter):
+
+    iter_type = CFIter
+
+class TestMaskedCFIter(TestIter):
+
+    iter_type = MaskedCFIter
+
+    def test_iterate_masked(self):
+        """
+        Test if iterator skips masked CFs
+        """
+        total = 0
+        dest = self.sim['Dest']
+        proj = dest.projections()['SrcToDest']
+        dest.mask.data = numpy.zeros(dest.activity.shape)
+        dest.mask.data[5,5] = 1
+        for cf,r,c in self.iter_type(proj)():
+            total += 1
+            self.failUnlessEqual((r,c),(5,5))
+            self.failUnless(cf is proj._cfs[5][5])
+        self.failUnlessEqual(total,1)
+        
+
+
+####
+cases = [TestCFIter, TestMaskedCFIter]
+
+suite = unittest.TestSuite()
+suite.addTests([unittest.makeSuite(case) for case in cases])
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite)
