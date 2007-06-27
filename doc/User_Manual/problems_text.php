f9a16cc74a7e3351fa6d2f92c823746c4e746f4a
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-06-27 17:36:30 +0000

    Initial version.

---
+++ doc/User_Manual/problems_text.php
@@ -0,0 +1,58 @@
+<!-- -*-html-helper-*- mode -- (Hint for Emacs)-->
+
+<H1>Problems using Topographica</H1>
+
+<P>Some introductory paragraph (what's on this page?).</P>
+
+[Can't work out how to do something specific: support requests forum;
+Want to discuss something else: open discussion, etc. What to do before
+filing a feature request.]
+
+
+<H2>Reporting problems with Topographica</H2>
+
+<P>When you have found what appears to be a problem with Topographica
+itself, please try the following:
+
+<ol>
+
+<li>First, you could search the list of <a
+href="http://sourceforge.net/tracker/?group_id=53602&atid=470929">bug
+reports</a> on our project pages at SourceForge to see if we already
+know about the problem. If you find the problem, please feel free to
+add additional comments.</li>
+
+<li>We also have a <a
+href="http://topographica.org/Future_Work/current.html">task list</a>,
+used by the developers to keep track of tasks. Try searching this list
+for information about your problem. If you find a related task, please
+send an e-mail to the developers [**link].</li>
+
+<li>Next, maybe you're using an old version, and the problem you
+have has since been fixed? [** Where can a user easily look to see if
+a bug was fixed? The changelog? Search the cvs mailings? Update to the
+latest cvs version on another copy? Email the developers?]  </li>
+
+<li>Once you're reasonably sure the problem is not one already
+mentioned somewhere, please file a bug report by following the link in
+(1). For someone to help with your problem, we need to be able to
+replicate it. Please include:
+<ul>
+<li>the full error message;
+<li>the output of <code>cvs diff</code>;
+<li>any additional file needed to replicate the problem (e.g. a script you're using).</li>
+</ul>
+To maximize the speed of resolution, please first try to reduce the differences
+between your copy of topographica and [**the original version/the one on cvs] to the
+minimum required to elicit [**right word?] the problem (e.g. by removing extra parts of a model, 
+removing other changes that do not affect the problem, reducing sheet densities, and so on.)
+</li>
+<li>Something about the tracking, what to do if you get no response, and so on.</li>
+
+</ol>
+
+</P>
+
+
+
+
