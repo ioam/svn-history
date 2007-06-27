5ee744bf7b68184175148bc65fc2273a2c98460f
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-06-27 04:47:34 +0000

    Altered confusing 'Sheet.nominal_density' line, because if GeneratorSheet's nominal_density is set in another test, setting Sheet's nominal_density makes no difference. Added some commented-out code. Added a HACKALERT.

--- topo/tests/testsnapshots.py
+++ topo/tests/testsnapshots.py
@@ -13,28 +13,39 @@ from topo.base.sheet import Sheet
 from topo.sheets.generatorsheet import GeneratorSheet
 from topo.commands.basic import save_snapshot,load_snapshot
 from topo.patterns.basic import Gaussian, Line
+from topo.base.simulation import Simulation,SomeTimer
 
 from utils import assert_array_equal
 
 
-# CEBALERT: testsnapshots is particularly sensitive to changes made by
-# other tests. I think its scope should be reduced, and instead we
-# ought to transfer some of its testing to one of the standalone
-# (i.e. in its own, fresh topographica instance), slow tests.
+# CEBHACKALERT: this test must depend on something that happens in
+# another one, (or there is some other problem) because when run on
+# its own it fails with a strange error.
+# ./topographica -c 'import topo.tests.testsnapshots; topo.tests.run(test_modules=[topo.tests.testsnapshots])'
 
 
 SNAPSHOT_LOCATION = "topo/tests/testsnapshot.typ"
 
 class TestSnapshots(unittest.TestCase):
 
+# CEBALERT: should use a new simulation
+##     def setUp(self):
+##         """
+##         Create a new Simulation as topo.sim (so this test isn't affected by changes made by other
+##         tests to the Simulation).
+##         """
+##         Simulation(register=True,name="testsnapshots")
+
+
     def test_basic_save_load_snapshot(self):
         """
         Very basic test to check the activity matrix of a GeneratorSheet
         comes back ok, and that class attributes are pickled.
         """
-        Sheet.nominal_density = 2.0
+        #assert topo.sim.name=="testsnapshots"
+         
+        topo.sim['R']=GeneratorSheet(input_generator=Gaussian(),nominal_density=2)
 
-        topo.sim['R']=GeneratorSheet(input_generator=Gaussian())
         topo.sim.run(1)
 
         R_act = copy.deepcopy(topo.sim['R'].activity)
@@ -42,6 +53,7 @@ class TestSnapshots(unittest.TestCase):
         topo.sim.startup_commands.append("z=99")
 
         save_snapshot(SNAPSHOT_LOCATION)
+
 
         Line.x = 9.0
         exec "z=88" in __main__.__dict__
@@ -51,6 +63,7 @@ class TestSnapshots(unittest.TestCase):
 
         load_snapshot(SNAPSHOT_LOCATION)
 
+        
         # CEBALERT: should also test that unpickling order is correct
         # (i.e. startup_commands, class attributes, simulation)
         assert_array_equal(R_act,topo.sim['R'].activity)
@@ -59,7 +72,7 @@ class TestSnapshots(unittest.TestCase):
 
         #  Test to make sure the above tests haven't screwed up
         # the ability to construct new simulation objects
-        sim = topo.base.simulation.Simulation()
+        topo.base.simulation.Simulation()
 
         # CB: add xml pickling test. Certainly seems like
         # gnosis.xml.pickle is not the drop-in replacement
