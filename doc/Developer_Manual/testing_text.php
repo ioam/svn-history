b50e436cd8202b9b922b5cd1a4c8f64ccd41adb7
Jefferson Provost <jefferson.provost@gmail.com>
2007-08-22 17:43:45 +0000

    Added doctest documentation.

--- doc/Developer_Manual/testing_text.php
+++ doc/Developer_Manual/testing_text.php
@@ -26,11 +26,44 @@ even particularly useful; they vary a lot in how comprehensive they
 are.  So please always check the test file when coding, especially
 when debugging, because it probably needs work too.
 
+<h2>Unittests and Doctests</h2>
+
+<P>Topographica's test suite supports test cases that use Python's <a href="http://docs.python.org/lib/module-unittest.html">unittest</a> module or its <a href="http://docs.python.org/lib/module-doctest.html">doctest</a> module.  Unittest provides a framework for writing test cases as objects containing a set of test methods plus common initialization and clean-up code.  This framework is useful for constructing heavy-duty tests, but can be cumbersome when only a simple set of correctness tests are required.  All unittests in modules with names matching the pattern <CODE>topo/tests/test*.py</CODE> can be automatically discovered and run by the topographica command <CODE>topo.tests.run()</CODE>
+
+<P>Python's doctest module allows tests to be specified as a sequence of Python expressions to be evaluated, each followed by the expected result of the command.  The entire sequence should be formatted like a trace of an interactive python session.  For example:
+<pre>
+>>> def f(x):
+...    return x+1
+... 
+>>> f(3)
+4
+>>> f(2)
+3
+>>> f('foo')
+Traceback (most recent call last):
+  File "<stdin>", line 1, in ?
+  File "<stdin>", line 2, in f
+TypeError: cannot concatenate 'str' and 'int' objects
+>>> 
+</pre>
+As long as each command produces the expected output (including any errors), the test passes.  See the <a href="http://docs.python.org/lib/module-doctest.html">doctest documentation</a> for details.
+
+As with unittest testsuites, all doctest files with names matching <code>topo/tests/test*.txt</code> can be found and run automatically with the function <code>topo.tests.run()</code>.
+
+<P>Important notes:
+<ul>
+<li> To construct a doctest file from an interactive topographica trace, the entire <code>topographica_tXXX</code> prefix must be removed from every line.  Lines with the prefix will be ignored.
+
+<li> The testrunner used by <code>topo.tests.run()</code> will happily run an empty doctest file and report no errors (since there were no tests).  Make sure to manually check new or modified doctest files with <code>doctest.testfile(filename,verbose=True)</code> to make sure that the tests are actually being run before running <code>make tests</code> or <code>topo.tests.run()</code>.
+
+<li> Topographica does not currently run doctests embedded in code documentation.
+</ul>
+
 <H2>Automatic testing</H2>
 
 <P>Currently, topographica is automatically checked out, built, and
 tested (via <code>make tests; make slow-tests</code>) every 12 hours on
-a linux computer (using <a href="http://buildbot.net/">buildbot</a>). 
+a Linux computer (using <a href="http://buildbot.net/">buildbot</a>). 
 The results of these builds and tests can be seen at
 <a
 href="http://buildbot.topographica.org/">buildbot.topographica.org</a>.
