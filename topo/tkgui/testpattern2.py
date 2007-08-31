6c94fad50f031e1ce173948a3fc42ad93ad41f46
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-08-31 12:30:21 +0000

    Continued cleanup.

--- topo/tkgui/testpattern2.py
+++ topo/tkgui/testpattern2.py
@@ -5,50 +5,67 @@ $Id$
 __version__='$Revision$'
 
 
-### JABHACKALERT: Need to remove Back/Forward buttons, because they do not
-### do what the user would expect.  
+# CEB: simplification of testpattern.TestPattern.
+
+## Needs to be upgraded to behave how we want, but should be simpler
+## to do that from here than the original test pattern.
+
+## Notes:
+# * need to remove audigen pgs
+# * there was: buttonBox.add('Use for future learning',command = self.use_for_learning)
+# * missing disparity flip hack (though see JABHACKALERT below)
+# * missing 'reset to defaults'
+
+### JABHACKALERT: Should use PatternPresenter (from
+### topo.commands.analysis), which will allow flexible support for
+### making objects with different parameters in the different eyes,
+### e.g. to test ocular dominance or disparity.
 
 
-# CEBALERT: taggedsliders refresh the display even when auto-refresh is off.
 
-# CB changing pattern doesn't refresh plots even when autorefresh is on.
-
-
-import copy
-import Pmw
+from Tkinter import Frame
 
 import topo
 
-import topo.base.sheetview
-from topo.base.parameterclasses import BooleanParameter,Number,ClassSelectorParameter
-
+from topo.base.sheetview import SheetView
+from topo.base.parameterclasses import BooleanParameter,Number,ClassSelectorParameter,ObjectSelectorParameter
 from topo.base.patterngenerator import PatternGenerator
-
 from topo.sheets.generatorsheet import GeneratorSheet
 from topo.commands.basic import pattern_present
-
 from topo.misc.keyedlist import KeyedList
+from topo.plotting.plot import make_template_plot
+from topo.plotting.plotgroup import SheetPlotGroup
 
 from parametersframe import XParametersFrame
 from plotgrouppanel import XPGPanel
-
-from topo.plotting.plot import make_template_plot
-
-from Tkinter import IntVar, StringVar, Checkbutton
-from Tkinter import TOP, LEFT, RIGHT, BOTTOM, YES, N, S, E, W, X,NO,NONE
-
-
 from tkparameterizedobject import ButtonParameter
+
 
 DEFAULT_PRESENTATION = 1.0
 
 
-from topo.plotting.plotgroup import SheetPlotGroup
-
 class TestPatternPlotGroup(SheetPlotGroup):
-    def __init__(self,**params):
-        super(TestPatternPlotGroup,self).__init__(**params)
 
+    def __init__(self,sheets=None,**params):
+        super(TestPatternPlotGroup,self).__init__(sheets=sheets,**params)
+            
+    def _plot_list(self):
+        plot_list = []
+        for sheet in self._sheets():
+            plot_list.append(self._create_plot(sheet))
+
+        return plot_list
+        
+    def _create_plot(self,sheet):
+        new_view = SheetView((sheet.input_generator(),sheet.bounds),
+                              sheet.name,sheet.precedence,topo.sim.time())        
+        sheet.sheet_view_dict['Activity']=new_view
+        channels = {'Strength':'Activity','Hue':None,'Confidence':None}
+
+        ### JCALERT! it is not good to have to pass '' here... maybe a test in plot would be better
+        p = make_template_plot(channels,sheet.sheet_view_dict,
+                               sheet.xdensity,sheet.bounds,self.normalize,name='')
+        return p
         
 
 
@@ -56,138 +73,108 @@ class TestPatternPlotGroup(SheetPlotGroup):
 
 class TestPattern(XPGPanel):
 
-    sheet_type = GeneratorSheet
     plotgroup_type = TestPatternPlotGroup
+
+    edit_sheet = ObjectSelectorParameter()
 
     learning = BooleanParameter(default=False,doc="""Whether to enable learning during presentation.""")
     duration = Number(default=1.0,doc="""How long to run the simulator when presenting.""")
 
     present = ButtonParameter(doc="""Present this pattern to the simulation.""")
-    reset = ButtonParameter(doc="""Reset the parameters for this pattern back to their defaults.""")
 
     pattern_generator = ClassSelectorParameter(class_=PatternGenerator, doc="""Type of pattern to present. Each type will have various parameters that can be changed.""")
-    
-
-    def __init__(self,console,master,label="Preview",**params):
-	super(TestPattern,self).__init__(console,master,label,**params)
-        self.auto_refresh=True
-
-        #self.plotgroup.params()['sheet'].range=
-
-        print "create_frame"
-
-        self.params_frame = XParametersFrame(self,PO=self.pattern_generator,
-                                               on_modify=self.refresh)
-        self.params_frame.hide_param('Close')
-
-
-        ### Find generator sheets
-        gsnames = topo.sim.objects(GeneratorSheet).keys()
-        self.generator_sheets_patterns = dict.fromkeys(gsnames,topo.sim[gsnames[0]].input_generator)
-
-
-        gsig = GeneratorSheet.classparams()['input_generator']
-
-        pgparam = self.params()['pattern_generator']
-        #pgparam.range = [pg() for pg in gsig.range().values()]
-        self.pattern_generator = gsig.default
-        #pgparam.default = GeneratorSheet.classparams()['input_generator'].default        
-        self.pack_param('pattern_generator',on_change=self.change_pattern_generator)
-
-
-
-        #self.params_frame.create_widgets(self.pattern_generator) 
-        self.params_frame.pack(side=TOP,expand=YES,fill=X)
-
-
-
-
-
-        self.pack_param('learning')
-        self.pack_param('duration')
-
-        self.pack_param('present',on_change=self.present_pattern)
-        self.pack_param('reset',on_change=self.reset_to_defaults)
-        
-        
-        # CB: remember
-        # - to remove audiogen
-        # - editing 'pattern_generator': None} 
-
-
-        
-
-        # SELECT GENERATORSHEET BUTTONS
-##         ###############################################################################################
-##         ### 'Edit patterns in' boxes
-##         #
-##         # CEBHACKALERT: Buttons seem to have strange behavior!
-##         # Also, will boxes be in the same order as the plots?
-##         self.__input_box = Pmw.RadioSelect(self, labelpos = 'w',
-##                                            command = self._input_change,
-##                                            label_text = 'Apply to pattern in:',
-##                                            selectmode = 'multiple')
-##         self.__input_box.pack(expand=NO,fill=NONE,padx=5)
-
-##         keys = copy.copy(self.generator_sheets_patterns.keys())
-##         keys.reverse()
-##         for generator_sheet_name in keys:
-##             self.__input_box.add(generator_sheet_name)
-##             self.__input_box.invoke(generator_sheet_name)
-##         ###############################################################################################
-            
-
-        
-	self.refresh()
-
-
 
 
     @staticmethod
     def valid_context():
-        """
-        Only open if GeneratorSheets are in the Simulation.
-        """
-        sim = topo.sim
-        if sim.objects(GeneratorSheet).items():
+        """Only open if GeneratorSheets are in the Simulation."""
+        if topo.sim.objects(GeneratorSheet).items():
             return True
         else:
             return False
 
 
-    def refresh(self):
-        print "refresh()"
-        self.update_plotgroup_variables()
-        self.__setup_pattern_generators()
-        super(TestPattern,self).refresh(update=True)
+    def generate_plotgroup(self):
+        # CB: could copy the sheets
+        sheets = []
+        for gen_sheet in topo.sim.objects(GeneratorSheet).values():
+            gscopy = GeneratorSheet(
+                name=gen_sheet.name,
+                nominal_density=gen_sheet.nominal_density,
+                nominal_bounds=gen_sheet.nominal_bounds)
+            sheets.append(gscopy)
+
+        return self.plotgroup_type(
+            sheets=sheets)
 
 
-    # METHODS FOR BUTTONS
-    ###############################################################################################
-##     def _input_change(self,button_name, checked):
-##         """
-##         Called by the input box.  The variable self.generator_sheets_patterns records
-##         all input event processors, and whether they are checked or
-##         not.
-##         """
-##         #self.generator_sheets_patterns[button_name]['editing'] = checked
+    def __init__(self,console,master,label="Preview",**params):
+	super(TestPattern,self).__init__(console,master,label,**params)
 
-##         #if not self.generator_sheets_patterns[button_name]['editing']:
-##         #    self.generator_sheets_patterns[button_name]['pattern_generator'] = copy.copy(self.__current_pattern_generator)
-##         #else:
-##         self.__setup_pattern_generators()
-##         if self.auto_refresh:self.refresh()
+        self.auto_refresh = True
+
+        # Remove plot_ & update_command
+        self.plotcommand_frame.pack_forget()
+        for name in ['update_command','Fwd','Back']:
+            self.hide_param(name) 
+
+        edit_sheet_param = self.get_parameter_object('edit_sheet')
+        edit_sheet_param.Arange = self.plotgroup._sheets()
+
+
+        self.pg_control_pane = Frame(self,bd=1,relief="sunken")
+        self.pg_control_pane.pack(side="top",expand='yes',fill='x')
+        
+        self.params_frame = XParametersFrame(self.pg_control_pane,
+                                             PO=self.pattern_generator,
+                                             on_modify=self.conditional_refresh)
+
+        self.params_frame.hide_param('Close')
+
+
+        self.pack_param('edit_sheet',parent=self.pg_control_pane,on_modify=self.switch_sheet)
+        self.pack_param('pattern_generator',parent=self.pg_control_pane,
+                        on_modify=self.change_pattern_generator,
+                        side="top")
+        
+        self.change_pattern_generator()
+
+        
+        present_frame = Frame(self)
+        present_frame.pack(side='bottom')
+
+        self.pack_param('learning',side='bottom',parent=present_frame)
+
+        self.params_frame.pack(side='bottom',expand='yes',fill='x')
+        
+        self.pack_param('duration',parent=present_frame,
+                        side="left")
+        self.pack_param('present',parent=present_frame,on_change=self.present_pattern,
+                        side="right")
+
+        
+    def switch_sheet(self):
+        self.pattern_generator = self.edit_sheet.input_generator
+        self.change_pattern_generator()
+
+
+    def conditional_refresh(self):
+        if self.auto_refresh:self.refresh()
+
         
     def change_pattern_generator(self):
         """
         Set the current PatternGenerator to the one selected and get the
         ParametersFrame to draw the relevant widgets
         """
-        print "change_pattern_generator()"
-        # CEBALERT: if test to hide a logic error!
-        if hasattr(self,'params_frame'): #and self.pattern_generator is not None:
-            self.params_frame.create_widgets(self.pattern_generator) 
-        if self.auto_refresh: self.refresh()
+        self.params_frame.create_widgets(self.pattern_generator)
+
+        for sheet in self.plotgroup._sheets():
+            if sheet==self.edit_sheet:
+                sheet.set_input_generator(self.pattern_generator)
+        
+        self.conditional_refresh()
+
 
     def present_pattern(self):
         """
@@ -196,10 +183,8 @@ class TestPattern(XPGPanel):
         patterns.
         """
 	topo.sim.state_push()
-        self.__setup_pattern_generators()
 
-        input_dict = dict([(name,pg)
-                           for name,pg in self.generator_sheets_patterns.items()])
+        input_dict = dict([(sheet.name,sheet.input_generator) for sheet in self.plotgroup._sheets()])
         
         pattern_present(input_dict,self.duration,
                         learning=self.learning,overwrite_previous=False)
@@ -207,67 +192,13 @@ class TestPattern(XPGPanel):
         self.console.auto_refresh()
 	topo.sim.state_pop()
 
-    def reset_to_defaults(self):
-        """
-        Reset to default presentation length, no learning, and the original PatternGenerator.
-        """
-        self.duration=DEFAULT_PRESENTATION
-        #self.learning_button.deselect()
-        #self.pg_choice_box.invoke(self.__default_pattern_generator_name)
-        if self.auto_refresh: self.refresh()
-
-    ###############################################################################################
 
 
-    def update_plotgroup_variables(self):
-        print "update_plotgroup_variables()"
-        plot_list = []       
-        for sheetname,pg in self.generator_sheets_patterns.items():
-            view_dict = {}
-            # xdensity and ydensity are the same for the pattern_generator
-            # because they were set from a Sheet xdensity and ydensity,
-            # which are guaranteed to be the same
-	    density = pg.xdensity
-            
-	    view_dict[sheetname] = topo.base.sheetview.SheetView((pg(),pg.bounds),src_name=sheetname)
-	    channels = {'Strength':sheetname,'Hue':None,'Confidence':None}
-	    ### JCALERT! it is not good to have to pass '' here... maybe a test in plot would be better
-	    plot_list.append(make_template_plot(channels,view_dict,density,None,self.normalize,name=''))
-
-        self.plotgroup.plot_list = plot_list
-	self.plotgroup.time = topo.sim.time()
- 				
     
 
-    ### JABHACKALERT: This should be replaced with a PatternPresenter
-    ### (from topo.commands.analysis), which will allow flexible
-    ### support for making objects with different parameters in the
-    ### different eyes, e.g. to test ocular dominance or disparity.
 
-    # (there was a) buttonBox.add('Use for future learning',command = self.use_for_learning)
-    def __setup_pattern_generators(self):
-        """
-        Make an instantiation of the current user patterns.
 
-        Also does some other things...
-        """
-        print "setup_pattern_generators()"
-        # CB: remember to replace disparity flip stuff.
-        
-        self.params_frame.set_parameters()
 
-        for sheetname in self.generator_sheets_patterns:
-            #if o_s_p['editing']==True:
-                
-            newpattern = copy.deepcopy(self.params_frame._extraPO)
-
-            sheet = topo.sim[sheetname]
-            
-            newpattern.bounds   = copy.deepcopy(sheet.bounds)
-            newpattern.xdensity = sheet.xdensity
-            newpattern.ydensity = sheet.ydensity
-
-            self.generator_sheets_patterns[sheetname] = newpattern
-
+    
 
 
