babd9001ac02797981e3877415c30b97c2f6f105
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-09-01 17:35:40 +0000

    Removed original TaggedSlider class. Makeshift TaggedSlider2 (what we are currently using) becomes TaggedSlider. Added TaggedSlider3, a far simpler class which appears to perform the same, but isn't quite finished.

--- topo/tkgui/tkguiwindow.py
+++ topo/tkgui/tkguiwindow.py
@@ -219,7 +219,8 @@ class WidgetTranslator(object):
             return self.get()
 
 
-class TaggedSlider(Frame,WidgetTranslator):
+
+class TaggedSlider(Frame):
     """
     Widget for manipulating a numeric value using either a slider or a
     text-entry box, keeping the two values in sync.
@@ -232,152 +233,10 @@ class TaggedSlider(Frame,WidgetTranslator):
     point out it only expects strings to come in, converts internally.
 
     """
-    def __init__(self,root,
-                 variable=None,
-                 min_value="0.0",
-                 max_value="1.0",
-                 string_format = '%f',
-                 tag_width=10,
-                 slider_length=100,
-                 translator=string.atof,
-                 **params):
-
-        Frame.__init__(self,root,**params)
-        WidgetTranslator.__init__(self,translator=translator)
-
-        self.root = root
-        self.__string_format = string_format
-        
-        # Add the tag
-        self.__tag_val = variable
-        self.__tag = Entry(self,textvariable=self.__tag_val,width=tag_width)
-        self.__tag.bind('<FocusOut>', self.refresh) # slider is updated on tag return... 
-        self.__tag.bind('<Return>', self.action)   # ...and on tag losing focus
-        self.__tag.pack(side=LEFT)
-
-        self.set_bounds(min_value,max_value,refresh=False)
-
-        
-        # Add the slider        
-        self.__slider_val = IntVar(0)
-        self.__slider = Scale(self,showvalue=0,from_=0,to=10000,
-                              orient='horizontal',
-                              length=slider_length,
-                              variable=self.__slider_val,
-                              command=self.__slider_command)
-        self.__slider.pack(side=LEFT,expand=YES,fill=BOTH)
-        self.__set_slider_from_tag()
-        self.__first_slider_command = True          # see self.__slider_command below
-
-
-    def set_bounds(self,min,max,refresh=True):
-        self.__min_value=self.translator(min)
-        self.__max_value = self.translator(max)
-        if refresh: self.refresh()
-
-    # CEBALERT: I find the refresh() and optional_refresh() methods
-    # confusing. Is there a simpler way to implement this kind of
-    # functionality?    
-    def refresh(self,e=None):
-        """
-        Sets the slider's position according to the value in the tag.
-        Additionally, calls the parent widget's optional_refresh()
-        method.
-        """
-        self.__set_slider_from_tag()
-        try:
-            # Refresh the ParametersFrame (if embedded in one)
-            self.root.optional_refresh()
-        except AttributeError:
-            pass
-
-    # CB: having said the above, I compounded the problem by adding
-    # more of the same.
-    def action(self, e=None):
-        """
-        Pressing return in a TaggedSlider causes the slider itself to
-        be refresh()ed, but also (optionally) allows an action to be
-        'passed up' to its parent.
-        """
-        try:
-            self.root.optional_action()
-        except AttributeError:
-            pass
-        self.refresh()
-
-
-    # CEBALERT: the comment is true, this is required. But
-    # maybe there's a cleaner way?
-    def __slider_command(self,arg):
-        """
-        When this frame is first shown, it calls the slider callback,
-        which would overwrite the initial string value with a string
-        translation (e.g. 'PI' -> '3.142').  This code prevents that.
-        """
-        if not self.__first_slider_command:
-            self.__set_tag_from_slider()
-            try:
-                # Refresh the ParametersFrame (if embedded in one)
-                self.root.optional_refresh()
-            except AttributeError:
-                pass
-        else:
-            self.__first_slider_command = False
-
-
-    def get(self):
-        """
-        Return the (string) value stored in the tag.
-        """
-        return self.__tag_val.get()
-
-     
-
-    def __set_tag_from_slider(self):
-        new_string = self.__string_format % self.__get_slider_value()
-        self.__tag_val.set(new_string)
-
-         
-    def __set_slider_from_tag(self):
-        """
-        Set the slider (including its movement limits) to match the tag value.
-        """
-        val = self.translator(self.__tag_val.get())
-        if val > self.__max_value:
-            self.__max_value = val
-        elif val < self.__min_value:
-            self.__min_value = val
-
-        self.__set_slider_value(val)
-
-
-    def __get_slider_value(self):
-        # CEBALERT: range is an inbuilt function!
-        range = self.__max_value - self.__min_value
-        return self.__min_value + (self.__slider_val.get()/10000.0 * range)
-    
-    def __set_slider_value(self,val):
-        range = self.__max_value - self.__min_value
-        assert range!=0, "A TaggedSlider cannot have a maximum value equal to its minimum value."
-        new_val = 10000 * (val - self.__min_value)/range
-        self.__slider_val.set(int(new_val))
-
-
-class TaggedSlider2(Frame):
-    """
-    Widget for manipulating a numeric value using either a slider or a
-    text-entry box, keeping the two values in sync.
-
-    The expressions typed into the text-entry box are evaluated using
-    the given translator, which can be overridden with a custom
-    expression evaluator (e.g. to do a Python eval() in the namespace
-    of a particular object.)
-
-    point out it only expects strings to come in, converts internally.
-
-    """
-    def __init__(self,master,variable,min_value=0.0,max_value=1.0,string_format=None,
+    def __init__(self,master,variable,min_value=0.0,max_value=1.0,
                  tag_width=10,slider_length=100,translator=None,**config):
+
+        # in calls: replace string_format with resolution
 
         Frame.__init__(self,master,**config)
         self.master = master
@@ -388,7 +247,7 @@ class TaggedSlider2(Frame):
         self.__tag.pack(side=LEFT)
         
         self.__tag.bind('<FocusOut>', self.refresh) # slider is updated on tag return... 
-        self.__tag.bind('<Return>', self.action)   # ...and on tag losing focus
+
         
 
         self.set_bounds(min_value,max_value,refresh=False)
@@ -501,7 +360,46 @@ class TaggedSlider2(Frame):
         self.__slider_val.set(int(new_val))
 
 
-TaggedSlider=TaggedSlider2
+
+class TaggedSlider3(Frame):
+    def __init__(self,master,variable,min_value=0,max_value=1,resolution=0.001,slider_length=100,
+                 string_format=None,tag_width=10,
+                 tag_extra_config={},slider_extra_config={}):
+
+        Frame.__init__(self,master)
+
+        self.variable=variable
+        
+        self.tag = Entry(self,textvariable=variable,width=tag_width,
+                           **tag_extra_config)
+        self.tag.pack(side='left')
+        self.tag.bind('<Return>', self.action)  
+        
+        self.slider = Scale(self,variable=variable,
+                           from_=min_value,to_=max_value,resolution=resolution,
+                           showvalue=0,orient='horizontal',length=slider_length,
+                           **slider_extra_config)
+        self.slider.pack(side='right')
+
+    def action(self,event=None):
+        raise NotImplementedError
+    def refresh(self,event=None):
+        raise NotImplementedError
+
+    # convenience methods
+    def set_bounds(self,min_val,max_val,refresh=True):
+        self.slider.config(from_=min_val,to_=max_val)
+
+    def config(self,**options):
+
+        if 'state' in options:
+            self.tag['state']=options['state']
+            self.slider['state']=options['state']
+            del options['state']
+
+        if len(options)>0:
+            raise NotImplementedError("Only state option is currently supported; \
+            set options on either the tag or the slider.")
 ######################################################################
         
 
