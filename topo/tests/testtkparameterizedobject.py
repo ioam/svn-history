287bd3b91545f67b0984f7c1ae0bb6bbd3c06513
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-09-14 09:59:02 +0000

    Commented out code that conditionally starts tkgui - still causes problems with other tests.

--- topo/tests/testtkparameterizedobject.py
+++ topo/tests/testtkparameterizedobject.py
@@ -26,10 +26,11 @@ from topo.outputfns.basic import PiecewiseLinear
 from topo.tkgui.tkparameterizedobject import TkParameterizedObject
 
 
-# In case this test is run alone, start tkgui
-# (otherwise simulation of gui set()s and get()s doesn't work)
-if not hasattr(topo,'guimain'):
-    import topo.tkgui; topo.tkgui.start()
+# CEBALERT: can't have this code present when doing "make tests" - why?
+## # In case this test is run alone, start tkgui
+## # (otherwise simulation of gui set()s and get()s doesn't work)
+## if not hasattr(topo,'guimain'):
+##     import topo.tkgui; topo.tkgui.start()
 
 
 class SomeFrame(TkParameterizedObject,Frame):
