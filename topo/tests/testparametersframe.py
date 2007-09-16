af3529f0ae3aa4a7083988f3f425bade0635a2e2
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-09-16 10:56:46 +0000

    Added test.

--- topo/tests/testparametersframe.py
+++ topo/tests/testparametersframe.py
@@ -21,14 +21,6 @@ from topo.tkgui.parametersframe import ParametersFrame
 
 
 
-# CEBALERT: can't have this code present when doing "make tests" - why?
-## # In case this test is run alone, start tkgui
-## # (otherwise simulation of gui set()s and get()s doesn't work)
-## import topo
-## if not hasattr(topo,'guimain'):
-##     import topo.tkgui; topo.tkgui.start()
-
-
 class TestPO(ParameterizedObject):
     boo = BooleanParameter(default=True)
     osp = ObjectSelectorParameter()
@@ -38,6 +30,8 @@ class TestPO(ParameterizedObject):
     nu = Number(default=1.0,bounds=(-1,1))
     st = StringParameter("string1")
 
+
+    
 
 class TestParametersFrame(unittest.TestCase):
 
@@ -61,6 +55,53 @@ class TestParametersFrame(unittest.TestCase):
         self.assertEqual(self.f._extraPO,self.testpo1)
         self.f.set_PO(self.testpo2)
         self.assertEqual(self.f._extraPO,self.testpo2)
+
+
+
+    def test_initial_update_parameters(self):
+        """
+        Checking for specific bug: quotation marks being
+        added to displayed values.
+
+        (Only time should get quotation marks is when
+        editing a Parameter and trying to create an instance
+        of a class that hasn't been imported: then string is
+        assumed, and quotes are added - since it's not a
+        StringParameter.)
+        """
+
+        ## check that update doesn't affect the variable values
+        orig_values = {}
+        for param_name,tkvar in self.f._tk_vars.items():
+            orig_values[param_name] = tkvar._original_get()
+
+        self.f.update_parameters()
+
+        for param_name,val in orig_values.items():
+            self.assertEqual(self.f._tk_vars[param_name].get(),val)
+
+
+        ## check that *displayed* values don't change 
+        orig_values = {}
+        for param_name,representation in self.f.representations.items():
+            widget = representation['widget']
+
+            # (not possible to get value from these widgets)
+            if widget.__class__ is not Tkinter.Button and \
+               widget.__class__ is not Tkinter.OptionMenu and \
+               widget.__class__ is not Tkinter.Checkbutton:
+
+                orig_values[param_name] = widget.get()
+        
+        self.f.update_parameters()
+
+        for param_name,val in orig_values.items():
+            widget = self.f.representations[param_name]['widget']
+            self.assertEqual(widget.get(),val)
+                             
+            
+
+    
         
 
 
