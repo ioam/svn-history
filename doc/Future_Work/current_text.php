5125a6196298d75b08e16c38f656b3f3b39ed5b1
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-10-11 08:37:24 +0000

    Update to scrollbars task.

--- doc/Future_Work/current_text.php
+++ doc/Future_Work/current_text.php
@@ -43,11 +43,7 @@ tracker rather than added to this list</B>.
 <li>Cleanup + doc of tkparameterizedobject.py and parametersframe.py</li>
 <li>Cleanup + doc of *panel.py files</li>
 <li>Cleanup + doc of plotgroup.py</li>
-<li>Reorganize classes and filenames in tkgui</li>
-<li>scrollbars: clean up behavior (can we stop bars coming on and going off
-when resize pressed? can we stop empty space when there are no bars? can we stop
-bars being there when not required [a bwidget bug])? why is 'press refresh to see
-plots...' text not centered? clean up + doc code. </li>
+<li>scrollbars: can we stop empty space when there are no bars? clean up + doc code. What to do about user resizing window?</li>
 <li>finish basic developer manual tkgui intro</li>
 <li>on windows, progress  bar always flicks on for a small fraction of a second
 (even for runs of 1).</li>
