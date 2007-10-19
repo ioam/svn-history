7e0e223f83e1dfc43fb13c2d91f4403f88585861
James A. Bednar <jbednar@inf.ed.ac.uk>
2007-10-19 00:29:50 +0000

    Updated docs and list of packages

--- topo/misc/__init__.py
+++ topo/misc/__init__.py
@@ -1,15 +1,15 @@
 """
 Miscellaneous functions used by Topographica files outside topo.base.
 
-This package should be self-contained, i.e., should not refer to any
-other part of Topographica.  For instance, no file may include an
+This package is intended to be self-contained, i.e., should not refer to any
+other part of Topographica.  For instance, files should not generally include an
 import statement like 'from topo.package.module import' or 'import
-topo.package.module'.  Thus these files should be easily available for
-anyone to copy and use in their own unrelated projects; they are not
-specific to Topographica in any way.
+topo.package.module'.  Those that follow this convention will be
+easily available for anyone to copy and use in their own unrelated
+projects; they are not specific to Topographica in any way.
 
 $Id$
 """
 __version__='$Revision$'
 
-__all__=['commandline','distribution','gendocs','inlinec','keyedlist','numbergenerators','patternfns','utils']
+__all__=['commandline','distribution','filepaths','gendocs','inlinec','keyedlist','numbergenerators','patternfns','traces','utils']
