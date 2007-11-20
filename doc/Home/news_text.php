c2f10d67a335455e9e763fde644693647bcca1d4
James A. Bednar <jbednar@inf.ed.ac.uk>
2007-11-20 12:57:11 +0000

    Made separate file for news

---
+++ doc/Home/news_text.php
@@ -0,0 +1,93 @@
+<?php
+print '<table width="100%" cellpadding="5"><tr><td bgcolor="'.$banner_bg_color.'">'
+?>
+<blockquote>
+<i>
+<b>News:</b>
+
+
+<p><b>26 Oct 2007:</b> Version 0.9.4 
+<A target="_top" href="../Downloads/index.html">released</A>, including:
+
+<center>
+<table width="100%" cellpadding="5">
+<tr>
+<td width="50%">
+<dl COMPACT>
+<font size="-1">  
+<dt>General improvements:</dt>
+<dd>
+  numerous bugfixes<br>
+  set up <A target="_top" href="http://buildbot.topographica.org">automatic daily builds</A><br>
+</dd>
+<dt>Example scripts:</dt>
+<dd>
+  new whisker barrel cortex simulation<br>
+  &nbsp;&nbsp;(using transparent Matlab wrapper)<br>
+  new elastic net ocular dominance simulation<br>
+  new spiking example; still needs generalizing<br>
+</dd>
+<dt>Command-line and batch:</dt>
+<dd>
+  <!-- <A target="_top" href="../User_Manual/commandline.html#option-a">-a
+  option to import commands automatically<br> -->
+  <A target="_top" href="../User_Manual/batch.html">batch 
+  mode</A> for running multiple similar simulations<br>
+  <A target="_top" href="../User_Manual/commandline.html#saving-bitmaps">saving 
+  bitmaps</A> from script/command-line (for batch runs)<br>
+  script/command-line <A target="_top" href="../User_Manual/commandline.html#scripting-gui">control over GUI</A><br>
+  <!-- grid_layout command to simplify model diagrams<br> -->
+  <!-- options for controlling plot sizing<br> -->
+  added auto-import option (-a and -g) to save typing<br>
+</dd>
+</font>
+</dl>
+</td>
+<td width="50%">
+<dl COMPACT>
+<font size="-1">
+<dt>GUI:</dt>
+<dd>
+  greatly simplified adding GUI code <!--<A target="_top" href="../Developer_Manual/gui.html#programming-tkgui">adding GUI code</A>--><br>
+  <!--  added GUI tests<br> -->
+  <!--  added optional pretty-printing for parameter names in GUI<br> -->
+  added progress bars, scroll bars, window icons<br>
+  new Step button on console
+  <!-- changed -g to launch the GUI where it is specified, to allow more control<br> -->
+  <!-- added categories for plots to simplify GUI<br> -->
+</dd>
+<dt>Plotting:</dt>
+<dd>
+  <A target="_top" href="../User_Manual/plotting.html#rfplots">reverse-correlation RF mapping</A><br>
+  <A target="_top" href="../User_Manual/commandline.html#3d-plotting">3D 
+  wireframe plotting</A> (in right-click menu)<br>
+  gradient plots, histogram plots (in right-click menu)<br>
+  <A target="_top" href="../User_Manual/plotting.html#measuring-preference-maps">simplified
+  bitmap plotting</A> (removed template classes)<br>
+  GUI plots can be saved as PNG or EPS (right-click menu)<br>
+</dd>
+<dt>Component library:</dt>
+<dd>
+  new
+  <A HREF="../Reference_Manual/topo.coordmapperfns-module.html">
+  coordmapperfns</A> (Grid, Pipeline, Polar/Cartesian)<br>
+  <!-- OutputFnDebugger for keeping track of statistics<br> -->
+</dd>
+</font>
+</dl>
+</td>
+</tr>
+</table>
+</center>
+
+Screenshots: 
+<A target="_top" href="../images/071018_plotting1_ubuntu.png">plotting 1</A>, 
+<A target="_top" href="../images/071018_plotting2_ubuntu.png">plotting 2</A>, 
+<A target="_top" href="../images/071018_modeleditor_ubuntu.png">model editor</A>.
+
+<p><A target="_top" href="../Home/oldnews.html"><i>Older news</i></A>
+</i>
+
+
+</blockquote>
+</td></tr></table>
