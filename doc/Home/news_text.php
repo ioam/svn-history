f11e67a3a24d73e7ced6916f924fb4ddbed0e2a7
James A. Bednar <jbednar@inf.ed.ac.uk>
2007-11-20 13:45:13 +0000

    Updated with news since 0.9.4

--- doc/Home/news_text.php
+++ doc/Home/news_text.php
@@ -5,9 +5,7 @@ print '<table width="100%" cellpadding="5"><tr><td bgcolor="'.$banner_bg_color.'
 <i>
 <b>News:</b>
 
-
-<p><b>26 Oct 2007:</b> Version 0.9.4 
-<A target="_top" href="../Downloads/index.html">released</A>, including:
+<p><b>20 November 07:</b> Updates since version 0.9.4:
 
 <center>
 <table width="100%" cellpadding="5">
@@ -18,27 +16,30 @@ print '<table width="100%" cellpadding="5"><tr><td bgcolor="'.$banner_bg_color.'
 <dt>General improvements:</dt>
 <dd>
   numerous bugfixes<br>
-  set up <A target="_top" href="http://buildbot.topographica.org">automatic daily builds</A><br>
+  fixed a number of pychecker warnings.<BR>
+  moved current to-do items to the sf.net trackers<BR>
+  
+  
+<!--  made start() method of EventProcessors be called just before the first
+  execution of the simulator after the EP is added, e.g. to allow joint
+  normalization across a Sheet's projections<BR> -->
+  simulation can now be locked to real time<BR>
+  added optional XML snapshot saving and loading (do 'make -C external gnosis')<BR>
+  
 </dd>
 <dt>Example scripts:</dt>
 <dd>
-  new whisker barrel cortex simulation<br>
-  &nbsp;&nbsp;(using transparent Matlab wrapper)<br>
-  new elastic net ocular dominance simulation<br>
-  new spiking example; still needs generalizing<br>
+  new example files for robotics interfacing (misc/playerrobot.py, misc/robotics.py)<BR>
 </dd>
 <dt>Command-line and batch:</dt>
 <dd>
-  <!-- <A target="_top" href="../User_Manual/commandline.html#option-a">-a
-  option to import commands automatically<br> -->
-  <A target="_top" href="../User_Manual/batch.html">batch 
-  mode</A> for running multiple similar simulations<br>
-  <A target="_top" href="../User_Manual/commandline.html#saving-bitmaps">saving 
-  bitmaps</A> from script/command-line (for batch runs)<br>
-  script/command-line <A target="_top" href="../User_Manual/commandline.html#scripting-gui">control over GUI</A><br>
-  <!-- grid_layout command to simplify model diagrams<br> -->
-  <!-- options for controlling plot sizing<br> -->
-  added auto-import option (-a and -g) to save typing<br>
+  added <A HREF="http://ipython.scipy.org">
+  IPython</A> (do 'make -C external ipython') for better command prompt, debugging, help<BR>
+  simplified commandline argument processing; now can do
+  topo.commandline.gui() or topo.commandline.auto_import_commands()
+  after Topographica has started.<BR>
+  simulation name set automatically from .ty script name by default<BR>
+  
 </dd>
 </font>
 </dl>
@@ -48,30 +49,21 @@ print '<table width="100%" cellpadding="5"><tr><td bgcolor="'.$banner_bg_color.'
 <font size="-1">
 <dt>GUI:</dt>
 <dd>
-  greatly simplified adding GUI code <!--<A target="_top" href="../Developer_Manual/gui.html#programming-tkgui">adding GUI code</A>--><br>
-  <!--  added GUI tests<br> -->
-  <!--  added optional pretty-printing for parameter names in GUI<br> -->
-  added progress bars, scroll bars, window icons<br>
-  new Step button on console
-  <!-- changed -g to launch the GUI where it is specified, to allow more control<br> -->
-  <!-- added categories for plots to simplify GUI<br> -->
+  cleaned up ParametersFrame and TaggedSlider behavior<BR>
 </dd>
 <dt>Plotting:</dt>
 <dd>
-  <A target="_top" href="../User_Manual/plotting.html#rfplots">reverse-correlation RF mapping</A><br>
-  <A target="_top" href="../User_Manual/commandline.html#3d-plotting">3D 
-  wireframe plotting</A> (in right-click menu)<br>
-  gradient plots, histogram plots (in right-click menu)<br>
-  <A target="_top" href="../User_Manual/plotting.html#measuring-preference-maps">simplified
-  bitmap plotting</A> (removed template classes)<br>
-  GUI plots can be saved as PNG or EPS (right-click menu)<br>
 </dd>
 <dt>Component library:</dt>
 <dd>
-  new
-  <A HREF="../Reference_Manual/topo.coordmapperfns-module.html">
-  coordmapperfns</A> (Grid, Pipeline, Polar/Cartesian)<br>
-  <!-- OutputFnDebugger for keeping track of statistics<br> -->
+  added code for estimating perceived values
+  (<A HREF="../Reference_Manual/topo.commands.analysis-module.html#decode_feature">commands.analysis.decode_feature</A>),<br>
+  &nbsp;&nbsp;e.g. for calculating aftereffects<BR>
+  Image class now allows camera inputs<BR>
+  <A HREF="../Reference_Manual/topo.outputfns.basic.PoissonSample-class.html">PoissonSample</A>
+  OutputFn class<BR>
+  Pipeline OutputFns can now be constructed easily using +<BR>
+  &nbsp;&nbsp;('x=HalfRectify() ; y=Square() ; z=x+y' gives 'z==PipelineOF(output_fns=x,y)')<BR>
 </dd>
 </font>
 </dl>
@@ -80,6 +72,7 @@ print '<table width="100%" cellpadding="5"><tr><td bgcolor="'.$banner_bg_color.'
 </table>
 </center>
 
+
 Screenshots: 
 <A target="_top" href="../images/071018_plotting1_ubuntu.png">plotting 1</A>, 
 <A target="_top" href="../images/071018_plotting2_ubuntu.png">plotting 2</A>, 
@@ -87,7 +80,6 @@ Screenshots:
 
 <p><A target="_top" href="../Home/oldnews.html"><i>Older news</i></A>
 </i>
-
 
 </blockquote>
 </td></tr></table>
