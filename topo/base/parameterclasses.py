8b929a3c1ec56b2b9aea3736629f2be21974f422
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-11-29 09:07:03 +0000

    Added ERRORALERT.

--- topo/base/parameterclasses.py
+++ topo/base/parameterclasses.py
@@ -491,6 +491,7 @@ class CompositeParameter(Parameter):
         """
         Return the values of all the attribs, as a list.
         """
+        # CBERRORALERT: advances Dynamic parameters
         if not obj:
             return [getattr(objtype,a) for a in self.attribs]
         else:
