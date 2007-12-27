f3cf5cc71d83b7c66ea216ce2fb275e97843165c
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-12-27 13:26:39 +0000

    Update to cvs2svn procedure.

--- doc/Future_Work/current_text.php
+++ doc/Future_Work/current_text.php
@@ -119,8 +119,7 @@ ERROR: A CVS repository cannot contain both ./topographica/external/pychecker-0.
 the directory at the root of the repository and running the following
 command: 
 
-cvs2svn --exclude="parametersframe_replacement_branch_FINAL" --exclude="before_merge_parametersframe_replacement_branch_200708291730BST" --exclude="before_merge_into_parametersframe_replacement_branch_200708271525BST" --exclude="before_merge_HEAD_in_200708271525BST" --exclude="parametersframe_replacement_branch" --exclude="tkgui_parameterization_branch" --exclude="shared_weights_use_views" --exclude="output_fn_has_return" --exclude="shelve_issue" --exclude="after_decref" --exclude="before_decref" --exclude="simulation_time_inclusive" --exclude="simulation_time_exclusive" --exclude="numpy_test_branch" --exclude="numpy_test_branch_merged" --exclude="LATEST_STABLE" --exclude="start" --exclude="InputSheet_per_kernel"  --dumpfile=svndump .
-
+cvs2svn --exclude="parametersframe_replacement_branch_FINAL" --exclude="before_merge_parametersframe_replacement_branch_200708291730BST" --exclude="before_merge_into_parametersframe_replacement_branch_200708271525BST" --exclude="before_merge_HEAD_in_200708271525BST" --exclude="parametersframe_replacement_branch" --exclude="tkgui_parameterization_branch" --exclude="shared_weights_use_views" --exclude="output_fn_has_return" --exclude="shelve_issue" --exclude="after_decref" --exclude="before_decref" --exclude="simulation_time_inclusive" --exclude="simulation_time_exclusive" --exclude="numpy_test_branch" --exclude="numpy_test_branch_merged" --exclude="LATEST_STABLE" --exclude="InputSheet_per_kernel" --exclude="topographica" --exclude="start" --dumpfile=svndump .
 
 
 
@@ -129,7 +128,12 @@ cvs2svn --exclude="parametersframe_replacement_branch_FINAL" --exclude="before_m
 
 7. use svndumpfilter to remove CVSROOT from repository:
 
-cat svndump | svndumpfilter exclude trunk/CVSROOT > svndump1
+cat svndump | svndumpfilter --drop-empty-revs --renumber-revs exclude trunk/CVSROOT tags/after_basic_optims/CVSROOT tags/CNV_COURSE/CVSROOT tags/first_numpy_version/CVSROOT tags/release_0_8_1/CVSROOT  tags/release_0_9_1/CVSROOT  tags/release_0_9_4/CVSROOT tags/april_2007_performance_testing/CVSROOT tags/cnv_course_2007/CVSROOT tags/last_numeric_version/CVSROOT tags/release_0_8_2/CVSROOT tags/release_0_9_2/CVSROOT tags/before_basic_optims/CVSROOT tags/cnv_course_2007_2/CVSROOT tags/release_0_8_0/CVSROOT tags/release_0_9_0/CVSROOT tags/release_0_9_3/CVSROOT > svndump1
+
+
+--drop-empty-revs, --renumber-revs:
+"Both of svndumpfilter's subcommands accept options for deciding how to deal with empty revisions. If a given revision contained only changes to paths that were filtered out, that now-empty revision could be considered uninteresting or even unwanted."
+
 
 
 8  Check the result:
@@ -140,7 +144,8 @@ svn checkout file:///home/ceball/topographica-svn/ topographica-co
 
 Then investigate it...
 
-
+Check-out the same branch/tag from both CVS and SVN into fresh directories
+Compare all files between the two (removing .svn/ and CVS/ directories)
 
 
 9. Tell people to stop CVS commits
@@ -203,6 +208,12 @@ Click the 'Finish Add' button
 15
 Remove all .cvsignore files from repository (cvs2svn did already take the information into svn:ignore
 properties).
+
+svn rm `find -name .cvsignore |xargs`
+svn commit -m ".cvs ignore information already exists in svn:ignore property." 
+
+** .cvsignore files will exist in tags/branches where they were included before **
+(so they aren't in release branches, which is the most important.
 
 
 
