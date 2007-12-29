0139ce862d79f98b5ca89d5df447e0e714240a8f
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2007-12-29 07:52:00 +0000

    Update to cvs2svn procedure (svn:eol=native).

--- doc/Future_Work/current_text.php
+++ doc/Future_Work/current_text.php
@@ -118,7 +118,7 @@ ERROR: A CVS repository cannot contain both ./topographica/external/pychecker-0.
 the directory at the root of the repository and running the following
 command: 
 
-cvs2svn --exclude="parametersframe_replacement_branch_FINAL" --exclude="before_merge_parametersframe_replacement_branch_200708291730BST" --exclude="before_merge_into_parametersframe_replacement_branch_200708271525BST" --exclude="before_merge_HEAD_in_200708271525BST" --exclude="parametersframe_replacement_branch" --exclude="tkgui_parameterization_branch" --exclude="shared_weights_use_views" --exclude="output_fn_has_return" --exclude="shelve_issue" --exclude="after_decref" --exclude="before_decref" --exclude="simulation_time_inclusive" --exclude="simulation_time_exclusive" --exclude="numpy_test_branch" --exclude="numpy_test_branch_merged" --exclude="LATEST_STABLE" --exclude="InputSheet_per_kernel" --exclude="topographica" --exclude="start" --use-cvs --dumpfile=svndump .
+cvs2svn --exclude="parametersframe_replacement_branch_FINAL" --exclude="before_merge_parametersframe_replacement_branch_200708291730BST" --exclude="before_merge_into_parametersframe_replacement_branch_200708271525BST" --exclude="before_merge_HEAD_in_200708271525BST" --exclude="parametersframe_replacement_branch" --exclude="tkgui_parameterization_branch" --exclude="shared_weights_use_views" --exclude="output_fn_has_return" --exclude="shelve_issue" --exclude="after_decref" --exclude="before_decref" --exclude="simulation_time_inclusive" --exclude="simulation_time_exclusive" --exclude="numpy_test_branch" --exclude="numpy_test_branch_merged" --exclude="LATEST_STABLE" --exclude="InputSheet_per_kernel" --exclude="topographica" --exclude="start" --use-cvs --default-eol="native" --dumpfile=svndump .
 
 
 
