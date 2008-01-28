fe4ecd7a3a4a0a03a2e321bd0b1299826386cbc1
Jan Antolik <antolikjan@gmail.com>
2008-01-28 14:30:14 +0000

    Removed some print statements that I forgot in the code of event_clear()

--- topo/base/simulation.py
+++ topo/base/simulation.py
@@ -1357,13 +1357,11 @@ class Simulation(ParameterizedObject):
         It keeps Trigger event on the event list as deleting this one corrupts simulation - it can be a NASTY HACK
         """
         events_temp = []
-        print self.events
         for e in self.events:
             if not isinstance(e,EPConnectionEvent):
               events_temp = events_temp + [e]
-            
         self.events = events_temp
-        print self.events
+
 
 
     # Could just process src and dest in conn_params.  
