d0b3443aa248ae267a9c1fbff009ec0609fa3ef8
James A. Bednar <jbednar@inf.ed.ac.uk>
2008-01-28 11:13:14 +0000

    Simplified news and made lines shorter to fit on smaller windows

--- doc/Home/news_text.php
+++ doc/Home/news_text.php
@@ -28,20 +28,18 @@ print '<table width="100%" cellpadding="5"><tr><td bgcolor="'.$banner_bg_color.'
   <A HREF="../Reference_Manual/topo.commands.basic-module.html#save_snapshot">saving</A> and
   <A HREF="../Reference_Manual/topo.commands.basic-module.html#load_snapshot">loading</A>
 <!-- (do 'make -C external gnosis') --><BR>
-  simpler and more complete support for dynamic parameter values<BR>  
+  simpler and more complete support for dynamic parameters<BR>  
   updated to Python 2.5<BR>
   source code repository moved from CVS to Subversion (<A HREF="../Downloads/cvs.html">SVN</A>)<BR>
 <!-- Simplified package importing API (promoting basic.py, etc.; not yet done) -->
 </dd>
 <dt>Command-line and batch:</dt>
 <dd>
-  command prompt now based on <A
-  HREF="http://ipython.scipy.org/">IPython</A> for better command prompt, debugging, help<BR> 
-  simplified commandline argument processing; now can do
-  <A HREF="../Reference_Manual/topo.misc.commandline-module.html#gui">topo.misc.commandline.gui()</A><br>
-  &nbsp;&nbsp;&nbsp;or
-  <A HREF="../Reference_Manual/topo.misc.commandline-module.html#auto_import_commands">topo.misc.commandline.auto_import_commands()</A>
-  after Topographica has started.<BR>
+  command prompt uses <A
+  HREF="http://ipython.scipy.org/">IPython</A> for better debugging, help<BR> 
+  command-line options can be called explicitly, e.g.<BR>
+  &nbsp;&nbsp;&nbsp;<A HREF="../Reference_Manual/topo.misc.commandline-module.html#gui">topo.misc.commandline.gui()</A> or<BR>
+  &nbsp;&nbsp;&nbsp;<A HREF="../Reference_Manual/topo.misc.commandline-module.html#auto_import_commands">topo.misc.commandline.auto_import_commands()</A><BR>
   simulation name set automatically from .ty script name by default<BR>
   
 </dd>
@@ -62,27 +60,25 @@ print '<table width="100%" cellpadding="5"><tr><td bgcolor="'.$banner_bg_color.'
 -->
 <dt>Example scripts:</dt>
 <dd>
-  new example files for robotics interfacing
-  (<A HREF="../Reference_Manual/topo.misc.playerrobot-module.html">misc/playerrobot.py</A>,
+  new example files for robotics interfacing<BR>
+  &nbsp;&nbsp;&nbsp;(<A HREF="../Reference_Manual/topo.misc.playerrobot-module.html">misc/playerrobot.py</A>,
   <A HREF="../Reference_Manual/topo.misc.robotics-module.html">misc/robotics.py</A>)<br>
 </dd>
 <dt>Component library:</dt>
 <dd>
-  added code for estimating perceived values
-  (<A HREF="../Reference_Manual/topo.commands.analysis-module.html#decode_feature">commands.analysis.decode_feature</A>),<br>
+  added <A HREF="../Reference_Manual/topo.commands.analysis-module.html#decode_feature">decode_feature</A> for estimating perceived values,<br>
   &nbsp;&nbsp;&nbsp;e.g. for calculating aftereffects<BR>
   PatternGenerator for real-time camera inputs<BR>
   Pipeline OutputFns can now be constructed easily using +<BR>
 <!-- &nbsp;&nbsp;&nbsp;('x=HalfRectify() ; y=Square() ; z=x+y' gives 'z==PipelineOF(output_fns=x,y)')<BR> -->
   new Output_Fn classes: 
-  <?php classref('topo.outputfns.basic','PoissonSample')?>,
-  <?php classref('topo.outputfns.homeostatic','ScalingOF')?> (for homeostatic plasticity),<BR>
+  <?php classref('topo.outputfns.basic','PoissonSample')?>,<BR>
+  &nbsp;&nbsp;&nbsp;<?php classref('topo.outputfns.homeostatic','ScalingOF')?> (for homeostatic plasticity),<BR>
   &nbsp;&nbsp;&nbsp;<?php classref('topo.outputfns.basic','AttributeTrackingOF')?> 
   (for analyzing or plotting values over time)<BR>
-  added post_initialization_weights_output_fn to
-  <?php classref('topo.sheets.lissom','LISSOM')?>,  to allow
-  normalization to<BR>
-  &nbsp;&nbsp;&nbsp;be turned off or changed after initialization<BR>
+  allowed <?php classref('topo.sheets.lissom','LISSOM')?>
+  normalization to be <A
+  HREF="../Reference_Manual/topo.sheets.lissom.LISSOM-class.html#post_initialization_weights_output_fn">changed</A> after initialization<BR>
   new CompositeSheetMask, AndMask, and OrMask classes<BR>
 </dd>
 </font>
