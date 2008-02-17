38dc680e386b23739d12aa38246b2fd2b6503cfd
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2008-02-17 11:35:22 +0000

    Continued writing.

--- doc/Downloads/git_text.php
+++ doc/Downloads/git_text.php
@@ -1,4 +1,4 @@
-<H2>Using Git to get Topographica's code</H2>
+<H2>Developing Topographica with Git</H2>
 
 
 <P>As well as git itself, you need to have git-svn.
@@ -6,6 +6,7 @@
 
 
 
+<H3>Getting the Topographica code</code>
 TOPOROOT=https://topographica.svn.sourceforge.net/svnroot/topographica
 
 <pre>
@@ -17,10 +18,20 @@ git-svn fetch -rN
 git-svn rebase
 </pre>
 
-ceball@doozy:~/temp$ git config --global user.name "C. Ball"
+(I just used 7986 but git is supposed to be fast enough and space-efficient enough that it doesn't really. Plus you only need to do it once, bedcause the rest of the time you can make branches from your 'master'.)
+
+<H3>Sending changes to Topographica's SVN repository</H3>
+
+<P>The first time, make sure you have identified yourself to git:
+<pre>
 ceball@doozy:~/temp$ git config --global user.email ceball@users.sf.net
 ceball@doozy:~/temp$ git config --global user.name "C. E. Ball"
+</pre>
 
+Then the following command will send each of your git commits to the SVN repository:
+<pre>
+git-svn dcommit
+</pre>
 
 
 References
