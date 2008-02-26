24d0493f4c472e0ce53ef5376171e1cdcb55520c
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2008-02-26 16:49:28 +0000

    Added comment.

--- topo/base/cf.py
+++ topo/base/cf.py
@@ -297,8 +297,10 @@ class ConnectionField(ParameterizedObject):
         if not (r1 == or1 and r2 == or2 and c1 == oc1 and c2 == oc2):
             # CEBALERT: it's faster to copy! Need to understand why.
             self.weights = (self.weights[r1-or1:r2-or1,c1-oc1:c2-oc1]).copy()
-            self.mask = self.weights_slice.submatrix(mask)
+            # CB: presumably it's not the copying - try this, too:
+            # self.weights = array(self.weights[r1-or1:r2-or1,c1-oc1:c2-oc1],copy=0)
 
+            self.mask = self.weights_slice.submatrix(mask)
             self.weights *= self.mask
             output_fn(self.weights)
             del self.norm_total
