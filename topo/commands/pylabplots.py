8423fa4f5c9b722f8a4c2b187ba3b1e6a667cc73
James A. Bednar <jbednar@inf.ed.ac.uk>
2008-04-04 10:15:33 +0000

    Updated usage tip

--- topo/commands/pylabplots.py
+++ topo/commands/pylabplots.py
@@ -1,6 +1,16 @@
 """
 Line-based and matrix-based plotting commands using MatPlotLib.
 
+Before importing this file, you will probably want to do something
+like:
+
+  from matplotlib import rcParams
+  rcParams['backend']='TkAgg'
+
+to select a backend, or else select an appropriate one in your
+matplotlib.rc file (if any).  There are many backends available for
+different GUI or non-GUI uses.
+
 $Id$
 """
 __version__='$Revision$'
