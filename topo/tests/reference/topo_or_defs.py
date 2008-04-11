95d25fb7c7c786f7fb007795f2520bf575a987d5
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2008-04-11 08:02:09 +0000

    All initial radii for weights now end in .5 (for odd weights matrices, and so that the radii are exactly half the matrix width).

--- topo/tests/reference/topo_or_defs.py
+++ topo/tests/reference/topo_or_defs.py
@@ -1,31 +1,36 @@
 ### import to _or_ reference simulations
 
+
+def half_odd_array(r):
+    """Return r that ends in .5"""
+    int_width = int(2*r)
+    # force odd width
+    if int_width%2==0:
+        int_width+=1
+    new_r = int_width/2.0
+    assert new_r-0.5==int(new_r)
+    return new_r
+
+
 def initialize_variables(BaseRN,BaseN):
 
     ############################################################
     # Variables that can be overridden & written into param file
-    rf_radius=BaseRN/4.0+0.5
-    # force odd aff weights matrix
-    if (2*rf_radius)%2==0:
-        rf_radius+=0.5
 
-    inh_rad=BaseN/4.0-1.0
-    # force odd inh weights matrix
-    if (2*inh_rad)%2==0:
-        inh_rad+=0.5
+    # force radii to end in .5, giving odd weights matrices with
+    # exactly 2xradius=matrix_width
 
-    for n,r in zip(['rf_radius','inh_rad'],[rf_radius,inh_rad]):
-        assert r>=1.5, "%s = %s; smaller than 1.5 - can't compare simulations"%(n,r)
-
-    exc_rad=max(2.5,BaseN/10.0)
+    # (could instead maybe set topographica's autosize_mask to false
+    # and set the mask sizes with the exact radii...)
+    rf_radius=half_odd_array(max(1.5,BaseRN/4.0+0.5))
+    inh_rad=half_odd_array(max(2.5,BaseN/4.0-1.0))
+    exc_rad=half_odd_array(max(2.5,BaseN/10.0))
     ############################################################
+
 
     rf_radius_scale=6.5/rf_radius
+    min_exc_rad=half_odd_array(max(1.5,BaseN/44.0))
 
-    min_exc_rad=max(1.5,BaseN/44)
-
-    ############################################################
-    # Variables to match 010910_or_map_512MB.param and or_defs
     area_scale=1.0
     #num_eyes=1
 
@@ -96,19 +101,25 @@ def add_scheduled_exc_bounds_changes(sim):
 ##     LE = sim['Primary'].projections('LateralExcitatory')
 ##     LE.initialized=False;LE.autosize_mask=False;LE.initialized=True
 ##     change_bounds = "LE.cf_shape.size=2.0*round(max(exc_rad,min_exc_rad))/BaseN;LE.change_bounds(BoundingBox(radius=exc_rad/BaseN,min_radius=min_exc_rad/BaseN));LE.learning_rate=alpha_exc*LE.n_units()"
+
+
+    change_bounds = "LE.change_bounds(BoundingBox(radius=half_odd_array(exc_rad)/BaseN,min_radius=min_exc_rad/BaseN));LE.learning_rate=alpha_exc*LE.n_units()"
+##     times = [199,499,999,1999,2999,3999,4999,6499,7999,19999]
+    e =     [0.6,0.7,0.8, 0.8, 0.8, 0.6, 0.6, 0.6, 0.6,  0.6]
     
-    change_bounds = "LE.change_bounds(BoundingBox(radius=exc_rad/BaseN,min_radius=min_exc_rad/BaseN));LE.learning_rate=alpha_exc*LE.n_units()"
-    
-    sim.schedule_command(199,'exc_rad=exc_rad*0.6; %s'%change_bounds)
-    sim.schedule_command(499,'exc_rad=exc_rad*0.7; %s'%change_bounds)
-    sim.schedule_command(999,'exc_rad=exc_rad*0.8; %s'%change_bounds)
-    sim.schedule_command(1999,'exc_rad=exc_rad*0.8; %s'%change_bounds)
-    sim.schedule_command(2999,'exc_rad=exc_rad*0.8; %s'%change_bounds)
-    sim.schedule_command(3999,'exc_rad=exc_rad*0.6; %s'%change_bounds)
-    sim.schedule_command(4999,'exc_rad=exc_rad*0.6; %s'%change_bounds)
-    sim.schedule_command(6499,'exc_rad=exc_rad*0.6; %s'%change_bounds)
-    sim.schedule_command(7999,'exc_rad=exc_rad*0.6; %s'%change_bounds)
-    sim.schedule_command(19999,'exc_rad=exc_rad*0.6; %s'%change_bounds)
+##     for t,i in zip(times,range(times)):
+##         sim.schedule_command(t,'exc_rad=half_odd_array(exc_rad*%s);%s'%(e[i],change_bounds))
+
+    sim.schedule_command(199,'exc_rad=exc_rad*%s; %s'%(e[0],change_bounds))
+    sim.schedule_command(499,'exc_rad=exc_rad*%s; %s'%(e[1],change_bounds))
+    sim.schedule_command(999,'exc_rad=exc_rad*%s; %s'%(e[2],change_bounds))
+    sim.schedule_command(1999,'exc_rad=exc_rad*%s; %s'%(e[3],change_bounds))
+    sim.schedule_command(2999,'exc_rad=exc_rad*%s; %s'%(e[4],change_bounds))
+    sim.schedule_command(3999,'exc_rad=exc_rad*%s; %s'%(e[5],change_bounds))
+    sim.schedule_command(4999,'exc_rad=exc_rad*%s; %s'%(e[6],change_bounds))
+    sim.schedule_command(6499,'exc_rad=exc_rad*%s; %s'%(e[7],change_bounds))
+    sim.schedule_command(7999,'exc_rad=exc_rad*%s; %s'%(e[8],change_bounds))
+    sim.schedule_command(19999,'exc_rad=exc_rad*%s; %s'%(e[9],change_bounds))
 
 
 def add_scheduled_exc_Lrate_changes(sim):
