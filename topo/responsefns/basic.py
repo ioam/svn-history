06d4387becfb2b798c5674f0ca794464a0e94d85
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2008-07-10 17:37:35 +0000

    Made basic ResponseFn classes available from topo.responsefns.

--- topo/responsefns/basic.py
+++ topo/responsefns/basic.py
@@ -5,6 +5,10 @@ $Id$
 """
 __version__='$Revision$'
 
+from topo.base.functionfamilies import ResponseFn
 
 # Imported here so that all ResponseFns will be in the same package
 from topo.base.cf import DotProduct
+
+__all__ = list(set([k for k,v in locals().items()
+                    if isinstance(v,type) and issubclass(v,ResponseFn)]))
