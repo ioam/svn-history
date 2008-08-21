1b3117f4dda646680b46845d8c390e02bdccc760
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2008-08-21 20:13:20 +0000

    Made save_plotgroup() work with a copy of the plotgroup.

--- topo/command/analysis.py
+++ topo/command/analysis.py
@@ -336,7 +336,7 @@ def save_plotgroup(name,saver_params={},**params):
     (To pass an optional parameter to the PlotFileSaver itself, the
     saver_params dictionary can be used.)
     """
-    plotgroup = plotgroups[name]
+    plotgroup = copy.deepcopy(plotgroups[name])
 
     # JABALERT: Why does a Projection plot need a Sheet parameter?
     # CB: It shouldn't, of course, since we know the sheet when we have
