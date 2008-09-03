c785103bd7d602d7caf6cc0d8d61c2c59d0eed68
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2008-09-03 23:17:35 +0000

    Added Mac screenshot.

--- doc/Home/news_text.php
+++ doc/Home/news_text.php
@@ -9,8 +9,8 @@ print '<table width="100%" cellpadding="5"><tr><td bgcolor="'.$banner_bg_color.'
 <A target="_top" href="../Downloads/index.html">released</A>; see the 
   <A target="_top" href="../News/index.html">News</A> page for more details.<BR>
   Screenshots: 
-  <A target="_top" href="../images/080903_plotting1_fedora.png">plotting 1</A> (Linux), 
-<!--  <A target="_top" href="../images/080903_plotting2_mac.png">plotting 2</A> (Mac), -->
+  <A target="_top" href="../images/080903_plotting1_fedora.png">plotting 1</A> (Linux),
+  <A target="_top" href="../images/080903_plotting2_mac.png">plotting 2</A> (Mac), 
   <A target="_top" href="../images/080903_docking_fedora.png">docking</A> (Linux),
   <A target="_top" href="../images/080903_modeleditor_fedora.png">model editor</A> (Linux).
 </i>
