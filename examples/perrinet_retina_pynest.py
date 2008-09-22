47435ce5672fae466ff1281209e1138af308296a
James A. Bednar <jbednar@inf.ed.ac.uk>
2008-09-22 11:35:41 +0000

    Reformatted and cleaned up for release

--- examples/perrinet_retina_pynest.py
+++ examples/perrinet_retina_pynest.py
@@ -1,25 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
+"""
+retina.py from Laurent Perrinet (laurent.perrinet [at] incm.cnrs-mrs.fr).
+
+This simulation is under development as part of the FACETS project
+(http://facets.kip.uni-heidelberg.de) and is being used in a
+large-scale spiking model of cortical columns in V1:
+
+  Kremkow, J., Perrinet, L., Kumar, A., Aertsen, A., and Masson, G. (2007).
+  Synchrony in thalamic inputs enhances propagation of
+  activity through cortical layers. 
+  BMC Neuroscience, vol. 8(Suppl 2), P206.
+  http://dx.doi.org/10.1186/1471-2202-8-S2-P180
+
+It requires the PyNN simulator interface
+(http://neuralensemble.org/trac/PyNN), and is currently set up to
+use the NEST simulation engine (http://www.nest-initiative.org).
+
+The model retina consists of 2 one-to-one connected layers of neurons
+on a rectangular grid, modelling ON and OFF magnocellular retinal
+ganglion cells in the primate retina.
+
+$Id$
 """
 
-retina.py
-===================
-
-The retina for 'benchmark one'. This should feed the "Hyper-Column" ( https://facets.kip.uni-heidelberg.de/private/wiki/index.php/V1_hypercolumn#Benchmark_one )
-For this retina, it consists of 2 layers of neurons on a rectangular grid connected in a one to one fashion.
-Here, we more use primate " 'Phasic' responses originate with morphologically larger ganglion cell types with fast optic nerve fiber conduction velocities (~4 m/s, Gouras, 1969). Microelectrodes staining of such cells shows that they are 'parasol' types (Dacey and Lee, 1994). ON types branch low in the inner plexiform layer (sublamina b), while OFF types branch high in the inner plexiform layer (sublamina a) following the classic branching pattern for ON and OFF center cells (Nelson et al, 1978; Dacey and Lee, 1994). Phasic cells are often referred to as the 'magnocellular' or 'M-cell' pathway because their fibers terminate in the magnocellular layer of the lateral geniculate nucleus of the thalamus. Near the fovea receptive fields of phasic cells are 2-3 times larger than those of tonic cells and may be 10 times larger in peripheral retina."
-
-See :
-Data for parameters http://webvision.med.utah.edu/GCPHYS1.HTM (TODO LUP: define different sets for different animals (cat, monkey, human))
-Morphology : http://webvision.med.utah.edu/GC1.html
-Topics on http://homepages.inf.ed.ac.uk/cgi/rbf/CVONLINE/phase3entries.pl?TAG59
-
-LUP : the grid is rectangular, but should be hexagonal
-TODO: justify or not SFA (since we are interested in synchrony effect, we should rather use a simpler model
-
-$ Id $
-
-"""
 import datetime
 try: 
     import pyNN.nest as pyNN
@@ -30,13 +32,15 @@ except:
 import os, tempfile
 import numpy
 
+
 def tmpfile2spikelist(filename,dt):
     """
-    Returns a spike list from the tmp file saved by PyNN-NEST (=a line commented, then one line per event: absolute time in ms, GID)
+    Returns a spike list from the tmp file saved by PyNN-NEST (a line
+    commented, then one line per event: absolute time in ms, GID)
     
-    the spike list is represented as a list of events. Events are tuples (relative time
-    since last event, neuron_id); neuron_id and time are integers
-
+    The spike list is represented as a list of events. Events are
+    tuples (relative time since last event, neuron_id); neuron_id and
+    time are integers.
     """
     import os
 
@@ -60,16 +64,20 @@ def tmpfile2spikelist(filename,dt):
 def spikelist2spikematrix(DATA, N, N_time, dt):
     """
     Returns a matrix of the number of spikes during simulation.
-    Is of the shape of N x N 
-    The spike list is a list of tuples (rel time, neuron_id) where the location of each neuron_id is given by the NEURON_ID matrix, which is in a standardized way [[ (N*i+j)  ]] where i and j are line and column respectively.
+    
+    Is of the shape of N x N.
+    
+    The spike list is a list of tuples (rel time, neuron_id) where the
+    location of each neuron_id is given by the NEURON_ID matrix, which
+    is in a standardized way [[ (N*i+j) ]] where i and j are line and
+    column respectively.
 
-    for instance for a 4x4 population
+    For instance, for a 4x4 population:
 
-       [[ 0,  1,  2,  3],
+      [[ 0,  1,  2,  3],
        [ 4,  5,  6,  7],
        [ 8,  9, 10, 11],
        [12, 13, 14, 15]]
-
     """
 
     DATA=numpy.array([[int(k), int(v)] for k,v in DATA])
@@ -84,90 +92,84 @@ def spikelist2spikematrix(DATA, N, N_time, dt):
 
     return spikematrix / N_time * dt
 
+
 def retina_default():
     """
-    (Over-)Writes default parameters for the retina into the HDF file located at url.
-
-    Sets up parameters and the whole structure of the dictionary / HDF file in url.
-
-    This part of the benchmark contains all relevant parameters and stores them to a dictionary for clarity &future compatibility with XML exports.
-
-    LUP TODO: this should also come from  a XML file? see Andrew's wrapper
-
+    Return a dictionary with default parameters for the retina.
     """
-    params = {} # a dictionary containing all parameters
-    # === Define parameters ========================================================
-    # LUP: get running file name and include script in HDF5?
+    params = {}
     N=100
     params = {'description' : 'default retina',
-        'N': N, # integer;  total number of Ganglion Cells LUP: how do we include types and units in parameters? (or by default it is a float in ISO standards)
-        'N_ret': 2.0, # float;  diameter of Ganglion Cell's RF
-        'K_ret': 4.0, # float; ratio of center vs. surround in DOG
-        'dt'         : 0.1,# discretization step in simulations (ms)
-        'simtime'    : 40000*0.1,      # float; (ms)
-        'syn_delay'  : 1.0,         # float; (ms)
-        'noise_std' : 2.0,       # (nA??) standard deviation of the internal noise
-        'snr' : 2.0,
-        'weight'     : 1.0,       #
-        'threads' : 2, 'kernelseeds' : [43210987, 394780234],      # array with one element per thread
-        #'threads' : 1, 'kernelseeds' : 43210987,       # array with one element per thread
+        'N'           : N,   # integer;  total number of Ganglion Cells
+        'N_ret'       : 2.0, # float;  diameter of Ganglion Cell's RF
+        'K_ret'       : 4.0, # float; ratio of center vs. surround in DOG
+        'dt'          : 0.1, # discretization step in simulations (ms)
+        'simtime'     : 40000*0.1, # float; (ms)
+        'syn_delay'   : 1.0, # float; (ms)
+        'noise_std'   : 2.0, # (nA??) standard deviation of the internal noise
+        'snr'         : 2.0,
+        'weight'      : 1.0,
+        'threads'     : 2,
+        'kernelseeds' : [43210987, 394780234], # array with one element per thread
         # seed for random generator used when building connections
         'connectseed' : 12345789,  # seed for random generator(s) used during simulation
-        'now' : datetime.datetime.now().isoformat() # the date in ISO 8601 format to avoid overriding old simulations
+        'now' : datetime.datetime.now().isoformat()
+        # the date in ISO 8601 format to avoid overriding old simulations
     }
 
-    # retinal neurons parameters
-    params['parameters_gc'] = {'Theta':-57.0, 'Vreset': -70.0,'Vinit': -63.0, 'TauR': 0.5, 'gL':28.95,
-    'C':289.5, 'V_reversal_E': 0.0, 'V_reversal_I': -75.0, 'TauSyn_E':1.5,
-    'TauSyn_I': 10.0, 'V_reversal_sfa': -70.0, 'q_sfa': 0., #14.48,
-    'Tau_sfa':110.0, 'V_reversal_relref': -70.0, 'q_relref': 3214.0,
-    'Tau_relref': 1.97,'python':True}
-    params['u'] = params['parameters_gc']['Vinit'] + numpy.random.rand(N,N)* (params['parameters_gc']['Theta'] - params['parameters_gc']['Vreset'])
-
+    # retinal neurons' parameters
+    params['parameters_gc'] = {
+        'Theta':-57.0,
+        'Vreset': -70.0,
+        'Vinit': -63.0,
+        'TauR': 0.5,
+        'gL':28.95,
+        'C':289.5,
+        'V_reversal_E': 0.0,
+        'V_reversal_I': -75.0,
+        'TauSyn_E':1.5,
+        'TauSyn_I': 10.0,
+        'V_reversal_sfa': -70.0,
+        'q_sfa': 0., #14.48,
+        'Tau_sfa':110.0,
+        'V_reversal_relref': -70.0,
+        'q_relref': 3214.0,
+        'Tau_relref': 1.97,
+        'python':True}
+    
+    params['u'] = params['parameters_gc']['Vinit'] + \
+                  numpy.random.rand(N,N)* (params['parameters_gc']['Theta'] - \
+                                           params['parameters_gc']['Vreset'])
     return params
 
-def retina_fiber():
 
-    """
-    Parameters for the retina to get just one fiber
-
-    """
-    params = retina_default()
-    params.update({'description' : 'fiber retina','N': 1})
-    #N = params['N']
-    #params.update({'amplitude' : numpy.ones((N, N))})
-    return params
 
 def retina_debug():
-
     """
-    Debug parameters for the retina
-
+    Return a dictionary with parameters for the retina suitable for debugging.
     """
     params = retina_default()
     params.update({'description' : 'debug retina','N': 8})
     return params
 
 
-def run_retina(params):#,url,branch):
-    """
-    params are the parameters to use
-    url,branch the url and branch where we store data
 
-    """
+def run_retina(params):
+    """Run the retina using the specified parameters."""
+    
     tmpdir = tempfile.mkdtemp()
-    # === Build the network ========================================================
+    
     print "Setting up simulation"
+    
     pyNN.Timer.start() # start timer on construction
     pyNN.setup(timestep=params['dt'],max_delay=params['syn_delay'])
     pyNN.pynest.setDict([0],{'threads' : params['threads']})
     pyNN.setRNGseeds(params['kernelseeds'])
 
     N = params['N']
-    #dc_generator
     phr_ON  = pyNN.Population((N,N),'dc_generator')
     phr_OFF  = pyNN.Population((N,N),'dc_generator')
-    noise_ON = pyNN.Population((N,N),'noise_generator',{'mean':0.,'std':params['noise_std']})  # TODO LUP : justify this noise model
+    noise_ON = pyNN.Population((N,N),'noise_generator',{'mean':0.,'std':params['noise_std']})
     noise_OFF = pyNN.Population((N,N),'noise_generator',{'mean':0.,'std':params['noise_std']})
 
     phr_ON.set({ 'start' : params['simtime']/4, 'stop' : params['simtime']/4*3})
@@ -175,20 +177,21 @@ def run_retina(params):#,url,branch):
     phr_OFF.set({ 'start' : params['simtime']/4, 'stop' : params['simtime']/4*3})
     phr_OFF.tset('amplitude', - params['amplitude']  * params['snr'])
 
-    # target ON and OFF populations (what about tridimensional Population?)
-    out_ON = pyNN.Population((N,N) ,'iaf_sfa_neuron',params['parameters_gc']) # IF_curr_alpha)#
-    out_OFF = pyNN.Population((N,N) ,'iaf_sfa_neuron',params['parameters_gc']) # IF_curr_alpha)#
+    # target ON and OFF populations
+    out_ON = pyNN.Population((N,N) ,'iaf_sfa_neuron',params['parameters_gc'])
+    out_OFF = pyNN.Population((N,N) ,'iaf_sfa_neuron',params['parameters_gc'])
 
-    #  Connecting the network
+    #print "Connecting the network"
+
     retina_proj_ON = pyNN.Projection(phr_ON, out_ON, 'oneToOne')
-    retina_proj_ON.setWeights(params['weight']) #*ones((N,N)) )
+    retina_proj_ON.setWeights(params['weight'])
     retina_proj_OFF = pyNN.Projection(phr_OFF, out_OFF, 'oneToOne')
-    retina_proj_OFF.setWeights(params['weight']) #*ones((N,N)) )
+    retina_proj_OFF.setWeights(params['weight'])
 
     noise_proj_ON = pyNN.Projection(noise_ON, out_ON, 'oneToOne')
-    noise_proj_ON.setWeights(params['weight']) #*ones((N,N)) )
-    noise_proj_OFF = pyNN.Projection(noise_OFF, out_OFF, 'oneToOne') # implication of ON and OFF having the same noise input?
-    noise_proj_OFF.setWeights(params['weight']) #*ones((N,N)) )
+    noise_proj_ON.setWeights(params['weight'])
+    noise_proj_OFF = pyNN.Projection(noise_OFF, out_OFF, 'oneToOne')
+    noise_proj_OFF.setWeights(params['weight'])
 
     out_ON_filename=os.path.join(tmpdir,'out_on.gdf')
     out_OFF_filename=os.path.join(tmpdir,'out_off.gdf')
@@ -198,34 +201,34 @@ def run_retina(params):#,url,branch):
     # reads out time used for building
     buildCPUTime= pyNN.Timer.elapsedTime()
 
-    # === Run simulation ===========================================================
     print "Running simulation"
 
     pyNN.Timer.start() # start timer on construction
     pyNN.run(params['simtime'])
     simCPUTime = pyNN.Timer.elapsedTime()
 
-    # TODO LUP use something like "for pop in [phr, out]" ?
     out_ON.printSpikes(out_ON_filename)
     out_OFF.printSpikes(out_OFF_filename)
 
-    # TODO LUP  get out_ON_DATA on a 2D grid independantly of out_ON.cell.astype(int)
     out_ON_DATA = tmpfile2spikelist(out_ON_filename,params['dt'])
     out_OFF_DATA = tmpfile2spikelist(out_OFF_filename,params['dt'])
 
     print "\nRetina Network Simulation:"
     print(params['description'])
     print "Number of Neurons  : ", N**2
-    print "Output rate  (ON) : ", out_ON.meanSpikeCount(), "spikes/neuron in ", params['simtime'], "ms"
-    print "Output rate (OFF)   : ", out_OFF.meanSpikeCount(), "spikes/neuron in ",params['simtime'], "ms"
+    print "Output rate  (ON) : ", out_ON.meanSpikeCount(), \
+        "spikes/neuron in ", params['simtime'], "ms"
+    print "Output rate (OFF)   : ", out_OFF.meanSpikeCount(), \
+        "spikes/neuron in ",params['simtime'], "ms"
     print "Build time         : ", buildCPUTime, "s"
     print "Simulation time    : ", simCPUTime, "s"
 
     return out_ON_DATA,out_OFF_DATA
 
-if __name__ == '__main__':
 
-    #retina_default(url)
+
+
+if __name__ == '__main__':
     params=retina_debug()
     params.update({'amplitude' : .10*numpy.ones((params['N'],params['N']))})
     out_ON_DATA, out_OFF_DATA=run_retina(params)
@@ -240,4 +243,5 @@ if __name__ == '__main__':
     neuron_id=out_OFF_DATA[:,1]
     pylab.plot(spike_time,neuron_id,'.b')
     pylab.axis('tight')
-    print spikelist2spikematrix(out_ON_DATA, params['N'], params['simtime']/params['dt'], params['dt'])
+    print spikelist2spikematrix(out_ON_DATA, params['N'],
+                                params['simtime']/params['dt'], params['dt'])
