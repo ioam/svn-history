de8f2be1474bb533bfc275d0cf297cdf0714a516
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2009-01-18 16:09:56 +0000

    Removed topo/base/parameterclasses.py.

--- topo/tests/testsheetview.py
+++ topo/tests/testsheetview.py
@@ -95,7 +95,7 @@ class TestSheetView(unittest.TestCase):
 ## from topo.base.sheet import Sheet
 ## from topo.base.simulation import EventProcessor
 ## from topo.misc.util import NxN
-## from topo.base.parameterclasses import Parameter
+## from topo.param import Parameter
 ## from pprint import *
 ## import Image, ImageOps
 
