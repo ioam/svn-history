93571bb323fb0d7eb2ac4a530d6cd7ddf6d92a6b
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2009-02-11 13:01:23 +0000

    Added section about visualizing map measurement.

--- doc/Tutorials/lissom_oo_or_text.php
+++ doc/Tutorials/lissom_oo_or_text.php
@@ -457,6 +457,42 @@ so that the network will be in a well-defined state.  (To do this,
 just type the fractional part into the "Run for:" box, i.e. 0.95 if
 the time is currently 10002.05, and then press "Go".)
 </li>
+
+<li>
+
+<!--CB: I think 'hook' is worse than 'command' in this context-->
+Earlier we introduced an orientation preference map, stating that it
+was the result of a procedure similar to that used in optical imaging
+experiments. In fact, you can watch the procedure as it takes
+place. To do this, we will re-measure the map, but with visualization
+enabled.
+
+<P>Returning to the Orientation Preference window, the commands used
+to generate the data for the existing plots are shown in
+the <span class="t_item">Pre plot hooks</span>. In Topographica, 'hook
+lists' are general-purpose locations for executable commands. Users
+can add their own commands at such locations, as well as editing the
+properties of existing ones. To access the commands for measuring the
+orientation map, right click on the list and select Properties. You
+will see that in this case there is one command,
+<?php classref('topo.command.analysis','measure_sine_pref') ?>. As
+described in the documentation, this is a command to measure
+preferences for sine gratings in various combinations. We can access
+its properties by right clicking on the measure_sine_pref command and
+again choosing Properties. As usual, a description of each property is
+available by hovering the mouse over its name. In this case, we are
+interested in 'Display', which controls whether or not the GUI should
+be updated as the command proceeds. Change its value to True, choose
+Apply, and return to the Orientation Preference window. At this point,
+ensure that you have an <span class="w_title">Activity</span> window
+open with <span class="t_item">Normalize</span> and
+<span class="t_item">Auto refresh</span> turned on, and 
+<span class="t_item">Strength only</span> turned off. Now press
+Refresh by the Orientation Preference window's Pre plot hooks; you
+will see a series of sine gratings presented to the network, and you
+can observe the response each time in the LGN and V1 sheets.
+</li>
+
 </ol>
 
 
