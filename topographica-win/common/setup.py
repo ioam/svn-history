376a6637beccf005ed6ad4333a19b0f5cdde939d
James A. Bednar <jbednar@inf.ed.ac.uk>
2009-02-12 11:16:52 +0000

    Updated to declare version 0.9.6

--- topographica-win/common/setup.py
+++ topographica-win/common/setup.py
@@ -51,7 +51,7 @@ f.write("environ['PATH']="+repr(os.path.join(path,compiler_path))+"\n")
 f.write("\n")
 f.write('import topo'+'\n')
 # CEBHACKALERT: This is hard-coded twice: should be read from somewhere.
-f.write("topo.release='0.9.5'\n")
+f.write("topo.release='0.9.6'\n")
 f.write("\n")
 f.write("# Process the command-line arguments\n")
 f.write('from sys import argv\n')
