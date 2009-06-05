a3381a5f751f8ea2592bb50831ff6795ad29be42
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2009-06-05 16:55:19 +0000

    Moved code common to distutils and setuptools into _setup.py.

---
+++ _setup.py
@@ -0,0 +1,45 @@
+common = dict(
+
+    name='Topographica',
+
+    version='0.9.6' ,
+
+    description='Topographica is a general-purpose neural simulator focusing on topographic maps.',
+
+    maintainer='Topographica Developers',
+
+    maintainer_email='developers[at]topographica[dot]org',
+
+    url='http://topographica.org/',
+
+    # CEBALERT: do I have to list these? if I do, can I generate the list automatically?
+    packages=['topo',
+              'topo.analysis',
+              'topo.base',
+              'topo.command',
+              'topo.coordmapper',
+              'topo.ep',
+              'topo.ipythonTk',
+              'topo.learningfn',
+              'topo.misc',
+              'topo.numbergen',
+              'topo.transferfn',
+              'topo.param',
+              'topo.pattern',
+              'topo.plotting',
+              'topo.projection',
+              'topo.responsefn',
+              'topo.sheet',
+              'topo.tests',
+              'topo.tkgui'],
+
+    package_data={'topo.tkgui': ['icons/*.*'],
+                  'topo.command':['*.png','*.pdf'],
+                  'topo.tests':['*.txt','*.jpg','*.pgm']},
+
+    scripts = ['topographica']
+
+    )
+
+
+
--- setup.py
+++ setup.py
@@ -1,41 +1,10 @@
 #!/usr/bin/env python
 
-# CB: work in progress. Current issues:
-#
-# (1) why does topo end up in /usr/local/lib/python2.6/dist-packages/
-#     rather than site-packages?
-# Maybe it's supposed to? E.g. https://bugs.launchpad.net/ubuntu/+source/python2.6/+bug/362570
+# CB: work in progress
 
 from distutils.core import setup
 
-setup(name='Topographica',
-      version='0.9.6',
-      description='Topographica is a general-purpose neural simulator focusing on topographic maps.',
-      maintainer='Topographica Developers',
-      maintainer_email='developers[at]topographica[dot]org',
-      url='http://topographica.org/',
-      # CEBALERT: do I have to list these? if I do, can I generate the list automatically?
-      packages=['topo',
-                'topo.analysis',
-                'topo.base',
-                'topo.command',
-                'topo.coordmapper',
-                'topo.ep',
-                'topo.ipythonTk',
-                'topo.learningfn',
-                'topo.misc',
-                'topo.numbergen',
-                'topo.transferfn',
-                'topo.param',
-                'topo.pattern',
-                'topo.plotting',
-                'topo.projection',
-                'topo.responsefn',
-                'topo.sheet',
-                'topo.tests',
-                'topo.tkgui'],
-      package_data={'topo.tkgui': ['icons/*.*'],
-                    'topo.command':['*.png','*.pdf'],
-                    'topo.tests':['*.txt','*.jpg','*.pgm']},
-      scripts = ['topographica']
-     )
+from _setup import common
+
+setup(**common)
+
