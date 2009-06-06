a5b43fcbdce78a286c513668cb360537058ab10a
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2009-06-06 15:14:59 +0000

    Began attempting to include external packages.

--- setup_app.py
+++ setup_app.py
@@ -7,7 +7,16 @@ Usage:
     python setup.py py2app
 """
 from setuptools import setup
+
+# Currently this doesn't work because Topographica doesn't build
+# lib/libpython2.5.dylib.  Not sure what to do about that.
+
+OPTIONS = {
+    'packages': ['numpy','matplotlib','pil'] # ...more to go
+    }
+
 setup(
-    app=["topographica"],
+    app=["topographica.py"],
     setup_requires=["py2app"],
+    options = {'py2app':OPTIONS}
 )
