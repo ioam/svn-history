fa7dcb29ed486c8fbf633128a326a803e2b3431b
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2009-09-13 20:17:31 +0000

    Various updates. Getting closer to working...

--- setup_ez.py
+++ setup_ez.py
@@ -1,18 +1,23 @@
 """
 easy_install build script for Topographica.
 
-("easy_install topographica")
+(eventually "easy_install topographica")
 
-Work in progress
+Work in progress: currently missing weave, and still have to get some
+prerequisites manually.
+
 
 Usage:
-    python setup.py develop
+    python setup_ez.py develop
 or
-    pythin setup.py install
+    python setup_ez.py install
 """
 
-# We need numpy, scipy, and pil to work with easy_install before we
-# can make any more progress...
+# prerequisites:
+#
+# * python (with Tkinter for the GUI: also tcllib and tklib for GUI)
+# * GMP (if using a package manager, also get -dev version. E.g. "sudo apt-get install libgmp3c2 libgmp3-dev")
+# * numpy (e.g. "sudo easy_install numpy")
 
 from setuptools import setup
 
@@ -21,19 +26,24 @@ import _setup
 _setup.create_topographica_script()
 
 setup(
-      install_requires=["numpy >= 1.0",
-                        "gmpy >= 1.0",
-                        "matplotlib >= 0.91",
-                        "scipy >= 0.3.0",
-                        "ipython >= 0.8",
-                        "imaging >= 1.1.6",
-                        #
-                        # python-imaging-tk 
-                        # python-tk          
-                        # tcllib
-                        # tklib
+      #parsed in reverse order?
+      install_requires=[
+                        "gmpy==1.04",
+                        "matplotlib>=0.99",
+# CEBALERT: need to do something about weave
+#                        "scipy >= 0.3.0",
+                        "ipython>=0.8",
+                        "PIL==1.1.6",
+# CEBALERT:"sudo easy_install numpy" works, but numpy doesn't work as dependency (numerous bug reports about this?)
+#                        "numpy >= 1.1.0",
                         ],
-      # pil pypi links are broken?
-      dependency_links=['http://www.pythonware.com/products/pil/'],
+            
+      dependency_links=[# pil pypi links are broken (last checked Sep 09)
+                        'http://doozy.inf.ed.ac.uk:8010/extra/PIL-1.1.6.tar.gz'
+                        # otherwise can't get 0.99 (previous ones fail to build) 
+                        'http://doozy.inf.ed.ac.uk:8010/extra/matplotlib-0.99.0.tar.gz',
+                        # otherwise can't get 1.04 (previous ones fail to build)
+                        'http://gmpy.googlecode.com/files/gmpy-1.04.zip'
+                        ],
       **_setup.common
      )
