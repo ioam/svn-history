550361589cbec3a4d867d5d6c46dbe3740924658
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2009-10-01 15:15:53 +0000

    First version that appears to work, depending only on Python, Tcl/Tk, and GMP.

--- setup_ez.py
+++ setup_ez.py
@@ -3,9 +3,7 @@ easy_install build script for Topographica.
 
 (eventually "easy_install topographica")
 
-Work in progress: currently missing weave, and still have to get some
-prerequisites manually.
-
+Work in progress.
 
 Usage:
     python setup_ez.py develop
@@ -13,11 +11,16 @@ or
     python setup_ez.py install
 """
 
-# prerequisites:
+# Required:
 #
-# * python (with Tkinter for the GUI: also tcllib and tklib for GUI)
+# * python, python-dev
 # * GMP (if using a package manager, also get -dev version. E.g. "sudo apt-get install libgmp3c2 libgmp3-dev")
-# * numpy (e.g. "sudo easy_install numpy")
+#
+# Optional:
+# 
+# * GUI: tcl8.5,tcl8.5-dev,tk8.5,tk8.5-dev,tcllib,tklib,python-tk
+
+
 
 from setuptools import setup
 
@@ -28,22 +31,32 @@ _setup.create_topographica_script()
 setup(
       #parsed in reverse order?
       install_requires=[
+                        "weave==0.4.9",
+                        "pyscrodget==0.0.2_2.1",
                         "gmpy==1.04",
                         "matplotlib>=0.99",
 # CEBALERT: need to do something about weave
 #                        "scipy >= 0.3.0",
                         "ipython>=0.8",
-                        "PIL==1.1.6",
-# CEBALERT:"sudo easy_install numpy" works, but numpy doesn't work as dependency (numerous bug reports about this?)
-#                        "numpy >= 1.1.0",
+                        "numpy >= 1.2.0",
+                        "PIL == 1.1.6", # error unless done before numpy!
                         ],
-            
-      dependency_links=[# pil pypi links are broken (last checked Sep 09)
-                        'http://doozy.inf.ed.ac.uk:8010/extra/PIL-1.1.6.tar.gz'
-                        # otherwise can't get 0.99 (previous ones fail to build) 
-                        'http://doozy.inf.ed.ac.uk:8010/extra/matplotlib-0.99.0.tar.gz',
+
+      # pil pypi links are broken (last checked Sep 09)
+      # pil doesn't work with setuptools (Imaging vs PIL)
+      # http://www.gossamer-threads.com/lists/python/python/772845
+      # pil patched to find tk includes
+      
+      # matplotlib-0.99.1.1.tar.gz has stray setup.cfg in it
+      # ...
+
+      # numpy patched to work as dependency
+      # (http://projects.scipy.org/numpy/ticket/999)            
+
+      dependency_links=['http://doozy.inf.ed.ac.uk:8010/extra/',
+
                         # otherwise can't get 1.04 (previous ones fail to build)
-                        'http://gmpy.googlecode.com/files/gmpy-1.04.zip'
-                        ],
+                        'http://gmpy.googlecode.com/files/gmpy-1.04.zip'],
+        
       **_setup.common
      )
