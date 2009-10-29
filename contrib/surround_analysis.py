9f8ae8bb964e3aa33cf3714bdd556b786fbe6fba
Jan Antolik <antolikjan@gmail.com>
2009-10-29 13:37:00 +0000

    change of parameters

--- contrib/surround_analysis.py
+++ contrib/surround_analysis.py
@@ -500,17 +500,17 @@ def plot_neural_dynamics(sheet_names,neurons,pattern_generator):
      
      
      
-def run_dynamics_analysis(x,y):
+def run_dynamics_analysis(x,y,cs):
     from topo.pattern.basic import OrientationContrast
     
     (xx,yy) = topo.sim["V1Complex"].sheet2matrixidx(x,y)
     
     orr=numpy.pi*topo.sim["V1Complex"].sheet_views["OrientationPreference"].view()[0][xx][yy]
     
-    pg = OrientationContrast(orientationcenter=orr,orientationsurround=orr,sizecenter=0.7,sizesurround=2.0,thickness=1.3,scalecenter=0.5,scalesurround=0.5,x=x,y=y,frequency=3.0)
+    pg = OrientationContrast(orientationcenter=orr,orientationsurround=orr,sizecenter=cs,sizesurround=2.0,thickness=2.0-cs,scalecenter=0.5,scalesurround=0.5,x=x,y=y,frequency=3.0)
     
     plot_neural_dynamics(["V1Complex","V1ComplexInh"],[("V1Complex",(x,y)),("V1ComplexInh",(x,y))],pg)
     
-    pg = OrientationContrast(orientationcenter=orr,orientationsurround=orr+numpy.pi/2,sizecenter=0.7,sizesurround=2.0,thickness=1.3,scalecenter=0.5,scalesurround=0.5,x=x,y=y,frequency=3.0)
+    pg = OrientationContrast(orientationcenter=orr,orientationsurround=orr+numpy.pi/2,sizecenter=cs,sizesurround=2.0,thickness=2.0-cs,scalecenter=0.5,scalesurround=0.5,x=x,y=y,frequency=3.0)
     
     plot_neural_dynamics(["V1Complex","V1ComplexInh"],[("V1Complex",(x,y)),("V1ComplexInh",(x,y))],pg)
