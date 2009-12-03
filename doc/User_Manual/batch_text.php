e7038bd58e34a6ce7060747d74c44c2433a8c0ce
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2009-12-03 15:13:09 +0000

    Added 'parameter_analysis' script for generating simple reports from collections of batch runs.

--- doc/User_Manual/batch_text.php
+++ doc/User_Manual/batch_text.php
@@ -75,3 +75,194 @@ simulation directory, because that is closed when run_batch completes.
 Thus it's usually best to use run_batch's options rather than
 the separate commands shown in this example.
 
+
+<!-- CB: draft
+
+<H2>Parameter searches</H2>
+
+<P>Often you will want to investigate the behavior of a simulation
+when changing certain aspects of it, such as the input or internal
+parameters. Doing this may require a large number of simulation runs,
+so to make this easy, we provide the script
+<code>parameter_search</code> for launching multiple batch runs
+(e.g. for submission to a cluster), and the script
+<code>parameter_analysis</code> for organizing and analysing the
+results.
+
+<H3>Parameterize your simulation</H3>
+
+<P>Ensure that your script file uses parameters (as shown in our
+example scripts) for any quantities that you want to vary.  For
+instance, you might include a section like this near the beginning of
+your script:
+
+<pre>
+
+from topo.misc.commandline import global_params as p
+p.add(
+    dataset=param.Enumeration(default='Gaussian',available=
+        ['Gaussian','Nature','FoliageA','FoliageB']),
+
+    dims=param.List(default=['or','od','dr','dy'],class_=str),
+
+    retina_density=param.Number(default=24.0,bounds=(0,None),
+        inclusive_bounds=(False,True)),
+
+    cortex_density=param.Number(default=48.0,bounds=(0,None),
+        inclusive_bounds=(False,True)),
+
+    afferent_strength=param.Number(default=1.0,bounds=(0,10),
+    
+    lateral_strength=param.Number(default=1.0,bounds=(0,100))
+)
+
+</pre>
+
+(where the docstrings of the parameters have been ommitted for brevity
+in this case).
+
+
+<H3>Set up the experiments</H3>
+
+<P>Create a text file describing combinations parameters to use as
+experiments. For instance, if we want to see the effects of changing
+<code>afferent_strength</code> from <code>2.0</code> to
+<code>8.0</code> on simulations that use either the
+<code>FoliageB</code> or <code>Gaussian</code> datasets, and contain
+either the dimensions <code>or,cr</code> or <code>or,cr,od</code>, we
+might define the following experiments:
+ 
+<pre>
+dataset      dims               afferent_strength
+"FoliageB"   ["or","cr"]        2.0
+"FoliageB"   ["or","cr"]        8.0
+"Gaussian"   ["or","cr"]        2.0
+"Gaussian"   ["or","cr"]        8.0
+"FoliageB"   ["or","cr","od"]   2.0
+"FoliageB"   ["or","cr","od"]   8.0
+"Gaussian"   ["or","cr","od"]   2.0
+"Gaussian"   ["or","cr","od"]   8.0
+</pre>
+
+CEBALERT: could be improved!
+(Formatting rules: there must be a header row, spaces are used as
+separators, strings must be enclosed in double quotes, no newline at
+the end of the file.) In this case, call the file something like
+<code>aff_strength_investigation</code>.
+
+
+<H3>Launch the experiments</H3>
+
+<P>Before you can use the file above to create a script that will
+launch multiple, simultaneous simulations, you need to specify how
+each simulation should be launched. For a normal desktop machine,
+there is usually nothing special to do, but a cluster will usually
+have its own job submission system. <code>parameter_search</code>
+reads a configuration file where this and other options can be set:
+
+<pre>
+[Options]
+# Where is the topographica script? Note that some job submission
+# systems require an absolute path
+# e.g. "/home/user/topographica/topographica". 
+topographica_script = './topographica'
+
+# The results (i.e. directories created by) all calls to run_batch()
+# will be in one overall directory: results_path + name of
+# combinations file.  results_path must already exist. Note that you
+# should usually supply a local path (rather than one on a network) if
+# you are going to be saving many or large files.
+# e.g. "/disk/scratch/"
+results_path = './Output/' 
+
+# run_batch()'s dirname_prefix; ${combonumber} will be replaced with
+# the combination number
+topographica_dirname_prefix_template = '${combonumber}'
+
+# The appropriate command template for your system; ${job} will be
+# replaced with the actual job. Additionally, you can use
+# ${combonumber} to get the combination number and ${comboname} to get
+# the name of the combination file.
+#
+# E.g. on eddie: 
+#  'qsub -cwd -P ecdf_baseline -pe "memory" 1 -l s_rt=40:00:0 -R y ${job}'
+job_submit_template = 'nohup nice ${job} > ${comboname}-${combonumber}.out &'
+
+# Arguments passed to Topographica before any others.
+topographica_args = ['-a']
+</pre>
+
+<P>If your configuration file is <code>ps.cfg</code>, and your
+simulation is defined in <code>myscript.ty</code>, you would call
+<code>parameter_search</code> like this:
+
+<pre>
+parameter_search ps.cfg myscript.ty aff_strength_investigation 'retina_density=48.0,lateral_strength=10.0,analysis_function=mymodule.myanalysisfn(),times=[0,100,1000]'
+</pre>
+
+<P>The final arguments, enclosed in single quotes, are any parameters
+that you want to pass to <code>run_batch</code> in all of the
+experiments. Here, we are overriding the script's default
+<code>retina_density</code> and <code>lateral_strength</code>, and
+requesting that a custom analysis function be called at times 0, 100,
+and 1000. Since we are calling a custom analysis function defined in
+<code>mymodule</code>, we need to ensure that <code>mymodule</code> is
+imported. To do this, <code>ps.cfg</code>'s
+<code>topographica_args</code> should have contained the appropriate
+code before running the command above:
+
+<pre>
+topographica_args = ['-a','import mymodule']
+</pre>
+
+<P><code>parameter_search</code> creates a script file that, when run,
+will submit all your experiments using the job submission command you
+defined in the <code>ps.cfg</code> file.
+
+<H3>Analysis</H3> 
+
+<P>Once your jobs have completed, you will need to explore the
+results.  An easy way of doing this, especially once you have lots of
+experiments, is to use the analysis script we provide, which puts
+selected plots into a pdf file (via latex).
+
+<P>The basic idea of the script is that you specify a directory to
+analyse, optionally along with any criteria for which experiments to
+include (e.g. only <code>dims=['or','cr']</code>) and what output to
+include for each experiment. This specification is done via a
+configuration file:
+
+<pre>
+[Outputs]
+interests = ["Hue_PreferenceAndSelectivity",
+             "Orientation_PreferenceAndSelectivity"]
+
+times = ["000000.00",
+         "000100.00",
+         "001000.00"]
+
+
+[Conditions]
+dims = ['cr','or']
+retina_density=48.0
+
+[Condition-matching functions]
+dims = 'sets_equal'
+</pre>
+
+Here we are saying that for each experiment, any plots containing the
+string <code>Hue_PreferenceAndSelectivity</code> or
+<code>Orientation_PreferenceAndSelectivity</code> should be included,
+at times 0, 100, and 1000. We are only interested in experiments that
+have <code>dims=['cr','or']</code> (<code>sets_equal</code> means
+<code>dims=['or','cr']</code> would also match) and
+retina_density=48.0.
+
+<P>Running the script as: <code>parameter_analysis
+/path/to/experiments/ config-file</code>, the output will be a pdf
+showing the specified plots at each of the specified times for all
+experiments in subdirectories of </code>/path/to/experiments/</code>
+that used parameters matching any specifications made in the
+<code>Conditions</code> section of config-file.
+
+-->
